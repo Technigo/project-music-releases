5d821d404361963d2c6b67364f3d5ab07d80d0f7
nasimmhn <nasim.mahzoun@gmail.com>
2019-11-18 23:28:58 +0100

    first commit

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import data from './data.json'
+import { Album } from './Album'
+import { Header } from './Header'
 
-console.log(data)
+
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
+      <Album />
     </div>
   )
 }
