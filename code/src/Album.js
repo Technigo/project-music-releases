dce72ec171808465baf282ec7ee245a259388815
nasimmhn <nasim.mahzoun@gmail.com>
2019-11-20 22:21:58 +0100

    added links to artists and albums

--- code/src/Album.js
+++ code/src/Album.js
@@ -3,13 +3,19 @@ import React from 'react'
 export const Album = (props) => {
   return (
     <div>
-      <img src={props.image} alt="" />
+      <img src={props.image} alt="Album" />
       <div className="album-info">
         <div className="album-title">
-          {props.name}
+          <a href={props.albumURL}>{props.albumTitle}</a>
         </div>
         <div className="artist-name">
-          {props.artists}
+
+          {props.artists.map((artist) => {
+
+            return <a key={artist.external_urls.spotify} href={artist.external_urls.spotify}>{artist.name}</a>
+
+          })}
+        
         </div>
       </div>
     </div>
--- code/src/App.js
+++ code/src/App.js
@@ -5,22 +5,16 @@ import data from './data.json'
 console.log(data)
 
 
+
 export const App = () => {
   return (
     <div>
       <Header />
       <section id="album-container">
         {data.albums.items.map((item) => {
-          // // Since there can be multiple artists per album 
-          // // I loop through the array artist and push the names into array = []
-          let array = []
-          for (let i = 0; i < item.artists.length; i++) {
-            array.push(item.artists[i].name)
-          }
-          // // Using the join method to make a string with the artists divided by ", "
-          let artistsList = array.join(", ")
 
-          return <Album key={item.id} name={item.name} image={item.images[1].url} artists={artistsList} />
+          return <Album key={item.id} albumTitle={item.name} image={item.images[1].url} artists={item.artists} />
+
         })}
       </section>
     </div>
