2b7146bd1771f912b845c58b10ce6a3b9637eee6
horsefacegirl <lindstrmida@gmail.com>
2019-11-21 14:19:43 +0100

    start

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import data from './data.json'
+import { Album } from "./Components/Album"
+import {AlbumCover} from "./Components/AlbumCover"
 
 console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  <div className="flex">
+    {data.albums.items.map(album => (
+      <div key={album.id}>
+      <AlbumCover
+      image={album.images[1].url}
+      />
+      <Album
+      name={album.name}
+      />
+      </div>
+    ))}
+  </div>
   )
 }
+
--- code/src/index.js
+++ code/src/index.js
@@ -2,5 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
+import './Components/Album.css'
+import './Components/AlbumCover.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
