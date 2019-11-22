95e427447443c862f61bc5927c8df3bf2d6dab13
Clara Gyökeres <claragyokeres@Claras-MacBook-Air.local>
2019-11-22 17:51:30 +0100

    update

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import data from './data.json'
+import React from "react"
+import data from "./data.json"
+import { Album } from "./components/Album"
+import { Artists } from "./components/Artist"
+import { Header } from "./components/Header"
 
 console.log(data)
+console.log(Album)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <div className='card'>
+      <Header />
+      <section className='album-container'>
+        {data.albums.items.map((item) => {
+          return <Album key={item.id} albumTitle={item.name} image={item.images[1].url} artists={item.artists[0].name} url={item.external_urls.spotify} />
+        })}
+      </section>
+    </div >
   )
 }
---
+++ code/src/components/Album.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Album = (props) => {
+    return (
+        <div className='artistBox'>
+            <div className='album-cover'>
+                <img className='album-img' src={props.image} alt='Album' />
+            </div>
+            <div className='album-info'>
+                <div className='album-title'>
+                    <a href={props.albumURL}>{props.albumTitle}</a>
+                    <div className='artist-name'>
+                        {props.artists}
+                    </div>
+                </div >
+            </div >
+        </div >
+    )
+}
