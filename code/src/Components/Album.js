bb4317cea5bb55bc3e60825cd8e809b50bf9d01f
Agnes Ketesdi <agnes@Agness-MacBook-Air.local>
2019-11-23 22:52:46 +0100

    added icons on hover and some styling, created css file for icons

--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { Icons } from "Components/Icons";
 
 export const Album = props => {
   return (
     <div className="album-container">
-      <img
-        src={props.album.images[1].url}
-        alt="album cover"
-        className="album-cover"
-      />
+      <div className="album-wrapper">
+        <img
+          src={props.album.images[1].url}
+          alt="album cover"
+          className="album-image"
+        />
+        <Icons />
+      </div>
+
       <a className="album-link" href={props.album.external_urls.spotify}>
         <h2>{props.album.name}</h2>
       </a>
