278ec4245f73e0729c87cf6ed6ce88b9426a01cf
Jens_Nilsen <jensnilsen89@gmail.com>
2019-11-23 02:18:38 +0100

    git problem solving

--- code/src/Album.js
+++ code/src/Album.js
@@ -4,15 +4,16 @@ import { Logos } from "./Logos";
 
 export const Album = props => (
   <div className="albumCard">
-    <div
-      className="albumImg"
-      style={{ backgroundImage: `url(${props.item.images[1].url})` }}
-    >
-      <div className="theLogos">
-        <Logos />
+    <div className="albumHead">
+      <div
+        className="albumImg"
+        style={{ backgroundImage: `url(${props.item.images[1].url})` }}
+      >
+        <div className="theLogos">
+          <Logos />
+        </div>
       </div>
     </div>
-
     <a href="#">{props.item.name}</a>
   </div>
 );
