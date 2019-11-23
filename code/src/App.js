af92b966a1b2940a30a293e98263b63c452744fb
Clara Gyökeres <claragyokeres@Claras-Air.lan>
2019-11-23 14:43:11 +0100

    media q added

--- code/src/App.js
+++ code/src/App.js
@@ -3,6 +3,7 @@ import data from "./data.json"
 import { Album } from "./components/Album"
 import { Artists } from "./components/Artist"
 import { Header } from "./components/Header"
+import { tsPropertySignature } from "@babel/types"
 
 console.log(data)
 console.log(Album)
@@ -13,7 +14,7 @@ export const App = () => {
       <Header />
       <section className='album-container'>
         {data.albums.items.map((item) => {
-          return <Album key={item.id} albumTitle={item.name} image={item.images[1].url} artists={item.artists[0].name} url={item.external_urls.spotify} />
+          return <Album key={item.id} albumTitle={item.name} image={item.images[1].url} artists={item.artists[0].name} url={item.url} />
         })}
       </section>
     </div >
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Artists = () => {
+export const Album = (props) => {
     return (
         <div>
             hej
