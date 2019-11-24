979d6a07e62cb15385a9d640083f1ab32ad48dde
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2019-11-24 20:32:54 +0100

    fix

--- code/src/App.js
+++ code/src/App.js
@@ -3,37 +3,37 @@ import data from './data.json'
 import { Album } from './Components/Album.js'
 import { Artist } from './Components/Artist.js'
 
+
 console.log(data)
 
 const releases = data.albums.items;
 
 export const App = () => {
-  return ( 
-    <section className='releases'>
-      <header className='release-header'>
-        <h1 className='release-title'>New albums &amp; singles</h1>
+  return (
+    <section className="releases">
+      <header className="release-header">
+        <h1 className="release-title">New albums &amp; singles</h1>
       </header>
-      <section className='release-container'>
-        {releases.map(album => {
+      <section className="release-container">
+        {releases.map((album) => {
           return (
-            <Album 
+            <Album
               key={album.id}
               name={album.name}
-              url={album.external_urls.sporify}
+              url={album.external_urls.spotify}
               image={album.images[1].url}>
-              {album.artists.map(artist => {
+              {album.artists.map((artist) => {
                 return (
                   <Artist
                     key={artist.id}
                     name={artist.name}
-                    url={artist.external_urls.sporify} />
+                    url={artist.external_urls.spotify} />
                 )
               })}
             </Album>
           )
         })}
       </section>
-
     </section>
   )
 }
--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -3,8 +3,13 @@ import React from 'react'
 export const Album = (props) => {
   return (
     <div className="album-container">
+      <div className="icon-box">
+          <img src="./icons/heart.svg" alt="Heart icon" className="icon-heart icon" /> 
+          <img src="./icons/play.svg" alt="Play icon" className="icon-play icon" /> 
+          <img src="./icons/dots.svg" alt="Dots icon" className="icon-dots icon" /> 
+        </div>
       <a href={props.url} className="album-link">
-        <img className="album-img" src={props.image} alt="Album"/>
+        <img className="album-img" src={props.image} alt="Album" />
         <div className="artist-info">
           <h1 className="album-title">{props.name}</h1>
         </div>
@@ -13,3 +18,4 @@ export const Album = (props) => {
     </div>
   )
 }
+
