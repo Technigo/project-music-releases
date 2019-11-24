4d67877b2479d011a0797b8ae35de426c531802f
Clara Gyökeres <claragyokeres@Claras-Air.lan>
2019-11-24 22:25:39 +0100

    added icons and I am done

--- code/src/App.js
+++ code/src/App.js
@@ -14,7 +14,7 @@ export const App = () => {
       <Header />
       <section className='album-container'>
         {data.albums.items.map((item) => {
-          return <Album key={item.id} albumTitle={item.name} image={item.images[1].url} artists={item.artists[0].name} albumUrl={item.external_urls.spotify} />
+          return <Album key={item.id} albumTitle={item.name} image={item.images[1].url} artists={item.artists[0].name} urlArtistName={item.artists[0].external_urls.spotify} albumUrl={item.external_urls.spotify} />
         })}
       </section>
     </div >
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,16 +1,18 @@
 import React from 'react'
+import { Icons } from "components/Icons"
 
 export const Album = (props) => {
     return (
         <div className='artistBox'>
             <div className='album-cover'>
                 <img className='album-img' src={props.image} alt='Album' />
+                <Icons />
             </div>
             <div className='album-info'>
                 <div className='album-title'>
                     <a href={props.albumUrl}>{props.albumTitle}</a>
                     <div className='artist-name'>
-                        {props.artists}
+                        <a href={props.urlArtistName}>{props.artists}</a>
                     </div>
                 </div >
             </div >
