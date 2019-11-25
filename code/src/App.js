8899c0a441a81a3cc503a2cd9311d91fe6afbed0
JoacimNilsen <joacimnilsen1986@gmail.com>
2019-11-25 20:12:56 +0100

    made a header

--- code/src/App.js
+++ code/src/App.js
@@ -2,17 +2,18 @@ import React from 'react'
 import data from './data.json'
 import './app.css'
 import { Album } from './components/Album'
-// import { Header } from './components/Header'
-import { Artist } from './components/Artists'
-
-console.log(data)
 
 export const App = () => {
 	return (
-		<div className='contentWrapper'>
-			{data.albums.items.map(item => (
-				<Album key={item.id} item={item} />
-			))}
-		</div>
+		<React.Fragment>
+			<header>
+				<h1>New albums & singles</h1>
+			</header>
+			<div className='contentWrapper'>
+				{data.albums.items.map(item => (
+					<Album key={item.id} item={item} />
+				))}
+			</div>
+		</React.Fragment>
 	)
 }
