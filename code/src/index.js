e930dd90bf57c2a5292fd65a215b1cbce2b441db
ucha3i <jchabik@gmail.com>
2019-11-25 17:05:49 +0100

    fixed bugs

--- code/src/index.js
+++ code/src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
-import "./components/Artist.css";
+import './components/Artist.css';
 
 ReactDOM.render(<App />, document.getElementById('root'))
