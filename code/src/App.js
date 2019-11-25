2d203d49e9d995dfb10fffacfc474ccc6816db97
ucha3i <jchabik@gmail.com>
2019-11-25 22:22:59 +0100

    working...?

--- code/src/App.js
+++ code/src/App.js
@@ -4,20 +4,27 @@ import './app.css'
 import { Artist } from "./components/Artist";
 import { Album } from "./components/Album";
 
-
-const renderArtist = artist => {
-  const { id, name, external_urls } = artist;
-  return <Artist key={id} name={name} url={external_urls.spotify} />;
-};
-
 const renderAlbum = album => {
   const { id, images, name, external_urls } = album;
   return (
-    <div key={id} className="album_artist_container">
-      <Album image={images[1].url} name={name} url={external_urls.spotify} />
+    <div key={id} className="artist-container">
+      <Album 
+      image={images[1].url} 
+      name={name} 
+      url={external_urls.spotify} 
+      />
       {album.artists.map(renderArtist)}
     </div>
   );
+};
+
+const renderArtist = artist => {
+  const { id, name, external_urls } = artist;
+  return <Artist 
+  key={id} 
+  name={name} 
+  url={external_urls.spotify} 
+  />;
 };
 
 export const App = () => {
