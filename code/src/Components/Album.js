1b19b9647073306224242750e7e8a9b5cee1cb14
Disa de Beau <Disan@Disas-MacBook-Air.local>
2019-11-25 17:16:45 +0100

    All but the play-button

--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -4,15 +4,12 @@ import { ReactComponent as Heart } from "./heart.svg"
 import { ReactComponent as Play } from "./play.svg"
 import { ReactComponent as Dots } from "./dots.svg"
 
+
+
 export const Album = (props) => (
+
+
   <div className="albumInfo">
-    <a href={props.albumLink}>
-      <img className="albumCover" src={props.image} alt="album cover" />
-    </a>
-    <h1>{props.title}</h1>
-    <a href={props.artistLink}>
-      <h3>{props.artist}</h3>
-    </a>
     <div className="icons">
       <div className="heart-icon">
         <Heart />
@@ -24,6 +21,15 @@ export const Album = (props) => (
         <Dots />
       </div>
     </div >
+
+    <a href={props.albumLink}>
+      <img className="albumCover" src={props.image} alt="album cover" />
+    </a>
+    <h1>{props.title}</h1>
+    <a href={props.artistLink}>
+      <h3>{props.artist}</h3>
+    </a>
+
   </div>
 
 )
