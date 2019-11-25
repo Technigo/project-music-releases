f3d4efe0e14782c2ecd109fb7a0b9ded23090534
tehvicke <vikda574@student.liu.se>
2019-11-25 13:29:53 +0100

    Added react relevant items (I think) - next step is to CSS the page pretty.

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import data from './data.json'
 
-console.log(data)
-
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <section>
+      <h2>New albums and singles</h2>
+      <hr />
+      <Albums albums={data.albums.items}/>
+    </section>
+  )
+}
+
+export const Album = (props) => {
+  return (
+    <div class="album">
+      <img src={props.img} alt="Album"></img>
+      <p class="title">{props.title}</p>
+      <p class="artist">{props.artist}</p>
+    </div>
+  )
+}
+
+export const Albums = (props) => {
+  const AlbumList = props.albums.map((album) => <Album img={album.images[1].url} title={album.name} artist={album.artists[0].name} />
+  )
+  return (
+    <div class="album-holder" id="album-holder">
+      {AlbumList}
     </div>
   )
 }
