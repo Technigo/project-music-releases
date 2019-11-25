cf50293d3cde957476e669523bb288338c90e5d0
ucha3i <jchabik@gmail.com>
2019-11-25 21:35:16 +0100

    2.0

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -5,18 +5,20 @@ import { Icons } from './Icons'
 export const Album = props => {
   const { image, url, name } = props;
   return (
-    <div>
-      
-      
+    <div className="poster">
       <div className="album-cover">
-      <div className="album-icons">
-        <Icons />
-      </div>
+        </div>  
       
-      <img className="artist-image" src={image} alt="Album images" />
+    <div className="release">
+      <img className="artist-image" src={image} alt="images" />
+      <div className="album-icons">
+            <Icons />
+        </div>
       </div>
       
       <a href={url}> <h4>{name}</h4> </a>
     </div>
+
+    
   );
 };
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Album.css';
 
 export const Artist = props => {
     return (
