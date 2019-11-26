28a8fac73e638be45744a2e13a9020c1b7eba1e5
Disa de Beau <Disan@Disas-MacBook-Air.local>
2019-11-26 15:05:24 +0100

    playing aroung with icons

--- code/src/Components/Album.js
+++ code/src/Components/Album.js
@@ -8,18 +8,15 @@ import { ReactComponent as Dots } from "./dots.svg"
 
 export const Album = (props) => (
 
-
   <div className="albumInfo">
+    <a href={props.albumLink}>
+      <img className="albumCover" src={props.image} alt="album cover" />
+    </a>
+    <h1>{props.title}</h1>
+    <a href={props.artistLink}>
+      <h3>{props.artist}</h3>
+    </a>
 
-    <div className="albumCard">
-      <a href={props.albumLink}>
-        <img className="albumCover" src={props.image} alt="album cover" />
-      </a>
-      <h1>{props.title}</h1>
-      <a href={props.artistLink}>
-        <h3>{props.artist}</h3>
-      </a>
-    </div>
 
     <div className="icons">
       <div className="heart-icon">
