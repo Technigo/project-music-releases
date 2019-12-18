320689b690c5ef645547a48c0878fb7da559bd22
Linda <isell.linda@gmail.com>
2019-12-18 19:10:22 +0100

    fixed readme and links

--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -3,6 +3,6 @@ import './artist.css'
 
 export const Artist = (props) => {
   return (
-    <a className="artistInfo" href={props.item.external_urls.spotify}>{props.item.name}</a>
+    <a className="artistInfo" href={props.item.external_urls.spotify} target="_blank" rel="noopener noreferrer">{props.item.name}</a>
   )
 }
--- code/src/components/Sidebar.js
+++ code/src/components/Sidebar.js
@@ -4,11 +4,11 @@ import './sidebar.css'
 export const Sidebar = (props) => {
   return (
     <section className="playlist-card">
-      <img className="playlist-cover" src={props.playlist.images[0].url} alt="playlist cover" />
+      <a href={props.playlist.external_urls.spotify} target="_blank" rel="noopener noreferrer">  <img className="playlist-cover" src={props.playlist.images[0].url} alt="playlist cover" /></a>
       <a className="playlist-name"
-        href={props.playlist.external_urls.spotify}>
+        href={props.playlist.external_urls.spotify} target="_blank" rel="noopener noreferrer">
         {props.playlist.name}</a>
-      <a className="playlist-owner" href={props.playlist.owner.external_urls.spotify}>by {props.playlist.owner.display_name}</a>
+      <a className="playlist-owner" href={props.playlist.owner.external_urls.spotify} target="_blank" rel="noopener noreferrer">by {props.playlist.owner.display_name}</a>
     </section>
   )
 }
