63e3a52152a71fb3632593a18d6cf57edbe010cd
Lisas Sturegard <lisasturegard@hotmail.com>
2020-03-05 14:30:17 +0100

    First stage Project Music-Releases

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import data from './data.json'
+import React from 'react';
+import data from './data.json';
+import { Album } from './Components/Album';
 
 console.log(data)
 
 export const App = () => {
   return (
     <div>
+      {data.albums.items.map(song => {
+        return <Album key={song.id} albumTitle={song.name} />
+      })}
       Find me in src/app.js!
     </div>
   )
