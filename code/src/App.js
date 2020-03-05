b4780ff53d2fa4cff104d0838585089329fb41be
palhamel <pal.hamel@gmail.com>
2020-03-05 22:17:48 +0100

    Test array for data

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import data from './data.json'
 
-console.log(data)
+console.log(data.albums.items[0].name) // titel
+console.log(data.albums.items[0].artists[0].name) // artistnamn
+console.log(data.albums.items[0].images[1].url) // picture
+
+console.log(data.albums.items[0,1, 2, 3, 4].name)
+
+
+const Album = () => {
+  return (
+    <div>
+    {}
+    </div>
+  )
+}
+
 
 export const App = () => {
   return (
     <div>
+      
       Find me in src/app.js!
     </div>
   )
