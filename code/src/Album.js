e956b9edf4a164d3c3373cfe4d19042afbde9251
palhamel <pal.hamel@gmail.com>
2020-03-06 12:07:24 +0100

    Change alt text for image after error log

--- code/src/Album.js
+++ code/src/Album.js
@@ -4,7 +4,7 @@ export const Album = (props) => {
   console.log(props)
   return (
     <div className="Card">
-      <img src={props.image} alt="A small image here please"></img>
+      <img src={props.image} alt="Album artwork"></img>
       <p> Artist: {props.name} </p>
       <p> Title: {props.title} </p>
     </div>
