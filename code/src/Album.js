6fe8b6c474320dc34c1efea560c719e908a6bd60
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-03-07 08:50:28 +0100

    First structure done

---
+++ code/src/Album.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+//console.log(Album);
+//console.log(data.album.items[1]);
+
+export const Album = props => {
+  return (
+    <div>
+      <div className="album-cover">
+        <img className="album-img" src={props.image} alt="Album" />
+        <div className="icons">
+          <img className="icon-heart" src="icons/heart.svg" alt="Heart" />
+          <img className="icon-play" src="icons/play.svg" alt="Play" />
+          <img className="icon-dots" src="icons/dots.svg" alt="Dots" />
+        </div>
+      </div>
+
+      <div className="album-info">
+        <div className="album-title">
+          <a href={props.albumURL}>{props.albumTitle}</a>
+        </div>
+      </div>
+
+      <div className="album-info">
+        <div className="album-title">
+          <a href={props.albumURL}>{props.name}</a>
+        </div>
+        <div className="artist-name">
+          <a href={props.albumURL}>{props.artists}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import data from './data.json'
+import React from "react";
+import data from "./data.json";
+import { Album } from "./Album";
 
-console.log(data)
+console.log(data);
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <div>\</div>
+
+      <section className="album-container">
+        {data.albums.items.map(item => {
+          return (
+            <Album
+              key={item.id}
+              albumTitle={item.name}
+              albumURL={item.external_urls.spotify}
+              image={item.images[1].url}
+              playIconURL={item.external_urls.spotify}
+              artists={item.artists[0].name}
+              link={item.external_urls.spotify}
+              artists_link={item.artists[0].external_urls.spotify}
+            />
+          );
+        })}
+        ;
+      </section>
     </div>
-  )
-}
+  );
+};
