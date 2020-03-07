3ee9e3cdc4f0c737bfe2751ac97a6f45d61bf60f
Johanna Åstrand <johanna@Johannas-MBP.lan>
2020-03-07 15:24:47 +0100

    added right data and styling

--- code/src/App.js
+++ code/src/App.js
@@ -5,8 +5,23 @@ console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="App">
+      {data.albums.items.map(item => {
+        return <div key={item.id}>
+          <container className="img-wrapper">
+            <div className="icon-wrapper">
+              <img className="hover-image dots" src="icons/dots.svg" alt="hover-image"></img>
+              <img className="hover-image play" src="icons/play.svg" alt="hover-image"></img>
+              <img className="hover-image heart" src="icons/heart.svg" alt="hover-image"></img>
+            </div>
+            <img className="album-cover" src={item.images[0].url} alt="images"></img>
+          </container>
+
+          <h3><a href={item.external_urls.spotify}>{item.name}</a></h3> <p><a href={item.artists[0].external_urls.spotify}>{item.artists[0].name}</a> </p> </div>;
+      })}
     </div>
-  )
-}
+  );
+};
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
