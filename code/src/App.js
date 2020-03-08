d4dec84d40c537b23ea267cff17dda27899d95ad
Mathias Thorström <thisen@gmail.com>
2020-03-08 15:05:20 +0100

    created header and fixed styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import data from './data.json'
 import { Album } from './components/Album'
+import { Header } from './components/Header'
 
 console.log(data)
-console.log(data.albums.items[0].name)
-console.log(data.albums.items[0].artists[0].name)
-console.log(data.albums.items[0].images[1])
-console.log(data.albums.items[0].external_urls.spotify)
 
 export const App = () => {
   return (
-    <div className="cardContainer">
+    <div>
+      <Header />
 
-      {data.albums.items.map( (album) => {
-      return (
-      <Album AlbumImg={album.images[1]} AlbumUrl={album.external_urls.spotify} AlbumName={album.name} AlbumArtistUrl={album.artists[0].external_urls.spotify} AlbumArtist={album.artists[0].name} />
-      )
-    })}
+      <div className="cardContainer">
 
+        {data.albums.items.map( (album) => {
+        return (
+        <Album key={album.name} AlbumImg={album.images[1]} AlbumUrl={album.external_urls.spotify} AlbumName={album.name} AlbumArtistUrl={album.artists[0].external_urls.spotify} AlbumArtist={album.artists[0].name} />
+        )
+      })}
+
+      </div>
     </div>
   )
 }
---
+++ code/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <h3>New albums & singles</h3>
+    </div> 
+  )
+}
