d4e472ce69a41b2f396de9aa34c2309bcfaaea9b
Johanna Åstrand <johanna@Johannas-MBP.lan>
2020-03-08 14:12:49 +0100

    added icons, hover effects, styling

--- code/src/App.js
+++ code/src/App.js
@@ -7,21 +7,21 @@ export const App = () => {
   return (
     <div className="App">
       {data.albums.items.map(item => {
-        return <div key={item.id}>
-          <container className="img-wrapper">
-            <div className="icon-wrapper">
-              <img className="hover-image dots" src="icons/dots.svg" alt="hover-image"></img>
-              <img className="hover-image play" src="icons/play.svg" alt="hover-image"></img>
-              <img className="hover-image heart" src="icons/heart.svg" alt="hover-image"></img>
-            </div>
-            <img className="album-cover" src={item.images[0].url} alt="images"></img>
-          </container>
+        return <div key={item.id} className="Card">
+          <div className="Img-wrapper">
+            <div className="Icon-wrapper">
+              <img className="Hover-image heart" src="icons/heart.svg" alt="hover-image"></img>
+              <img className="Hover-image play" src="icons/play.svg" alt="hover-image"></img>
+              <img className="Hover-image dots" src="icons/dots.svg" alt="hover-image"></img>
 
-          <h3><a href={item.external_urls.spotify}>{item.name}</a></h3> <p><a href={item.artists[0].external_urls.spotify}>{item.artists[0].name}</a> </p> </div>;
+            </div>
+            <img className="Album-cover" src={item.images[0].url} alt="images"></img>
+          </div>
+          <h3><a href={item.external_urls.spotify}>{item.name}</a></h3> <p><a href={item.artists[0].external_urls.spotify}>{item.artists[0].name}</a> </p>
+        </div>;
       })}
     </div>
   );
 };
 
-//ReactDOM.render(<App />, document.getElementById('root'));
 
