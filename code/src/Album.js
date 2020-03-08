55e2b1073296c81016e1d55562865b02acb69797
palhamel <pal.hamel@gmail.com>
2020-03-08 11:35:08 +0100

    Start on CSS styling

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,12 +1,23 @@
 import React from 'react'
+
+import './album_style.css'
+
+
+
+
+
 // Album card
 export const Album = (props) => {
   console.log(props)
   return (
     <div className="Card">
-      <img src={props.image} alt="Album artwork"></img>
-      <p> Artist: {props.name} </p>
-      <p> Title: {props.title} </p>
+      <div className="album-container">
+        <img className="album-image" src={props.image} alt="Album artwork"></img>
+        <p className="song-title"> {props.title} </p>
+        <a className="artist-name"> {props.name} </a>
+      </div>
+      <div></div>
+      
     </div>
   )
 }
--- code/src/App.js
+++ code/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import data from './data.json'
 import { Album } from './Album'
 
+import './app_style.css'
+
 // console.log(data.albums.items[0].name) // Song title
 // console.log(data.albums.items[0].artists[0].name) // Artist name
 // console.log(data.albums.items[0].images[1].url) // Picture
@@ -19,7 +21,8 @@ const Album = () => {
 
 export const App = () => {
   return (
-    <div>
+    <div className="main-app-container">
+      <div className="album-container">
       {data.albums.items.map((album) => {
         return ( <Album key={album.artists[0].name} name={album.artists[0].name} 
           title={album.name}
@@ -27,6 +30,8 @@ export const App = () => {
           )
       })}
       {/* Find me in src/app.js! */}
+      </div>
+
     </div>
   )
 }
