de3dc53f7659b2daed1b58f6f996c9e41f0e3443
annesophiegendron <annesophie.gendron@gmail.com>
2020-03-09 13:51:43 +0100

    :tada:album component created

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import data from './data.json'
+import { Album } from './components/Album'
+import './App.css'
 
 console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="albumComponent">
+      {data.albums.items.map((item) => (
+        <Album
+          key={item.id}
+          albumTitle={item.name}
+          urlArtistName={item.artists[0].external_urls.spotify}
+          image={item.images[1].url}
+          artistName={item.artists[0].name}
+          urlAlbumTitle={item.external_urls.spotify} />
+      ))}
     </div>
   )
 }
