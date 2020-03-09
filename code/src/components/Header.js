7a98d5928d601ba9d28e1b240ef745c2e8f5fd3a
annesophiegendron <annesophie.gendron@gmail.com>
2020-03-09 22:42:04 +0100

    html file updated cleaned up

--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Header.css'
 
 export const Header = (props) => (
-  <div>
+  <div className="slide-right">
     <h1 className="pageTitle">New albums &amp; singles</h1>
   </div>
 )
