2e552278bb0922fe8c4aaa34ac5db62e8cb4b308
annesophiegendron <annesophie.gendron@gmail.com>
2020-03-10 10:18:05 +0100

    sidebar added, styling needs to be fixed

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import data from './data.json'
+import stretchgoal from './stretchgoal'
 import { Album } from './components/Album'
 import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar'
 import './App.css'
 
 console.log(data)
@@ -21,6 +23,19 @@ export const App = () => {
             image={item.images[1].url}
             artistName={item.artists[0].name}
             urlAlbumTitle={item.external_urls.spotify} />
+        ))}
+      </div>
+      <div className="sidebarComponent">
+        <div className="playlistTitle">
+          <h4>popular playlist</h4>
+        </div>
+        {stretchgoal.playlists.items.map((item) => (
+          <Sidebar
+            key={item.id}
+            playlistName={item.name}
+            urlPlaylistName={item.external_urls.spotify}
+            urlPlaylistImage={item.images[1]}
+          />
         ))}
       </div>
     </div>
--- code/src/components/Sidebar.js
+++ code/src/components/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import './Sidebar.css'
 
-export const Sidebar = () => (
-
-  
+export const Sidebar = (props) => (
+  <div className="sidebarWrapper">
+    <div className="sidebarContainer">
+      <div>
+        <img src={'props.urlPlaylistImage'} />
+      </div>
+      <div>
+        <a className="linkPlaylist" href={props.urlPlaylistName} target="_blank"><p className="playlist-name">{props.playlistName}</p>
+        </a>
+      </div>
+    </div>
+  </div>
 )
