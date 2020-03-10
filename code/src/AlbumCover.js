e8e31b12ce5b11abea974d6ab43b0bc555b293f0
Jihyun An <jihyunan42@gmail.com>
2020-03-10 21:54:50 +0100

    solved album cover issue

--- code/src/AlbumCover.js
+++ code/src/AlbumCover.js
@@ -5,11 +5,7 @@ export const AlbumCover = ({ bananaimage }) => {
   return (
 
     < div className="AlbumCover">
-      {
-        bananaimage.map(item => {
-          return <img src={bananaimage[1].url} alt="album-cover" />
-        })
-      }
+      <img src={bananaimage[1].url} alt="album-cover" />
     </div >
 
   )
