42bf8464cdb64753a31c5ef9fd3044cc82d35ce1
Ida Abrahamsson <idaabrahamsson@hotmail.com>
2020-04-08 17:26:21 +0200

    Finished the header and separator

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import data from './data.json'
+import { Play, Dots, Heart } from './icons/Icons'
+import { Header } from './components/Header'
 
 console.log(data)
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Header />
+      <hr className="separator"></hr>
     </div>
+
+
+
+    // <div>
+    //   <Play />
+    //   <Dots />
+    //   <Heart />
+
+    // </div>
   )
 }
---
+++ code/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>New albums & singles</h1>
+    </header>
+  )
+}
