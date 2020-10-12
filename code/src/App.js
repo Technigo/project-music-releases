dafab27339b7ee016b96816a49e28e8174ba4d45
Vanessa Sue Smith <sue27.vanessa@gmail.com>
2020-10-12 15:57:03 +0200

    Got album covers showing with album title

--- code/src/App.js
+++ code/src/App.js
@@ -10,17 +10,20 @@ const albumsArray = data.albums.items; //Extracts array with 50 album objects fr
 
 //console.log(albumsArray);
 
+//Here on App.js we extract the data from the json object via props and use those properties' names in the components
+
 export const App = () => {
   return (
     <>
       <Header />
-    <div className="albums-container">
-      {albumsArray.map((album) => {
-        return <Album key={album.name} 
-        name={album.name} 
-        />
-      })}
-    </div>
+      <div className="albums-container">
+        {albumsArray.map((album) => {
+          return <Album key={album.name} 
+          name={album.name}
+          imgsrc={album.images[0].url}
+          />
+        })}
+      </div>
     </>
   );
 };
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-export const Album = (props) => {
-  
+export const Album = (props) => {  
   return (
     <div className="album-card">
+        <div className="album-cover">
+            <img src={props.imgsrc} alt="" className="album-cover-img"/>
+        </div>
+
       <div>
         <h3>{props.name}</h3>
       </div>
     </div>
-  )
-}
+  );
+};
