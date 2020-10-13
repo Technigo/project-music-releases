9991eb0cd036458c0a0a6f5acedb10bf89156abc
Agnes Harrysson Hansson <aagneshansson@hotmail.com>
2020-10-13 23:18:58 +0200

    animation in progress

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import './album.css';
 import { Artist } from './Artist'
+import { Play } from './Play'
 
 // Körs en gång för varje varv i map, returnerar bild, artist coh album
 export const Album = props => {
-    return(
-        <article className='album' >
+    return (
+        <article className='album'>
             {/* ist för data.album... använder vi props som skickas med när <Album /> monteras i App.js */}
-            <img src={props.album.images[1].url}></img>
-            
+            <div className="image-container">
+                <img src={props.album.images[1].url}>
+                </img>
+                <div className="animation">
+                    <Play />
+                </div>
+            </div>
             <a href={props.album.artists[0].external_urls.spotify}>
-            <h1>
-                {props.album.artists.map(item => {
-                    return (
-                        <Artist key={item.id} item={item}/> 
-                    )
-                })}
-            </h1>
+
+                <h1>
+
+                    {props.album.artists.map(item => {
+                        return (
+                            <Artist key={item.id} item={item} />
+                        )
+                    })}
+                </h1>
             </a>
-            
+
             <a href={props.album.external_urls.spotify}>
                 <h2>{props.album.name}</h2>
             </a>
 
-        </article>       
+        </article>
     )
 }
 
