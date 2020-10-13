62b6a6dfc2cb552032e2f2fd306b8ce096d4a25c
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-13 17:06:54 +0200

    Added Musiccard.js which acts as the card for each of our 50 array items.

---
+++ code/src/Musiccard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Musicimage } from './card-component-directory/Musicimage';
+import { Musicalbum} from './card-component-directory/Musicalbum';
+import { Musicartist} from './card-component-directory/Musicartist';
+
+
+// Component for the music item which will house the album image, album title and artist name
+export const Musiccard = (prop) => {
+    return (
+        <div className="card-container">
+            {/* Here we're only getting one album, album name and artist name */}
+            <Musicimage image={prop.item.images}/>
+            <Musicalbum albumname={prop.item.name} albumlink={prop.item.external_urls}/>
+            <Musicartist artistsname={prop.item.artists}/>
+        </div>       
+    );
+};
+
+
