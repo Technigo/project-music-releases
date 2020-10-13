083967ad209dc7021586a4fcbf2242aa51d3a5b4
Rebkova <rebkova@gmail.com>
2020-10-13 19:07:53 +0200

    hover effect, artist names separated by comma

--- code/src/Album.js
+++ code/src/Album.js
@@ -6,27 +6,32 @@ import { ReactComponent as DotButton } from './icons/dots.svg';
 
 //props are passed into components like arguments are into function
 export const Album = (props) => {
+
+  //create array that converts the array of objects into an array with names only.
+  const artistsArr = props.artists.map(artist => artist.name);
+
+  //create a string from the array with commas between the names.
+  const artistsStr = artistsArr.join(", ");
+  console.log(artistsStr);
   return (
     <>
       <div className="album-container">
         <div className="image-container">
-          <img src={props.img[1].url} alt="Album cover" />
+          <img className="album-image" src={props.img[1].url} alt="Album cover" />
           <div className="icon-container">
             <HeartButton className="album-icon" />
-            <PlayButton className="album-icon" />
-            <DotButton className="album-icon" />
+            <PlayButton className="album-icon play-icon" />
+            <DotButton className="album-icon dots-icon" />
           </div>
         </div>
         <h2>
           {props.name}
         </h2>
         <p>
-          {props.artists.map(artist =>
-            <Artist
-              key={artist.name}
-              name={artist.name}
-            />
-          )}
+          <Artist
+            key={artistsStr}
+            artistNames={artistsStr}
+          />
         </p>
       </div>
     </>
--- code/src/Artist.js
+++ code/src/Artist.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Artist = (props) => {
   return (
-    <p>{props.name}</p>
+    <p>{props.artistNames}</p>
   )
 }
