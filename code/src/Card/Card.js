bb0b91bde37b2d9c0a60304a959ae97c00dae294
Sandra Shumanteva Kaevska <sandra@shumantevi.mk>
2020-10-13 14:44:25 +0200

    icon styling

--- code/src/Card/Card.js
+++ code/src/Card/Card.js
@@ -10,8 +10,25 @@ const Card = (props) => {
             alt="Picture of site about information Did you sleep well?"
           />
         </div>
-        <h1 className="card__title">{props.title}</h1>
-        <p className="card__info">{props.info}</p>
+        <div className="icon__container">
+          <img
+            className="heart-icon"
+            src="/icons/heart.svg"
+            alt="heart button"
+          />
+          <img
+            className="play-button"
+            src="/icons/play.svg"
+            alt="play button"
+          />
+          <img className="dots-icon" src="/icons/dots.svg" alt="dots-button" />
+        </div>
+        <a href="">
+          <h1 className="card__title">{props.title}</h1>
+        </a>
+        <a href="">
+          <p className="card__info">{props.info}</p>
+        </a>
       </div>
     </>
   );
