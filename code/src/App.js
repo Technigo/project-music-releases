913bd75de802ee6c99e86db62eba53296a489e16
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-13 16:57:55 +0200

    Updated App.js: 1. Edited app component with a main tag and h1 tag for the page title. 2. Added MusicList to the app component and passed the album object into the album prop to be then used in the Musiclist component. 3. Added the import Musiclist into the App.js.

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import data from './data.json'
+import React from 'react';
+import data from './data.json';
+import { Musiclist } from './Musiclist';
 
 console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <main>
+      <h1>New albums & singles</h1>
+      {/* Below accessing all album information & sending it as the prop albums to Musiclist */}
+      <Musiclist albums={data.albums} /> 
+    </main>     
+  );
+};
