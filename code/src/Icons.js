76da1adaca5493372b1be2e5819ae2669398bcfe
Emelie Svensson <emeliesv@live.se>
2020-10-13 21:57:08 +0200

    fixed icons

--- code/src/Icons.js
+++ code/src/Icons.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import './icons.css'
+import React from "react";
+import "./icons.css";
 
 const Icons = () => {
   return (
-    <div className="icon-box">
-      <img className="icon heart" src="../icons/heart.svg" alt="Heart icon" />
-      <img className="icon play" src="../icons/play.svg" alt="Play icon" />
-      <img className="icon dots" src="../icons/dots.svg" alt="Dots icon" />
-    </div>
+    <>
+      <div className="overlay">
+        <div className="icon-box">
+          <img
+            className="icon heart"
+            src="../icons/heart.svg"
+            alt="Heart icon"
+          />
+          <img className="icon play" src="../icons/play.svg" alt="Play icon" />
+          <img className="icon dots" src="../icons/dots.svg" alt="Dots icon" />
+        </div>
+      </div>
+    </>
   );
 };
 
