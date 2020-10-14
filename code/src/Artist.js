cb4ed8665029d503e3d2754738b0c68bec6c5639
Emelie Svensson <emeliesv@live.se>
2020-10-14 09:51:58 +0200

    worked on artists

--- code/src/Artist.js
+++ code/src/Artist.js
@@ -4,7 +4,7 @@ import './artist.css'
 /*We need to make a split-function here for artistName */
 
 const Artist = props => {
-  return <a className="artist-name" href={props.artistURL}>{props.artistName}</a>;
+    return <a className="artist-name" href={props.artistURL}>{props.artistName}</a>;  
 };
 
 export default Artist;
