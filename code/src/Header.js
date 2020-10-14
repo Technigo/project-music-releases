379ff777d8e53c4cd4b45d38352670b7372bde51
Karan Mann <62376840+karanmann@users.noreply.github.com>
2020-10-14 21:41:40 +0200

    Artist Text Fixed

--- code/src/Header.js
+++ code/src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
+import 'Header.css'
 
 export const Header = () => {
   return (
     <header className='header'>New Releases
-      <div className='hairline'></div>
     </header>
   )
 }
