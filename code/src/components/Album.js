ecaaecc4a0c310cf18e3cf5704a0d663e292ded0
Katsiez <katarinajovanovic.a@gmail.com>
2020-10-14 15:37:19 +0200

    Committing to Kat branch before we merge Aamndas into master

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -5,7 +5,7 @@ const Album = ({ item }) => {
     return (
         <div className="albumContainer">
             <div className="albumInfo">
-                <img src={item.images[1].url} alt="album cover" class="albumCover" />
+                <img src={item.images[1].url} alt="album cover" className="albumCover" />
                 <div className="icons">
                     <img src="icons/play.svg" alt="play button" className="playIcon" />
                     <img src="icons/heart.svg" alt="like button" className="heartIcon" />
