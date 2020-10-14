b639e88d002a05e644f36e35032ca614a3cead80
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2020-10-14 14:35:06 +0200

    cleaned the code

--- code/src/Albumcontainer.js
+++ code/src/Albumcontainer.js
@@ -3,20 +3,19 @@ import { Albumartistname } from './Albumartistname'
 import { Heart } from './Heart'
 import {Play} from './Play'
 import { Dots } from './Dots'
-// import data from  './data.json';
-// console.log(data.albums.items[0].name);
 
+const artistsWithComma = artists.join(', ');
 
 export const Albumcontainer = props => {
   console.log("We want album name:", props)
     return (
     <>
-    <a href={props.link}>
+    <a href={props.linkAlbum}>
       <img src={props.image} alt="Album cover" ></img>
     <div className="albumsname">{props.name}</div>
-             {/* // < Albumsname /> */}
+
     <div>
-      {props.artists.map (artist => {return <Albumartistname
+      {props.artistsWithComma.map (artist => {return <Albumartistname
       key={artist.id}
       name={artist.name}
       linkArtist={artist.external_urls.spotify} />
--- code/src/App.js
+++ code/src/App.js
@@ -1,26 +1,9 @@
 import React from 'react'
-// import { Albumsname } from './Albumsname'
-// import { Albumimage } from './Albumimage'
-// import { Albumartistname } from './Albumartistname'
-// import { Card } from './Card'
 import data from './data.json'
 import Header from './Header'
 import { Albumcontainer } from './Albumcontainer'
-import {Play} from './Play'
-// console.log(data.albums.items[0].name)
-// const name = data.albums.items[0].name
-// console.log(data.albums.items[0].images[0].url)
-// console.log(data.albums.items[0].external_urls.spotify)
 
-// console.log(data.albums.items[0].artists[0].name)
-// console.log(data.albums.items[0].artists[0].external_urls.spotify)
-
-// const shortarray = () => { data.slice(0,7);
-
-// console.log(shortarray);
-// }
-
-// shortarray();
+// const artistsWithComma = artists.join(', ');
 
 export const App = () => {
 
@@ -30,15 +13,12 @@ export const App = () => {
       <article>
         {data.albums.items.map(album=>{return <Albumcontainer key={album.id} 
         name={album.name} 
-        link={album.external_urls.spotify}
+        linkAlbum={album.external_urls.spotify} 
         image={album.images[0].url}
-        artists={album.artists} />
-        // icon = {}
+        artists={album.artists.join(', ')}
+         />
         })}
-        {/* <Play /> */}
         </article>
-      {/* < Card name={data.albums.items[0].name} image={data.albums.items[0].images[0].url} artistname={data.albums.items[0].artists[0].name}/> 
-      < Card name={data.albums.items[1].name} image={data.albums.items[1].images[0].url} artistname={data.albums.items[1].artists[0].name}/>  */}
     </>
   );
 }
