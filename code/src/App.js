dc55940e066700c474334d27de27eb969aa1bc02
AxelHammarback <axel.hammarback@gmail.com>
2020-10-14 21:44:07 +0200

    We're done

--- code/src/App.js
+++ code/src/App.js
@@ -9,15 +9,23 @@ console.log(data)
 export const App = () => {
     return (
       <main>
+        <img class="spotifake-logo" src={require(`./components/icons/spotifake_logo.png`)}/>
+        
         <Header />
         <div className="album-grid">
           {albumArray.map(album => (
             <AlbumCard 
+            key={album.artists[0].id}
             albumName={ album.name }
             artistName={ album.artists[0].name }
             image={ album.images[0].url }
-            />
+            albumURL={ album.external_urls.spotify }
+            artistURL={ album.artists[0].external_urls.spotify }
+            artists={ album.artists }
+            />  
+            
           ))}
+
         </div>
       </main>
     )
--- code/src/components/AlbumCard.js
+++ code/src/components/AlbumCard.js
@@ -1,22 +1,33 @@
-import React from "react";
+import React from 'react';
 
 export const AlbumCard = (props) => {
-  // console.log(props)
   return (
-    <div className="AlbumCard">
+    <div className="album-card">
       <div className="image-container">
         <div className="overlay">
           <div className="button-container">
-            <div className="button-small"><img src={require('./icons/heart.svg')} /> </div>
-            <div className="button-play"><img src={require('./icons/play.svg')} /></div>
-            <div className="button-small"><img src={require('./icons/dots.svg')} /></div>
+            <div className="button-small"><img alt={'Like icon'} src={require('./icons/heart.svg')} /> </div>
+            <div className="button-play"><img alt={'Play icon'} src={require('./icons/play.svg')} /></div>
+            <div className="button-small"><img alt={'More icon'} src={require('./icons/dots.svg')} /></div>
           </div>
         </div>
-        <img className="albumImage" src={props.image} alt={props.artistName} />
+        <img className="album-image" src={props.image} alt={props.artistName} />
       </div>
-      
-      <p className="album-text">{props.albumName}</p>
-      <p className="artist-text">{props.artistName}</p>
+      <a href={props.albumURL} target="_blank" rel="noopener noreferrer" className="album-text">{props.albumName}</a>
+
+      {/* Loops over each artist in the "artists" array  */}
+      {props.artists.map((artist) => {
+          console.log(artist)
+          return <a
+            key={artist.id}
+            className="artist-text"
+            href={artist.external_urls.spotify}
+            target="_blank"
+            rel="noopener noreferrer">
+            {artist.name}
+          </a>
+        })}
+
     </div>
   );
 };
