d7dfe4e2c3766e68f40e56ee03f6cb280f24964e
Moa Blomkvist <moa@birgerochblom.se>
2020-10-16 10:09:36 +0200

    changes

--- code/src/Album.js
+++ code/src/Album.js
@@ -7,21 +7,21 @@ import { Heart } from './Icons'
 const Album = props => {
   return (
     <article className="album-cards">
-      <a className="album-container" href={props.link} target="_blank">
-        <img className="album-image" src={props.images} alt="Image of album cover"></img>
+      <a className="album-container" href={props.link} target="_blank" rel="noopener noreferrer">
+        <img className="album-image" src={props.images} alt="image of album"></img>
         <div className="icon-container">
           <Heart />
           <Play />
           <Dots />
         </div>
       </a>
-      <a className="album-text" href={props.link} target="_blank">
+      <a className="album-text" href={props.link} target="_blank" rel="noopener noreferrer">
         {props.name}
       </a>
       <div className="artist-text-container">
         {props.artists.map(artist => {
           return (
-            <a className="artist-text" href={artist.external_urls.spotify} target="_blank">
+            <a className="artist-text" href={artist.external_urls.spotify} target="_blank" rel="noopener noreferrer">
               {artist.name}
             </a>)
         })}
