69b3566a16c6430bd4b0b156a0b7ca3b88eaebdb
Marjaana Tienaho <marjaanatienaho@Marjaanas-Air.lan>
2020-10-16 15:15:06 +0200

    Red level

--- code/src/Album.js
+++ code/src/Album.js
@@ -5,9 +5,9 @@ export const Album = props =>{
         <article className ="album-container">
           <div className="album-cover">
             <div className="hover-cover">
-              <img className="hover-icons"src="./icons/heart.svg" alt="like icon"></img>
-              <img className="play-icon" src="./icons/play.svg" alt="like icon"></img>
-              <img className="hover-icons"src="./icons/dots.svg" alt="like icon"></img>
+              <img className="hover-icons"src="./icons/heart.svg" alt="like button"></img>
+              <img className="play-icon" src="./icons/play.svg" alt="play button"></img>
+              <img className="hover-icons"src="./icons/dots.svg" alt="dots button"></img>
             </div>
             <img className="album-img"src={props.albumImg} alt="Album cover"></img>
           </div>
@@ -17,9 +17,11 @@ export const Album = props =>{
               if(index<props.albumArtist.length-2){
                 return <> <a href = {artist.external_urls.spotify} key={artist.name}> {artist.name}</a><span>, </span></>
               }
+              /** If the artist is last artist in the array do not add & or , **/
               else if(index === props.albumArtist.length-1){
                 return <a href = {artist.external_urls.spotify} key={artist.name}> {artist.name}</a>
               }
+              /** If the artist has index 1 in the array add & after artist name **/
               else if (index === props.albumArtist.length-2){
                 return <><a href = {artist.external_urls.spotify} key={artist.name}> {artist.name}</a><span> & </span></>
               }
