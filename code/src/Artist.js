f3abad85d2b0227eef1c5cee3f29373a08754bc8
emeliesv <emeliesv@live.se>
2020-10-16 13:55:12 +0200

    removed comments

--- code/src/Artist.js
+++ code/src/Artist.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './artist.css';
 
-/*We need to make a split-function here for artistName */
-
 const Artist = props => {
   if (props.index === 0) {
     return (
@@ -15,7 +13,6 @@ const Artist = props => {
     return (
       <a
         className="artist__name"
-        /* key={artist.id} */
         href={props.artistURL}
       >
         {'\u00A0 ' + ' & ' + props.artistName}
@@ -26,7 +23,6 @@ const Artist = props => {
     return (
       <a
         className="artist__name"
-        /* key={artist.id} */
         href={props.artistURL}
       >
         {', ' + props.artistName + ' '}
@@ -35,125 +31,3 @@ const Artist = props => {
   }
 };
 export default Artist;
-
-//   // if (props.length === 1)
-//   //   return (
-//   //     <a className="artist__name" href={props.artistURL}>
-//   //       {artist.name}
-//   //     </a>
-//   //   );
-//   // else {
-//   //   if (props.index < props.length - 1)
-//   //     return (
-//   //       <a className="artist__name" href={props.artistURL}>
-//   //         <span>{props.artistName}, </span>
-//   //       </a>
-//   //     );
-//   //   else
-//   //     return (
-//   //       <a className="artist__name" href={props.artistURL}>
-//   //         {props.artistName}
-//   //       </a>
-//   //     );
-//   // }
-// });
-
-/* Påbörjat för att kolla hur lång props-arrayen är. Om index = 0, returnera bara artist.name, om längre, returnera artist.name med komma om sista med & */
-// console.log(props.length);
-// console.log(props.index, props.artistName, props.length);
-// if (props.index === 1)
-//   return (
-//     <a
-//       className="artist__name"
-//       key={props.artistName}
-//       // href={props.external_urls.spotify}
-//     >
-//       {props.artistName}
-//     </a>
-//   ) else if (props.index > 1 )
-//   return (
-//     <a
-//       className="artist__name"
-//       key={props.artistName}
-//       // href={props.external_urls.spotify}
-//     >
-//       {props.artistName}
-//       {',\u00A0'}
-//     </a>
-//   )
-
-// if (props.index < 0)
-//   return (
-//     <a
-//       className="artist__name"
-//       key={artist.id}
-//       href={artist.external_urls.spotify}
-//     >
-//       {artist.name}
-//       {',\u00A0'}
-//     </a>
-//   );
-
-// if (props.artistsArray.length > 1) {
-//   return props.artistsArray.map(artist => {
-//     console.log(artist);
-//     return (
-//       <a
-//         className="artist__name"
-//         key={artist.id}
-//         href={artist.external_urls.spotify}
-//       >
-//         {artist.name}
-//         {',\u00A0'}
-//       </a>
-//     );
-//   });
-// }
-
-// return props.artistsArray.map((artist, index) => {
-//   console.log(artist, index);
-//   return (
-//     <a
-//       className="artist__name"
-//       key={artist.id}
-//       href={artist.external_urls.spotify}
-//     >
-//       {artist.name}
-//       {index < props.artistsArray - 1 ? ',\u00A0' : ''}
-//     </a>
-//   );
-// });
-
-/* Skriver ut namnen med komma emellan */
-
-// if (props.artistsArray.length === 1) {
-//   return props.artistsArray.map((artist, index) => {
-//     console.log(artist);
-//     return (
-//       <a
-//         className="artist__name"
-//         key={artist.id}
-//         href={artist.external_urls.spotify}
-//       >
-//         {artist.name}
-//       </a>
-//     );
-//   });
-// } else if (props.artistsArray.length > 1) {
-//   return props.artistsArray.map((artist, index) => {
-//     console.log(artist);
-//     return (
-//       <a
-//         className="artist__name"
-//         key={artist.id}
-//         href={artist.external_urls.spotify}
-//       >
-//         {artist.name}
-//         {/* {artist.name} {index < props.artistsArray.length - 1 ? ',\u00A0' : ''} */}
-//       </a>
-//     );
-//   });
-// }
-
-/* return (props.artistsArray.map(artist => {
-  console.log(artist); */
