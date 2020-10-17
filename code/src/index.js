5427b01c182b972b7dcf4ed598dcf242f7a53124
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-17 09:47:55 +0200

    Updated index.js file with a new comment

--- code/src/index.js
+++ code/src/index.js
@@ -4,4 +4,4 @@ import './index.css'
 import { App } from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-// Connecting the HTML element root into the react component file index.js and rendering the DOM
+// Connecting the HTML element root into the react component file index.js and rendering the virtual DOM
