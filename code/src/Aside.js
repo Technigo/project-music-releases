83f252c5a04bced65a1fa8ab7731c8dda525e226
Emelie Svensson <emeliesv@live.se>
2020-10-17 20:33:14 +0200

    added image-conatiner to playlist image

--- code/src/Aside.js
+++ code/src/Aside.js
@@ -5,11 +5,13 @@ export const Aside = props => {
   return (
     <section className="aside__section">
       <a className="aside__link" href={props.playlistURL}>
+        <div className="aside__image-container">
         <img
           className="aside__image"
           src={props.image}
           alt="Cover of playlist"
         />
+        </div>
       </a>
     </section>
   );
