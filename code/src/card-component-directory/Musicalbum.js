5c54442f65ff284fc25d63e1d10702f0a7b9c337
ClaireCaudwell <clairecaudwell@gmail.com>
2020-10-17 09:51:31 +0200

    Updated MusicAlbum.js with some new comments

--- code/src/card-component-directory/Musicalbum.js
+++ code/src/card-component-directory/Musicalbum.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Component for the music album which will fetch the album url and title
+// Component for the music album which will fetch the album name and url
 export const MusicAlbum = (prop) => {
     return (
         <a href={prop.albumlink.spotify} target="_blank" rel="noopener noreferrer">
@@ -8,3 +8,6 @@ export const MusicAlbum = (prop) => {
         </a>            
     );
 };  
+
+/* Above we're accessing both the spotfiy url and the album name that we already got from the array
+in the MusicCard */
