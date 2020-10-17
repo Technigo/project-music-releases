66d876c48e4394029699cfa6baf135c1cf4110a0
LillanEriksson <nailsbylillan@hotmail.com>
2020-10-17 19:49:34 +0100

    added noopener noreferrer to playlist-links

--- code/src/Playlists.js
+++ code/src/Playlists.js
@@ -3,7 +3,9 @@ import React from 'react';
 export const Playlists = (props) => {
 	return (
 		<div className="playlist-links">
-			<a href={props.playListUrl}>{props.playListName}</a>
+			<a href={props.playListUrl} target="_blank" rel="noopener noreferrer">
+				{props.playListName}
+			</a>
 		</div>
 	);
 };
