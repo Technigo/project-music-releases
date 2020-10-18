eae0c4066702e7ff8cc51847fb11dcb8285c2dc4
Karin Nordkvist <kontakt@karinnordkvist.se>
2020-10-18 16:37:42 +0200

    Changed construction of the Artist-component and added new function for comma/ampersand (full js)

--- code/src/Components/AlbumList.js
+++ code/src/Components/AlbumList.js
@@ -20,36 +20,21 @@ const AlbumList = () => {
               albumURL={album.external_urls.spotify}
             />
 
-            {/* Importing artist(s), separating commas and link to each artist */}
-            <Artist
-              names={album.artists.map((artist, i) => {
-                return (
-                  <span key={artist.id}>
-                    {/* Returning a complete HTML link with a data-path as href-element, 
-                    containing the artist name(s) */}
-                    <a
-                      href={artist.external_urls.spotify}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="artist-name--link"
-                    >
-                      {artist.name}
-                    </a>
-                    {/* Adding an ampersand between each artist if there are more than one,
-                    and a comma if there are more than 2. */}
-                    <span className="last-child-comma">
-                      {album.artists.length >= 2
-                        ? i === album.artists.length - 2
-                          ? ' & '
-                          : ', '
-                        : ''}
-                    </span>
-                  </span>
-                ); // Second return ends here
-              })}
-            />
+            {album.artists.map((artist, i) => {
+              return (
+                /* Importing artist(s), separating comma/ampersand and link to each artist */
+                <Artist
+                  key={artist.id}
+                  name={artist.name}
+                  id={artist.id}
+                  artistLink={artist.external_urls.spotify}
+                  amountOfArtists={album.artists.length}
+                  index={i}
+                />
+              );
+            })}
           </article>
-        ); // First return ends here
+        );
       })}
     </>
   );
--- code/src/Components/Artist.js
+++ code/src/Components/Artist.js
@@ -3,9 +3,30 @@ import React from 'react';
 const Artist = (props) => {
   return (
     <>
-      {/* p-tag with prop containing all available 
-      artists for each album */}
-      <p className="artist-name">{props.names} </p>
+      {
+        <>
+          {/* Returning a complete HTML link with a data-path as href-element, 
+                    containing the artist name(s) */}
+          <a
+            href={props.artistLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-name"
+          >
+            {props.name}
+          </a>
+
+          <span className="artist-text">
+            {props.amountOfArtists > 2 &&
+            props.index >= 0 &&
+            props.index < props.amountOfArtists - 2
+              ? ', '
+              : props.index === props.amountOfArtists - 2
+              ? ' & '
+              : ''}
+          </span>
+        </>
+      }
     </>
   );
 };
