88771e98db7aa65123c5ce330d41c65d3bda4877
Rebkova <rebkova@gmail.com>
2020-10-18 13:00:40 +0200

    added conditional for including ampersand

--- code/src/Artist.js
+++ code/src/Artist.js
@@ -5,11 +5,15 @@ export const Artist = (props) => {
   //determine if artist should have comma after or not
   if (props.length === 1)
     return <a href={props.url} target="_blank" rel="noopener noreferrer"><span className="artist-name">{props.name}</span></a>
+  else if (props.length === 2)
+    return <a href={props.url} target="_blank" rel="noopener noreferrer"><span className="artist-name">{props.name}
+      {props.index === 0 ? " & " : null}</span></a>
   else {
-    if (props.index < (props.length - 1))
-      return <a href={props.url} target="_blank" rel="noopener noreferrer"><span className="artist-name">{props.name} , </span></a>
+    if (props.index < (props.length - 2))
+      return <a href={props.url} target="_blank" rel="noopener noreferrer"><span className="artist-name">{props.name}, </span></a>
+    else if (props.index === (props.length - 2))
+      return <a href={props.url} target="_blank" rel="noopener noreferrer"><span className="artist-name">{props.name} &#38; </span></a>
     else
       return <a href={props.url} target="_blank" rel="noopener noreferrer"><span className="artist-name">{props.name}</span></a>
   }
-
 }
