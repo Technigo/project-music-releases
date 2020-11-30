fc751a3019e77d659c84ca5a1fb846b43532309e
Sabina de Maré <62811005+Snabbina@users.noreply.github.com>
2020-11-30 22:07:01 +0100

    fixed some extra w responsiveness

--- code/src/components/AlbumCard.js
+++ code/src/components/AlbumCard.js
@@ -4,24 +4,20 @@ import { ReactComponent as Heart } from "./Icons/Heart.svg";
 import { ReactComponent as Play } from "./Icons/Play.svg";
 import { ReactComponent as Dot } from "./Icons/Dots.svg";
 
-
 export const AlbumCard = (props) => {
   const { title, artists, image, albumLink } = props;
 
   return (
     <article className="album-card">
-    
       <div className="image-container">
         <img src={image} alt="Album cover" />
-    
-
-      <div className="icons-container">
-      <div className="icons">
-        <Heart className="heart" />
-        <Play className="play" />
-        <Dot className="dot" />
-      </div>
-      </div>  
+        <div className="icons-container">
+          <div className="icons">
+            <Heart className="heart" />
+            <Play className="play" />
+            <Dot className="dot" />
+          </div>
+        </div>
       </div>
       <a href={albumLink} target="_blank" rel="noopener noreferrer">
         <h1>{title}</h1>
@@ -29,8 +25,12 @@ export const AlbumCard = (props) => {
       {artists.map((artist, index) => {
         return (
           <div key={artist.name} index={index}>
-            <a href={artist.external_urls.spotify} target="_blank" rel="noopener noreferrer" >
-            {artist.name}
+            <a
+              href={artist.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {artist.name}
             </a>
           </div>
         );
