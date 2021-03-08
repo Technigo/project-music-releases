aa3fa74734021db4742b90fdb8d4472dff6802de
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-08 20:52:18 +0100

    added a map function and mounted the component

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import data from './data.json'
+import {Album} from "./components/Album"
 
 console.log(data)
+
+const albums = data.albums.items;
+console.log(albums)
 
 export const App = () => {
   return (
     <div>
       Find me in src/app.js!
+      { albums.map((album) => {
+        return <Album name={album.name}/>
+      }) }
     </div>
   )
 }
+
---
+++ code/src/components/Album.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Album = (props) => {
+
+
+
+return (
+  <div className="album">
+    <p> {props.name} </p>
+  </div>
+  )
+};
