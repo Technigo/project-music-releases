6e0938c9f16e9abfd22f15e4f4b0508fde769c84
Pauan8 <pauline.j.andersson@vgregion.se>
2021-03-08 18:15:58 +0100

    built the basic grid and added album covers + title + artist

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import data from './data.json'
+import {Album} from './Album'
 
 console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <>
+    <h1 className="header">New albums &amp; singles</h1>
+    <div className="container">
+      {data.albums.items.map(MapAlbum)}
     </div>
+    </>
   )
+}
+
+const MapAlbum = (item) => {
+  return <Album image = {item.images[0].url} albumName = {item.name} artist = {item.artists[0].name}/>
 }
