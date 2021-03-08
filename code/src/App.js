169e3c3d7886b062b286855c2ff0421e6f57e620
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-03-08 15:10:20 +0100

    Added files for components, and mapped over names

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import data from './data.json'
+import { Header } from './components/Header'
+import { Images } from './components/Images'
 
-console.log(data)
+console.log(data.albums.items[0].name)
+
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
+    return ( <
+        div >
+        <
+        Header / >
+
+        < Images />
+
+        <
+        /div>
+    )
 }
