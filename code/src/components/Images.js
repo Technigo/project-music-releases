05c32ec83c3050eca611883d350f4956fe498218
jess-stack <72085164+jess-stack@users.noreply.github.com>
2021-03-08 22:25:15 +0100

    Changed icons to white

--- code/src/components/Images.js
+++ code/src/components/Images.js
@@ -3,11 +3,11 @@ import React from "react";
 export const Images = props => {
     return (
     <div class="album-cover">
-        <img src={props.image} class="image" alt="album image" />
+        <img src={props.image} class="image" alt="album cover" />
         <div class="image-icons">
-            <img src="./icons/dots.svg" alt="dots" class="dot-icon"/>
-            <img src="./icons/play.svg" alt="play icon" class="play-icon" />
             <img src="./icons/heart.svg" alt="heart" class="heart-icon" />
+            <img src="./icons/play.svg" alt="play icon" class="play-icon" />
+            <img src="./icons/dots.svg" alt="dots" class="dot-icon"/>
         </div>
     </div>
     )
