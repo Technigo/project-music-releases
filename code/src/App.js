49ca93b97cce5b306a851c1cfcb79a9ba7ec7ae5
petercreations <hello@petercreations.com>
2021-03-09 11:37:22 +0100

    added fetch data from json data

--- code/src/App.js
+++ code/src/App.js
@@ -2,11 +2,15 @@ import React from 'react'
 import data from './data.json'
 
 console.log(data)
+console.log(data.albums.items[0].name)
+
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+       {data.albums.items[0].name}
+      {data.albums.items[0].artists[0].name}
+      <img src={data.albums.items[0].images[0].url} alt="" width="500" height="600"></img>
     </div>
   )
 }
