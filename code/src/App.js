e3b58330832f9d64be5cf6de47e11360609358e8
petercreations <hello@petercreations.com>
2021-03-09 14:50:55 +0100

    added components and album keys

--- code/src/App.js
+++ code/src/App.js
@@ -1,19 +1,50 @@
-import React from 'react'
-import data from './data.json'
-import { Album } from "./Album"
+// import React from 'react'
+// import data from './data.json'
+// import { Album } from "./Album"
 
-console.log(data)
-console.log(data.albums.items[0].name)
+// console.log(data)
+// console.log(data.albums.items[0].name)
 
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <Album />
+//       <h1> Test Info</h1>
+//        {data.albums.items[0].name}
+//       {data.albums.items[0].artists[0].name}
+//       <img src={data.albums.items[0].images[0].url} alt="" width="500" height="600"></img>
+//     </div>
+//   )
+// }
+
+
+import React from 'react';
+import data from './data.json';
+import { Album } from './components/Album';
+import { Header } from './components/Header';
+import './index.css';
+
+// Extracts array with 50 album objects from main data object
+const albumsArray = data.albums.items;
+
+/* Here on App.js we extract the data from the json object via props and use
+those properties' names in the components */
 
 export const App = () => {
   return (
-    <div>
-      <Album />
-      <h1> Test Info</h1>
-       {data.albums.items[0].name}
-      {data.albums.items[0].artists[0].name}
-      <img src={data.albums.items[0].images[0].url} alt="" width="500" height="600"></img>
-    </div>
-  )
-}
+    <>
+      <Header />
+      <div className="albums-container">
+        {albumsArray.map((album) => {
+          return <Album
+            key={album.name}
+            name={album.name}
+            imgsrc={album.images[0].url}
+            artists={album.artists}
+            albumurl={album.external_urls.spotify} />
+        })}
+      </div>
+    </>
+  );
+};
---
+++ code/src/components/Album.js
@@ -0,0 +1,49 @@
+// import React from 'react'
+
+// export const Album = () => {
+//     return (
+//       <div>
+//         Album Component
+//       </div>
+//     );
+//   };
+  
+  import React from 'react';
+
+  export const Album = (props) => {
+    return (
+      <div className="album-card">
+        <div className="album-cover">
+          <div className="icons">
+            <img className="heart-button" src="icons/heart.png" alt="" />
+            <img className="play-button" src="icons/play.png" alt="" />
+            <img className="dots-button" src="icons/dots.png" alt="" />
+          </div>
+          <a href={props.albumurl} className="album-img-hover" target="_blank" rel="noopener noreferrer">
+            <img src={props.imgsrc} alt="" className="album-cover-img" />
+          </a>
+        </div>
+  
+        <div>
+          <h3>{props.name}</h3>
+        </div>
+  
+        <div className="album-artists">
+          {props.artists.map((artist) => {
+            return <a
+              key={artist.id}
+              className="artists-link"
+              href={artist.external_urls.spotify}
+              target="_blank"
+              rel="noopener noreferrer">
+              {artist.name}
+              <span>,&nbsp;</span>
+            </a>
+          })}
+        </div>
+      </div>
+    );
+  };
+  
+
+  
