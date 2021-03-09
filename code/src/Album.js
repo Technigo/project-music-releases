7e50b5abbdc79275d957f2fb73d94a9511cc2b1e
Laima Duhovnaja <laima@laimas-mbp.mynet>
2021-03-09 16:37:36 +0100

    responsivness added

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
+
 const Album = (prop) => {
     
     return (
         <div className="albumCard">
-            <img className="albumImage" src={prop.image}/>
+            {/* <div class="imageAndIcon"> */}
+                <img className="albumImage" src={prop.image}/>
+                <div className="iconsHidden">
+                    <a className="heart"></a>
+                    <img/>
+                    <img/>
+                </div>
+            {/* </div> */}
+            
             <h2 className="albumTitle">{prop.title}</h2>
             <h3 className="artistName">{prop.artist}</h3>
         </div>     
