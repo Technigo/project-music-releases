f54088bdda099518b1e41471b85ed0f43cd83b8e
Sandra Wallén <sandrawallen_9@hotmail.com>
2021-03-09 11:23:26 +0100

    reworked artist and album componenents, currently styling the hover icons

--- code/src/App.js
+++ code/src/App.js
@@ -3,13 +3,17 @@ import data from './data.json'
 import Header from './components/Header'
 import Album from './components/Album'
 
-console.log(data)
-
 export const App = () => {
+  const AlbumItems = data.albums.items
+  console.log(AlbumItems)
   return (
     <>
       <Header />
-      <div className="album-wrapper">{Album}</div>
+      <div className="album-wrapper">
+        {AlbumItems.map(album => {
+          return <Album image={album.images[0].url} name={album.name} artist={album.artists} url={album.external_urls.spotify} />
+        })}
+      </div>
     </>
   )
 }
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -2,16 +2,38 @@ import React from 'react'
 import data from '../data.json'
 import Artist from './Artist'
 
-console.log(data)
+const Album = (props) => {
 
-const Album = data.albums.items.map((album) => {
+  console.log(props)
+
   return (
     <div className="album-card">
-      <img className="album-image" src={album.images[0].url} alt={album.name} />
-      <p className="album-name">{album.name}</p>
-      <Artist />
+      <div className="icons">
+        <img className="icon heart" src="/../../public/icons/heart.svg"></img>
+        <img className="icon play" src="/code/public/icons/play.svg"></img>
+        <img className="icon dot" src="/code/public/icons/dots.svg"></img>
+      </div>
+      <img className="album-image" src={props.image} alt={props.name} />
+      <a className="album-name" href={props.url}>
+        <p>{props.name}</p>
+      </a>
+      
+      {props.artist.map(artist => {
+        return <Artist key={artist.id} name={artist.name} url={artist.external_urls.spotify}/>
+      })}
+
     </div>
   )
-  })
+}
+
+// const Album = data.albums.items.map((album) => {
+//   return (
+//     <div className="album-card">
+//       <img className="album-image" src={album.images[0].url} alt={album.name} />
+//       <p className="album-name">{album.name}</p>
+//       <Artist />
+//     </div>
+//   )
+//   })
 
 export default Album
