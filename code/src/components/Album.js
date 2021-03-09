340d49bc6d4b152980cbd388e15332c799110123
Sandra Wallén <sandrawallen_9@hotmail.com>
2021-03-09 16:54:14 +0100

    styling done for mobile, tablet and desktop. Stuck att rendering links for each artist

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -8,32 +8,32 @@ const Album = (props) => {
 
   return (
     <div className="album-card">
-      <div className="icons">
-        <img className="icon heart" src="/../../public/icons/heart.svg"></img>
-        <img className="icon play" src="/code/public/icons/play.svg"></img>
-        <img className="icon dot" src="/code/public/icons/dots.svg"></img>
+      <div className="album-cover">
+        <div className="icons">
+          <img className="icon heart" src="/icons/heart.svg"></img>
+          <img className="icon play" src="/icons/play.svg"></img>
+          <img className="icon dot" src="/icons/dots.svg"></img>
+        </div>
+        <img className="album-image" src={props.image} alt={props.name} />
       </div>
-      <img className="album-image" src={props.image} alt={props.name} />
       <a className="album-name" href={props.url}>
         <p>{props.name}</p>
       </a>
       
-      {props.artist.map(artist => {
-        return <Artist key={artist.id} name={artist.name} url={artist.external_urls.spotify}/>
-      })}
-
-    </div>
+      <div>
+        <Artist
+          artists={props.artist}
+        />
+      </div>
+      </div>
   )
 }
 
-// const Album = data.albums.items.map((album) => {
-//   return (
-//     <div className="album-card">
-//       <img className="album-image" src={album.images[0].url} alt={album.name} />
-//       <p className="album-name">{album.name}</p>
-//       <Artist />
-//     </div>
-//   )
-//   })
+
 
 export default Album
+
+{/* </div>
+      {props.artist.map(artist => {
+        return <Artist key={artist.id} name={artist.name} url={artist.external_urls.spotify}/>
+      })} */}
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -3,13 +3,22 @@ import data from '../data.json'
 
 
 const Artist = (props) => {
-  console.log(props)
-  return (
-    <a className="artist-name" href={props.url}>
-      <p>{props.name}</p>
-    </a>
-  )
-}
+  console.log(props.artists[0].name)
+  const Artists = props.artists.map(artist => {
+    return <a key={artist.id} href={artist.external_urls.spotify}>{artist.name}</a>
+  });
+
+
+
+  return Artists.join(", ")
+  
+};
+
+{/* <a className="artist-name" href={artist.external_urls.spotify}>
+<span>{artist.name}</span>
+</a> */}
+
+//Artists.length ? Artists.join(", ") + "&" + LastArtist : LastArtist
 
 // const FinalArtist = `& ${props[-1].name}`
 // if (props.lenght > 1) {
