04810f72c898d5c36871d1d3dc87b51d3ded5cfc
petercreations <hello@petercreations.com>
2021-03-09 13:43:02 +0100

    linked component file

--- code/src/App.js
+++ code/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import data from './data.json'
+import { Album } from "./Album"
 
 console.log(data)
 console.log(data.albums.items[0].name)
@@ -8,6 +9,8 @@ console.log(data.albums.items[0].name)
 export const App = () => {
   return (
     <div>
+      <Album />
+      <h1> Test Info</h1>
        {data.albums.items[0].name}
       {data.albums.items[0].artists[0].name}
       <img src={data.albums.items[0].images[0].url} alt="" width="500" height="600"></img>
