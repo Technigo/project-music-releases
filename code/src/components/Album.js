e0239bbee6855dad6476ecdd9def93f2240f6b99
Sandra Wallén <sandrawallen_9@hotmail.com>
2021-03-10 14:34:43 +0100

    Completed if statement for delimiter signs in between artists

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -8,7 +8,6 @@ const Album = (props) => {
 
   return (
     <div className="album-card">
-<<<<<<< HEAD
       <div className="album-cover">
         <div className="icons">
           <img className="icon heart" src="/icons/heart.svg"></img>
@@ -17,25 +16,16 @@ const Album = (props) => {
         </div>
         <img className="album-image" src={props.image} alt={props.name} />
       </div>
-=======
-      <div className="icons">
-        <img className="icon heart" src="/../../public/icons/heart.svg"></img>
-        <img className="icon play" src="/code/public/icons/play.svg"></img>
-        <img className="icon dot" src="/code/public/icons/dots.svg"></img>
-      </div>
-      <img className="album-image" src={props.image} alt={props.name} />
->>>>>>> f54088bdda099518b1e41471b85ed0f43cd83b8e
       <a className="album-name" href={props.url}>
         <p>{props.name}</p>
       </a>
       
-<<<<<<< HEAD
       <div>
         <Artist
           artists={props.artist}
         />
       </div>
-      </div>
+    </div>
   )
 }
 
@@ -47,24 +37,3 @@ export default Album
       {props.artist.map(artist => {
         return <Artist key={artist.id} name={artist.name} url={artist.external_urls.spotify}/>
       })} */}
-=======
-      {props.artist.map(artist => {
-        return <Artist key={artist.id} name={artist.name} url={artist.external_urls.spotify}/>
-      })}
-
-    </div>
-  )
-}
-
-// const Album = data.albums.items.map((album) => {
-//   return (
-//     <div className="album-card">
-//       <img className="album-image" src={album.images[0].url} alt={album.name} />
-//       <p className="album-name">{album.name}</p>
-//       <Artist />
-//     </div>
-//   )
-//   })
-
-export default Album
->>>>>>> f54088bdda099518b1e41471b85ed0f43cd83b8e
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -3,68 +3,31 @@ import data from '../data.json'
 
 
 const Artist = (props) => {
-  console.log(props.artists[0].name)
-  const Artists = props.artists.map(artist => {
-    return <a key={artist.id} href={artist.external_urls.spotify}>{artist.name}</a>
-  });
+  console.log(props.artists)
+  console.log(props.artists.length)
 
-
-
-  return Artists.join(", ")
+  const generateDelimiter = (index, length) => {
+    if (index === length - 2) {
+      return " & "
+    } else if (index < length - 2) {
+      return ", "
+    } else {
+      return
+    }
+  }
   
-};
-
-{/* <a className="artist-name" href={artist.external_urls.spotify}>
-<span>{artist.name}</span>
-</a> */}
-
-//Artists.length ? Artists.join(", ") + "&" + LastArtist : LastArtist
-
-// const FinalArtist = `& ${props[-1].name}`
-// if (props.lenght > 1) {
-// return (
-// <a href={props.url}>
-//   <p>{props[0].name} & {props[1].name} </p>
-// </a>)
-// } else if (props.lenght > 2) {
-//   return ( 
-//   <a href={props.url}>
-//   <p>{props.join(', ') && {FinalArtist}}</p>
-// </a>)
-// } else {
-//   return (
-//   <a href={props.url}>
-//   <p>{props.name}</p>
-// </a>)
-// }
-
-// const ArtistArray = data.albums.items.map((item) => {
-//   return item.artists
-// })
-/*
-const ArtistArrayMap = ArtistArray.map((artistt) => {
-  return artistt.name
-})
-
-console.log(ArtistArray)
-console.log(ArtistArrayMap)*/
-
-// const Artist = (props) => {
-
-//   return (
-    
-//   )
-// 
-
-// const Artist = ArtistArray.map((artist) => {
-//   console.log(artist)
-//   return (
-//       <p className="artist-name">{artist[0].name}</p>
-//   )
-//   })
-
-//   const ArtistArray = data.album.items.map((item) => {
-//     return item.map 
-//   })
+   return props.artists.map((artist, index) => {
+      return (
+        <span className="delimiter">
+          <a className="artist-name" href={artist.external_urls.spotify}>
+          {artist.name}</a>{generateDelimiter(index, props.artists.length)}
+        </span>
+        )
+    }) 
+  
+}
+  
 
 export default Artist
+
+
