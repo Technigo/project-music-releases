c45a4a369f4f4591a5fbed745140ae7a8018b113
Sari-Anne Peltola <sarianne.peltola@gmail.com>
2021-03-10 18:39:05 +0100

    Styling and hover

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -6,11 +6,18 @@ const Album = (props) => {
   return (
     <div className="album">
       <a href={props.albumContent.external_urls.spotify}>
-        <img className="cover" src={props.albumContent.images[1].url} alt="cover" />
+        <div className="cover-wrapper">
+          <img className="cover" src={props.albumContent.images[1].url} alt="cover" />
+          <div className="icons">
+            <img className="icon" src={require("./heart.svg")} alt="favorite icon" />
+            <img className="icon" src={require("./play.svg")} alt="play icon" />
+            <img className="icon" src={require("./dots.svg")} alt="dots icon" />
+          </div>
+        </div>
         <h3>{props.albumContent.name}</h3>
       </a>
       <div>{props.albumContent.artists.map((name) => {
-        return <a href={name.external_urls.spotify}><div key={name.name}>{name.name}</div></a>
+        return <a className="artist-container" href={name.external_urls.spotify}><span>{name.name}</span></a>
         })}</div>
     </div>
   )
