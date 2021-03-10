81564cab47cba3e0c60b37fa95455c8ba4d27f9c
Sara Carlstein <carlstein.sara@gmail.com>
2021-03-10 17:49:27 +0100

    styling

--- code/src/App.js
+++ code/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import data from './data.json'
 import Album from './components/Album'
+import Header from './components/Header'
 
 // console.log(data)
 
 export const App = () => {
   return (
     <div>
+      <Header />
       <Album data={data}/>
-      Find me in src/app.js!
     </div>
   )
 }
---
+++ code/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+    return(
+        <div className="main-wrapper">
+            <h1 className="main-header">New albums & singles </h1>
+        </div>
+    )
+}
+
+export default Header
