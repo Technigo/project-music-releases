0bf6728224bc236e98d0e5123bdd168dcb4f785e
katliebig <liebig.kat@gmail.com>
2021-03-10 10:10:41 +0100

    added logic for displaying more than one artists

--- code/src/components/InfoArtist.js
+++ code/src/components/InfoArtist.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 export const InfoArtist = (props) => {
-    if (props.artist.length === 1) {
-        return <a className="infoArtist" href={props.artistURL} target="_blank" >{props.artist[0].name}</a>
-    } else if (props.artist.length === 2) {
-        return <a className="infoArtist" href={props.artistURL} target="_blank" >{props.artist[0].name} & {props.artist[1].name}</a>
-    } else {
-        return <a className="infoArtist" href={props.artistURL} target="_blank" >{props.artist[0].name}, {props.artist[1].name} & {props.artist[2].name}</a>
+    const firstArtist = props.artist[0].name;
+    const firstArtistURL = props.artist[0].external_urls.spotify
+    const lastArtist = props.artist[props.artist.length - 1].name
+    const lastArtistURL = props.artist[props.artist.length - 1].external_urls.spotify
+    const middleArtists = props.artist.slice(1, -1)
+
+    let allArtists = [<a className='infoArtist' href={firstArtistURL} target='_blank'>{firstArtist}</a>]
+
+    for (let artist of middleArtists) {
+        allArtists.push(", ")
+        allArtists.push(<a className='infoArtist' href={artist.external_urls.spotify} target='_blank'>{artist.name}</a>);
     }
+
+    if (props.artist.length > 1) {
+        allArtists.push(" & ")
+        allArtists.push(<a className='infoArtist' href={lastArtistURL} target='_blank'>{lastArtist}</a>)
+    }
+
+    return <p> {allArtists} </p>
 }
