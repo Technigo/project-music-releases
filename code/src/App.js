a8051877c398b04fbbb121ac08f3592f6e67287c
Pauan8 <pauline.j.andersson@vgregion.se>
2021-03-11 17:10:49 +0100

    styling and singles + album sections added

--- code/src/App.js
+++ code/src/App.js
@@ -1,57 +1,54 @@
-import React from 'react'
-import playlist from './playlist.json'
-import data from './data.json'
-import {Album} from './components/Album'
-import {Artists} from './components/Artists'
-import {Sidebar} from './components/Sidebar'
-console.log(playlist)
-console.log(data)
+import React from "react";
+import playlist from "./playlist.json";
+import data from "./data.json";
+import { Album } from "./components/Album";
+import { Artists } from "./components/Artists";
+import { Sidebar } from "./components/Sidebar";
 
 export const App = () => {
+  const mapMusic = (type) => {
+    return data.albums.items
+      .filter((item) => item.album_type === type)
+      .map((item) => {
+        return (
+          <Album
+            key={item.name}
+            image={item.images[0].url}
+            albumName={item.name}
+            albumLink={item.external_urls.spotify}
+            artist={item.artists.map((artist) => (
+              <Artists
+                key={artist.name}
+                name={artist.name}
+                url={artist.external_urls.spotify}
+              />
+            ))}
+          />
+        );
+      });
+  };
+
   return (
     <>
-    <h1 className="header">New albums &amp; singles</h1>
-
-    <div className='main'>
-      <div className='sidebar'>
-        <h3 className='playlist'>Playlists</h3> 
-          {playlist.playlists.items.map(item => MapLists(item))} 
+      <h1 className="header">New albums &amp; singles</h1>
+      <div className="main">
+        <div className="sidebar">
+          <h3 className="playlist">Playlists</h3>
+          {playlist.playlists.items.map((list) => (
+            <Sidebar
+              key={list.id}
+              list={list.name}
+              link={list.external_urls.spotify}
+            />
+          ))}
         </div>
-      <div className="container">
-        {data.albums.items.map(MapAlbum)}
+        <div class="container">
+          <h4 className="singles">Singles</h4>
+          <div className="singles-container">{mapMusic("single")}</div>
+          <h4 className="album">Albums</h4>
+          <div className="albums-container">{mapMusic("album")}</div>
+        </div>
       </div>
-     </div> 
     </>
-  )
-}
-
-const MapAlbum = (item) => {
-  return (
-  <Album 
-    key = {item.name} 
-    image = {item.images[0].url} 
-    albumName = {item.name} 
-    albumLink = {item.external_urls.spotify}
-    artist = {item.artists.map(artist => MapArtists(artist))}
-  />)
-}
-
-const MapArtists = (artist) => {
-  return (
-    < Artists 
-      key = {artist.name} 
-      name = {artist.name} 
-      url = {artist.external_urls.spotify} 
-    />
-  )
-}
-
-const MapLists = (list) => {
-  return (
-    < Sidebar
-      key = {list.id}
-      list = {list.name}
-      link = {list.external_urls.spotify}
-    />  
-  )
-}
+  );
+};
