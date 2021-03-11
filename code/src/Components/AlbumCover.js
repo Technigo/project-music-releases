2172f9ac20d3087aa67653f59c5fee7a1044b2ef
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-03-11 18:18:50 +0100

    added alt text

--- code/src/Components/AlbumCover.js
+++ code/src/Components/AlbumCover.js
@@ -31,9 +31,9 @@ const AlbumCover = (props) => {
         <img src={props.item.images[1].url} alt="Album cover"></img>
 
           <div className="icons">
-              <img className="icon-heart" src= "./icons/heart.svg"></img>
-              <img className="icon-play" src= "./icons/play.svg"></img>
-              <img className="icon-dots" src= "./icons/dots.svg"></img>
+              <img className="icon-heart" src= "./icons/heart.svg" alt="heart icon"></img>
+              <img className="icon-play" src= "./icons/play.svg" alt="play-icon"></img>
+              <img className="icon-dots" src= "./icons/dots.svg" alt="dots-icon"></img>
           </div>
 
       </div>
