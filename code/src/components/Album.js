5695a442ebd5c5513216b95d6b9367d777cb2ea4
Rasmus Persson <rrasmuspersson@gmail.com>
2021-03-11 10:34:49 +0100

    added more css. Added play, heart and dots icons and positioning/animations/hover effects.

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -4,7 +4,16 @@ export const Album = (props) => {
 
 return (
   <div className="album-card">
-    <img className="album-image" src={props.image} alt="Album cover"/>
+    
+    <div className="album-cover">
+      <img className="album-image" src={props.image} alt="Album cover"/>
+    
+    <div className="icons">
+      <img className="heart" src={require("../images/heart.svg")} alt="like icon"/>
+      <img className="play" src={require("../images/play.svg")} alt="play icon"/>
+      <img className="dots" src={require("../images/dots.svg")} alt="menu icon"/>
+    </div>
+    </div>
     <p className="album-title"> {props.name} </p>
     <p className="album-artist"> {props.artists} </p>
   </div>
