6c1953b541048564494d8eaceb1e5d8d097384dd
Martin <martno15@gmail.com>
2021-03-11 16:48:40 +0100

    added icons and attempted spacing them
    Co-authored-by: tuttibalutti <tuttibalutti@users.noreply.github.com>

--- code/src/Album.js
+++ code/src/Album.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Artist } from './Artist'
 
-const Album = (props) => {
+const Album = (props) => {  //for albumArt: style={{ backgroundImage: `url(${props.image})` }}
     return (
         <div className="album">
-            <div className="albumArt">
+            <div className="albumArt" >
                 <img src={props.image}></img>
+                <div className="icons">
+                    <img src="icons/heart.svg"></img>
+                    <img src="icons/play.svg"></img>
+                    <img src="icons/dots.svg"></img>
+                </div>
             </div>
+            
+            
+
             <div className="albumName">
                 <a href={props.albumUrl}>{props.albumName}</a>
             </div>
