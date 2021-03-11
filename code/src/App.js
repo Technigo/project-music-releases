b38aae30e2a0842431186497f4d0c0774d01eba5
Ingetora <maria.bergman@outlook.com>
2021-03-11 17:38:25 +0100

    Testing React

--- code/src/App.js
+++ code/src/App.js
@@ -4,9 +4,21 @@ import data from './data.json'
 console.log(data)
 
 export const App = () => {
+  const exampleHeading = 'Welcome to my website!';
+  const imageSrc = 'https://via.placeholder.com/150';
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <>
+        <header>
+            <h1>Article website</h1>
+        </header>
+        <div>
+            <h3>{exampleHeading}</h3>
+            <p>Spring is coming!</p>
+            <img src={imageSrc} alt="Spring" />      
+        </div>
+    </>
   )
 }
+
+export default App;
