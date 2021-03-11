8b4cca970022d66aacf04dd71d074e25cf26ed3c
NoemiSalome <noemifrischknecht@macbook-pro.home>
2021-03-12 00:32:52 +0100

    flexbox for desktop, added icons for tablet and desktop on albumcovers (hovereffect missing)

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -6,9 +6,17 @@ const Album = (props) => {
         <>
         <div className="artistcard-container">
            <div className="artistcard">
-                <a href={props.albumContent.external_urls.spotify}>
-                    <img src={props.albumContent.images[1].url} className="albumimg" alt="albumcover" />
-                </a>
+               <div className="albumcover">
+                    <a href={props.albumContent.external_urls.spotify}>
+                        <img src={props.albumContent.images[1].url} className="albumimg" alt="albumcover" />
+                        <div className="hover-icons">
+                            <img src="../icons/heart.svg" class="icons" alt="heart-icon"></img>
+                            <img src="../icons/play.svg" class="icons" alt="play-icon"></img>
+                            <img src="../icons/dots.svg" class="icons" alt="dot-icon"></img>
+                        </div>
+                    </a>
+                </div>
+
                 <div className="artistinfo">
                     <div className="artistname">
                         <a className="artistlink" href={props.albumContent.artists[0].external_urls.spotify}>
