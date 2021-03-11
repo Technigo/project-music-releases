081826e6dc7f61ddbc83f9de0cac4fddd03bc70d
[Darcy Winterhill] <[darcy.winterhill@gmail.com]>
2021-03-11 11:08:10 +0100

    README and cleaned up file

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -8,17 +8,27 @@ export const Album = (props) => {
         <a href={props.albumLink} target='_blank' rel="noopener noreferrer">
           <div className="cover-container">
             <img src={props.cover} alt='album cover' className="cover" />
+            
             <div className="icons-container">
               <img className="heart-icon" src="/icons/heart.svg" alt='' />
               <img className="play-icon" src="/icons/play.svg" alt='' />
               <img className="dots-icon" src="/icons/dots.svg" alt='' />
             </div>
           </div>
-          <h2 className="album-title"> {props.name} </h2>
-        </a>
-        <a className="artist_container" href={props.artistLink} target='_blank' rel="noopener noreferrer">
-          <h2 className="artist-name"> {props.artists}</h2>
-        </a>
+          </a>
+          
+          <div className="album-artist-container">
+          <h2>
+            <a className="album-link" href={props.albumLink} target='_blank' rel="noopener noreferrer">
+              {props.name}
+            </a>
+          </h2>
+          <h2 className="artist-name">
+            <a className="artist-link" href={props.artistLink} target='_blank' rel="noopener noreferrer">
+              {props.artists}
+            </a>
+          </h2>
+        </div>
        </div>
     )
   }
