50c382cba0d9b455129e288f4545210ee169ce47
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-03-12 18:52:23 +0100

    image added

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -3,13 +3,16 @@ import React from 'react'
 const Album = (props) => {
     return (
         <div>
-            {props.album.map((artist) => {
+            {props.album.map((album) => {
             return (
-                <a href={artist.external_urls.spotify} key={artist.id}>
-                <h5 className="artist-name">
-                    {artist.name}
-                </h5>
+                <>
+                <a href={album.external_urls.spotify} key={album.id}>
+                    <img src={album.images[0].url} ></img>
+                    <h5 className="artist-name">
+                     {album.name}
+                    </h5>
                 </a>
+                </>
             )
             })}
         </div>
