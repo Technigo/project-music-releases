e53ebb3b26c735a5117e593c104ec361bb29092d
katliebig <liebig.kat@gmail.com>
2021-03-12 10:32:10 +0100

    updated readme, added tabindex to headings, fixed scrollbar for firefox

--- code/src/components/CategoryHeading.js
+++ code/src/components/CategoryHeading.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const CategoryHeading = (props) => {
     return (
-        <h1 className="category-heading">{props.heading}</h1>
+        <h1 className="category-heading" tabindex="0" >{props.heading}</h1>
     )
 }
