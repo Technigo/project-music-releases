58a46c0cc015d9f9556c30b6af3fd98b307b1cc3
Sofarz <72409381+Sofarz@users.noreply.github.com>
2021-03-12 13:42:56 +0100

    Add separator with css between artists, fixed indentation, removed comments.

--- code/src/components/AlbumCard.js
+++ code/src/components/AlbumCard.js
@@ -8,9 +8,9 @@ const AlbumCard = (props) => {
             <img className="album-cover" src={props.albumContent.images[0].url} alt="album cover"/>
             <div className="image-overlay"> 
                 <div className ="icon-container">
-                    <img className="heart-icon" src="./icons/heart.svg" alt="heart icon"></img>
-                    <img className="play-icon" src="./icons/play.svg" alt="play icon"></img>
-                    <img className="dots-icon" src="./icons/dots.svg" alt="dots icon"></img>
+                    <img className="heart-icon" src="./icons/heart.svg" alt="heart icon" />
+                    <img className="play-icon" src="./icons/play.svg" alt="play icon" />
+                    <img className="dots-icon" src="./icons/dots.svg" alt="dots icon" />
                 </div>
             </div>
         </div>
@@ -19,9 +19,11 @@ const AlbumCard = (props) => {
             <a className="title" href={props.albumContent.external_urls.spotify} target="_blank" rel="noopener noreferrer">
                 {props.albumContent.name}
             </a>
-            {props.albumContent.artists.map(item => (
-            <Artists key={item.id} artistContent = {item} />
-            ))}   
+            <div>
+                {props.albumContent.artists.map((item) => (
+                <Artists key={item.id} artistContent = {item} />
+                ))}  
+            </div>
         </div>
     </div>
         )
--- code/src/components/Artists.js
+++ code/src/components/Artists.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Artists = (props) => {
     return (
-        <a className="artist" href={props.artistContent.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+        <span className="artist-container">
+            <a className="artist" href={props.artistContent.external_urls.spotify} target="_blank" rel="noopener noreferrer">
             {props.artistContent.name}
-        </a>
+            </a>
+        </span>
     )
 }
 
