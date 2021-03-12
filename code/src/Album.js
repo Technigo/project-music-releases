212787cf4594d88b072362285b1181cef75a82b7
Lars Söderman <lars.soderman@gmail.com>
2021-03-12 10:02:54 +0100

    got icons in place kind of

--- code/src/Album.js
+++ code/src/Album.js
@@ -5,7 +5,14 @@ export const Album = (props) => {
 
         return (
             <div className="album">
-                <img src={props.imageSrc} alt={props.albumName}></img>
+                <div className="image-container">
+                    <img src={props.imageSrc} alt={props.albumName}></img>
+                    <div className="icons-container">
+                        <img className="heart" src="./icons/heart.svg"/>
+                        <img className="play-button" src="./icons/play.svg"/>
+                        <img className="dots" src="./icons/dots.svg"/>
+                    </div>
+                </div>
                 <p className="album-title">{props.albumName}</p>
                 <div className="artists">
                     <Artist key={props.id} artists={props.artists} />
