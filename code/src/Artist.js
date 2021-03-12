21fb9bbd45fd5e92725552db0b19bd31ab95d906
Lars Söderman <lars.soderman@gmail.com>
2021-03-12 08:48:46 +0100

    gonn try to merge now

--- code/src/Artist.js
+++ code/src/Artist.js
@@ -2,15 +2,6 @@ import React from 'react'
 
 export const Artist = (props) => {
     return(
-        // console.log(props.artists)
-
-
-        // <div className="artist-container">
-        //     key={artist.id} 
-        //     artistUrl={artist.external_urls.spotify}
-        //     artistName={artist.name}
-        // </div>
-        // return (null)
         <>
             {
             props.artists.map(artist => {
