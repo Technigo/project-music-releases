091fe29c8c410c49c48aa757a8749a7c3567efdb
Lars Söderman <lars.soderman@gmail.com>
2021-03-13 00:35:04 +0100

    fixed icon overlay bug

--- code/src/Album.js
+++ code/src/Album.js
@@ -5,16 +5,14 @@ export const Album = (props) => {
 
         return (
             <div className="album">
-                <div className="image-container">
-                    <img src={props.imageSrc} alt={props.albumName}></img>
-                        <div className="overlay">
-                            <div className="icons-container">
-                                <img className="heart" src="./icons/heart.svg"/>
-                                <img className="play-button" src="./icons/play.svg"/>
-                                <img className="dots" src="./icons/dots.svg"/>
-                            </div>
+                    <div class="image-container">
+                        <div className="icons-container">
+                            <img className="heart" src="./icons/heart.svg"/>
+                            <img className="play-button" src="./icons/play.svg"/>
+                            <img className="dots" src="./icons/dots.svg"/>
                         </div>
-                </div>
+                        <img src={props.imageSrc} alt={props.albumName} />
+                    </div>
                 <p className="album-title">{props.albumName}</p>
                 <div className="artists">
                     <Artist key={props.id} artists={props.artists} />
