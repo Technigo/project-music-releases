f470cfe28ed664cd945f00dc8a327153a0a0a0bb
NoemiSalome <noemifrischknecht@macbook-pro.home>
2021-03-12 07:49:36 +0100

    added hover effect on icons

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -10,9 +10,10 @@ const Album = (props) => {
                     <a href={props.albumContent.external_urls.spotify}>
                         <img src={props.albumContent.images[1].url} className="albumimg" alt="albumcover" />
                         <div className="hover-icons">
-                            <img src="../icons/heart.svg" class="icons" alt="heart-icon"></img>
-                            <img src="../icons/play.svg" class="icons" alt="play-icon"></img>
-                            <img src="../icons/dots.svg" class="icons" alt="dot-icon"></img>
+                            <img src="../icons/heart.svg" className="icons" alt="heart-icon"></img>
+                            <div className="play-icon-hover"></div>
+                            <img src="../icons/play.svg" className="play-icon" alt="play-icon"></img>
+                            <img src="../icons/dots.svg" className="icons" alt="dot-icon"></img>
                         </div>
                     </a>
                 </div>
