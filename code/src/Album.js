c86dd50df33dc1c43eae5f3d6018dac022caee8a
Theresehag <tezzinthecity@gmail.com>
2021-03-13 13:46:13 +0100

    add components, add mapping, add icon styling

---
+++ code/src/Album.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+export const Album = (props) => {
+    return (
+        <div className="card"> 
+          <div className="picture">
+            <a href={props.albumurl}>         
+            <img className="image" src={props.picture} alt="album cover"/></a>
+            <div className="icons">
+            <img className="heart-icon" src="icons/heart.svg" alt="" />
+            <img className="play-icon"  src="icons/play.svg" alt="" />
+            <img className="dots-icon"  src="icons/dots.svg" alt="" />
+            </div>
+        </div>
+            <h2 className="albums-name">{props.name}</h2>
+            <div>
+            <a className="link" href={props.externalurls}>
+            <h3 className="artist-name">{props.artist}</h3></a>           
+        </div>
+        </div>
+
+    )
+}
--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import data from './data.json'
+import {Album} from './Album.js'
+import {Header} from './Header'
+import './index.css'
+
 
 console.log(data)
 
+const albumArray = data.albums.items
+
+
+
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+  <>
+  <Header/>
+    <div className="main">
+      {albumArray.map((album) => {
+        return (  
+          <Album key={album.name} name={album.name} picture={album.images[0].url} albumurl={album.external_urls.spotify} artist={album.artists.map((item) => 
+            <a className="link" key={item.name} href={item.external_urls.spotify}><h3 className="artist-name">{item.name}</h3></a>
+          )
+      }/>) })}
     </div>
-  )
+    </>
+        )
 }
