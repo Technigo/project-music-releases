6092630be667806297223ae32c46303e75d920f3
Nazanin Adampira <Nazi.pira88@gmail.com>
2021-03-14 22:54:53 +0100

    fix equal sign

--- code/src/components/Artists.js
+++ code/src/components/Artists.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Artists = (props) => {
   return props.artists.map((artist) => {
-    const isLastArtist = props.artists.indexOf(artist) == props.artists.length - 1;
+    const isLastArtist = props.artists.indexOf(artist) === props.artists.length - 1;
     return (
         <a href={artist.external_urls.spotify} key={artist.id}>
           { isLastArtist ? 
