2838e15b4f7c7bbb8923587a00ae2b9e18a5377b
Your Name <isabellamarkskog@live.se>
2021-03-22 15:26:35 +0100

    Made adjustments in accordance with requested changes

--- code/src/App.js
+++ code/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import data from './data.json'
+
 import { Album } from './components/Album'
+import data from './data.json'
 
 export const App = () => {
   return (
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,4 +1,5 @@
 import React from 'react'
+
 import { Artist } from './Artist'
 
 export const Album = (props) => {
