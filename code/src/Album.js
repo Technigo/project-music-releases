19279306a8afce0b4489b8e784495144997a5344
Theresehag <tezzinthecity@gmail.com>
2021-04-14 23:06:55 +0200

    remove console, fix hover effect and show artists in one line after code review

--- code/src/Album.js
+++ code/src/Album.js
@@ -5,13 +5,14 @@ export const Album = (props) => {
     return (
         <div className="card"> 
           <div className="picture">
-            <a href={props.albumurl}>         
+          <a href={props.albumurl}>         
             <img className="image" src={props.picture} alt="album cover"/></a>
             <div className="icons">
             <img className="heart-icon" src="icons/heart.svg" alt="" />
             <img className="play-icon"  src="icons/play.svg" alt="" />
             <img className="dots-icon"  src="icons/dots.svg" alt="" />
             </div>
+            
         </div>
             <h2 className="albums-name">{props.name}</h2>
             <div>
--- code/src/App.js
+++ code/src/App.js
@@ -4,9 +4,6 @@ import {Album} from './Album.js'
 import {Header} from './Header'
 import './index.css'
 
-
-console.log(data)
-
 const albumArray = data.albums.items
 
 
@@ -19,7 +16,7 @@ export const App = () => {
       {albumArray.map((album) => {
         return (  
           <Album key={album.name} name={album.name} picture={album.images[0].url} albumurl={album.external_urls.spotify} artist={album.artists.map((item) => 
-            <a className="link" key={item.name} href={item.external_urls.spotify}><h3 className="artist-name">{item.name}</h3></a>
+           <a className="link" key={item.name} href={item.external_urls.spotify}><h3 className="artist-name">{item.name}</h3></a>
           )
       }/>) })}
     </div>
