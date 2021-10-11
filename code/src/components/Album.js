a1f3240575712cb732e5f8780602c48fe3ab7341
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-10-11 16:34:40 +0200

    more styling

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -3,7 +3,8 @@ import React from 'react'
 
 export const Album = props => {
     return (
-        <section className="album-container" >
+
+        <section  >
             <img className="album-cover" src={props.src} alt="album-cover" />
             <a href={props.titleUrl} target="_blank"><h1 className="album-title">{props.title}</h1></a>
             <a href={props.artistUrl} target="_blank"><h1 className="album-artist">{props.artist}</h1></a>
--- code/src/components/App.js
+++ code/src/components/App.js
@@ -11,18 +11,20 @@ export const App = () => {
   return (
     <div>
       <Header />
-      {items.map((data) => {
-        return (
-          <Album
-            key={data.artists[0].id}
-            src={data.images[1].url}
-            title={data.name}
-            artist={data.artists[0].name}
-            titleUrl={data.external_urls.spotify}
-            artistUrl={data.artists[0].external_urls.spotify}
-          />
-        )
-      })}
+      <div className="album-container">
+        {items.map((data) => {
+          return (
+            <Album
+              key={data.artists[0].id}
+              src={data.images[1].url}
+              title={data.name}
+              artist={data.artists[0].name}
+              titleUrl={data.external_urls.spotify}
+              artistUrl={data.artists[0].external_urls.spotify}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
