157d3d87a8ec39d57e2f8e848befa60c6cec0469
molbimien <43146046+molbimien@users.noreply.github.com>
2021-10-11 18:44:05 +0200

    project initial setup

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import data from './data.json'
 
+import Article from './components/Article'
+
 console.log(data)
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  <div>
+    <Article 
+      title ="Spring is coming!" 
+      description="Just a descritpion"
+      blabla="blublublu"
+    />
+    
+    <Article 
+      title ="Spring is coming sooner!" 
+      description="Just a descritpion"
+      blabla="blublublu"
+    />
+  </div>
   )
 }
