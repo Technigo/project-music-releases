2c707a69cf2a8a6e562c2c4491aa91695f708f1f
Gustav Frid <gustav.frid@gmail.com>
2021-10-11 21:37:40 +0200

    created albumcover component as test to pass props from data

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import data from './data.json'
+import { AlbumCover } from 'components/AlbumCover'
 
 console.log(data)
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
+	return data.albums.items.map(item => {
+		return <AlbumCover url={item.images[0].url} />
+	})
 }
