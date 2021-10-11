761e6ed60bafd925f4e94ce3046c8dcad182c1b6
HedvigM <hedvig@mejstedt.se>
2021-10-11 19:20:27 +0200

    made an Article and placed it into a grid

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,49 @@
 import React from 'react'
 import data from './data.json'
+import Article from './components/Article';
 
 console.log(data)
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+	return (
+		<div className="grid">
+			<Article
+        img="https://via.placeholder.com/150x150"
+				title="Album!"
+        band="Random band name"
+			/>
+
+			<Article
+        img="https://via.placeholder.com/150x150"
+				title="Another Album!"
+				band="random band name 2"
+			/>
+
+      <Article
+        img="https://via.placeholder.com/150x150"
+				title="wow a third album!"
+				band="random band name 3"
+			/>
+
+      <Article
+        img="https://via.placeholder.com/150x150"
+				title="sooo much music!"
+				band="random band name 4"
+			/>
+
+      <Article
+        img="https://via.placeholder.com/150x150"
+				title="It never ends!"
+				band="random band name 5"
+			/>
+
+      <Article
+        img="https://via.placeholder.com/150x150"
+				title="OK a 6:th album!"
+				band="random band name 6"
+			/>
+		</div>
+	);
+};
+
+
---
+++ code/src/components/Article.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Article = (props) => {
+	console.log(props);
+
+	return (
+            <article className="article">
+                    <img src={props.img} alt="album image" />
+                    <h2>{props.title}</h2>
+                    <p>{props.band}</p>
+            </article>
+	);
+};
+
+export default Article;
