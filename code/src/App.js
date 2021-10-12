069bfcfd182a86ab2d41439fd505b3742147650e
HedvigM <hedvig@mejstedt.se>
2021-10-12 20:10:18 +0200

    importing info from the json and printing one album

--- code/src/App.js
+++ code/src/App.js
@@ -1,49 +1,50 @@
-import React from 'react'
-import data from './data.json'
-import Article from './components/Article';
+import React from "react";
+import data from "./data.json";
+import Article from "./components/Article";
 
-console.log(data)
+console.log(data.albums.items[0]);
 
 export const App = () => {
-	return (
-		<div className="grid">
-			<Article
-        img="https://via.placeholder.com/150x150"
-				title="Album!"
-        band="Random band name"
-			/>
+  return (
+    <>
+      <h1>New Albums & Singles</h1>
+      <div className="grid">
+        <Article
+          img={data.albums.items[0].images[1].url}
+          title={data.albums.items[0].name}
+          band={data.albums.items[0].artists[0].name}
+        />
 
-			<Article
-        img="https://via.placeholder.com/150x150"
-				title="Another Album!"
-				band="random band name 2"
-			/>
+        <Article
+          img="https://via.placeholder.com/150x150"
+          title="Another Album!"
+          band="random band name 2"
+        />
 
-      <Article
-        img="https://via.placeholder.com/150x150"
-				title="wow a third album!"
-				band="random band name 3"
-			/>
+        <Article
+          img="https://via.placeholder.com/150x150"
+          title="wow a third album!"
+          band="random band name 3"
+        />
 
-      <Article
-        img="https://via.placeholder.com/150x150"
-				title="sooo much music!"
-				band="random band name 4"
-			/>
+        <Article
+          img="https://via.placeholder.com/150x150"
+          title="sooo much music!"
+          band="random band name 4"
+        />
 
-      <Article
-        img="https://via.placeholder.com/150x150"
-				title="It never ends!"
-				band="random band name 5"
-			/>
+        <Article
+          img="https://via.placeholder.com/150x150"
+          title="It never ends!"
+          band="random band name 5"
+        />
 
-      <Article
-        img="https://via.placeholder.com/150x150"
-				title="OK a 6:th album!"
-				band="random band name 6"
-			/>
-		</div>
-	);
+        <Article
+          img="https://via.placeholder.com/150x150"
+          title="OK a 6:th album!"
+          band="random band name 6"
+        />
+      </div>
+    </>
+  );
 };
-
-
--- code/src/components/Article.js
+++ code/src/components/Article.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Article = (props) => {
-	console.log(props);
+  console.log(props);
 
-	return (
-            <article className="article">
-                    <img src={props.img} alt="album image" />
-                    <h2>{props.title}</h2>
-                    <p>{props.band}</p>
-            </article>
-	);
+  return (
+    <article className="article">
+      <img src={props.img} alt="album image" />
+      <h2>{props.title}</h2>
+      <p>{props.band}</p>
+    </article>
+  );
 };
 
 export default Article;
