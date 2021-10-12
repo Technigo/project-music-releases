4fdeb85385def6805635f790c42c3b2ed9bd0658
dandeloid <82587220+dandeloid@users.noreply.github.com>
2021-10-12 11:21:07 +0200

    map album added

--- code/src/App.js
+++ code/src/App.js
@@ -2,53 +2,42 @@ import React from 'react'
 import data from './data.json'
 
 import { Album } from './components/Album'
+import { Header } from './components/Header'
 
 console.log(data)
 
+
+// Album component which is rendered using .map() and which you pass the album data into using props.
+
+// Artist component which is rendered using .map() using each item in the artists array, 
+// inside the Album component to show the artist name and a link.
+
 export const App = () => {
   return (
-    <div className="container">
-      <Album 
-        artist={data.albums.items[0].artists[0].name}
-        title={data.albums.items[0].name}
-        img={data.albums.items[0].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[1].artists[0].name}
-        title={data.albums.items[1].name}
-        img={data.albums.items[1].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[2].artists[0].name}
-        title={data.albums.items[2].name}
-        img={data.albums.items[2].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[3].artists[0].name}
-        title={data.albums.items[3].name}
-        img={data.albums.items[3].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[4].artists[0].name}
-        title={data.albums.items[4].name}
-        img={data.albums.items[4].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[5].artists[0].name}
-        title={data.albums.items[5].name}
-        img={data.albums.items[5].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[6].artists[0].name}
-        title={data.albums.items[6].name}
-        img={data.albums.items[6].images[1].url}
-      />
-      <Album 
-        artist={data.albums.items[7].artists[0].name}
-        title={data.albums.items[7].name}
-        img={data.albums.items[7].images[1].url}
-      />
+    <div className="site-container">
+      <Header />
+        <div className="container">
+          {data.albums.items.map((item) => {
+            return <Album
+              img = {item.images[1].url}
+              title = {item.name}
+              artist = {item.artists[0].name}
+              key = {item.name}
+              />
+            }
+          )}
+
+        </div>
     </div>
     
   )
 }
+
+
+
+
+{/* <Album 
+artist={data.albums.items[0].artists[0].name}
+title={data.albums.items[0].name}
+img={data.albums.items[0].images[1].url}
+/> */}
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,15 +1,12 @@
-
-/* Monday lecture stuff */
-
 import React from 'react'
 
 export const Album = (props) => {
     console.log(props)
     return (
         <article className="album">
-          <img src={props.img} alt="spring view" />
-          <h2>{props.title}</h2>
-          <p>{props.artist}</p>
+          <img src={props.img} alt="album cover" />
+          <h2 className="title">{props.title}</h2>
+          <p className="artist">{props.artist}</p>
         </article>
       )
 } 
