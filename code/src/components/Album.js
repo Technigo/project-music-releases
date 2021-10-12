36af58dbf1ffac9c30499bf75c21ab3fb33191ec
unknown <rebecca.blixt@hotmail.com>
2021-10-12 17:07:45 +0200

    added links to album and artists to album.js. finished overall styling.

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,12 +1,22 @@
 import React from "react";
-import Artists from "./Artists";
 
 const Album = (props) => {
   return (
     <article className="album-card">
       <img src={props.item.images[0].url} alt="#" />
       <div className="text-wrapper">
-        <Artists />
+        <h2 className="song-title">
+          <a href={props.item.external_urls.spotify}>{props.item.name}</a>
+        </h2>
+        <h3 className="song-artists">
+          <a href={props.item.artists[0].external_urls.spotify}>
+            {props.item.artists[0].name}
+          </a>
+        </h3>
+
+        {/* {prop.artists.items.map((items) => (
+            <Artists key={items.id} item={items} />
+          ))} */}
       </div>
     </article>
   );
--- code/src/components/Artists.js
+++ code/src/components/Artists.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Artists = () => {
+const Artists = (props) => {
   return (
-    <div>
-      <h2 className="song-title">Song Title</h2>
-      <h3 className="song-artists">List Artists</h3>
-    </div>
+    <>
+      <div>
+        <h2 className="song-title">{props.item.name}</h2>
+        <h3 className="song-artists">{props.item.artists.name}</h3>
+      </div>
+    </>
   );
 };
 
-export default Artists;
+// export default Artists;
