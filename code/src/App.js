d968d17f1cbc56dedf33c508096e375384447bfb
Gustav Frid <gustav.frid@gmail.com>
2021-10-12 14:41:19 +0200

    change indentation

--- code/src/App.js
+++ code/src/App.js
@@ -5,7 +5,7 @@ import { AlbumCover } from 'components/AlbumCover'
 console.log(data)
 
 export const App = () => {
-	return data.albums.items.map(item => {
-		return <AlbumCover url={item.images[0].url} />
-	})
+  return data.albums.items.map(item => {
+    return <AlbumCover url={item.images[0].url} />
+  })
 }
--- code/src/components/AlbumCover.js
+++ code/src/components/AlbumCover.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const AlbumCover = props => {
-	return <img src={props.url} alt='' style={{ width: '100px', margin: '5px' }} />
+  return <img src={props.url} alt='' style={{ width: '100px', margin: '5px' }} />
 }
