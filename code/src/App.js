1e04b2e5dad1793e59e0dd228f130537fd6e176d
elsisco <elsa.carlstrom@gmail.com>
2021-10-13 18:16:01 +0200

    fixed links to individual artists

--- code/src/App.js
+++ code/src/App.js
@@ -8,32 +8,66 @@ console.log(AlbumArray);
 
 export const App = () => {
   return (
-    <div className="albums-wrapper">
-      {AlbumArray.map((album) => {
-        return (
-          <Album
-            image={album.images[1].url}
-            title={album.name}
-            titleLink={album.external_urls.spotify}
-            artist={album.artists.map((artist, index) => {
-              
-              const numberOfArtists = album.artists.length
 
-              if (numberOfArtists === index + 1 && numberOfArtists !== 1) {
-                return <a> & {artist.name}</a>
-              } else if (numberOfArtists === index + 2 && index !== 0) {
-                return <a>, {artist.name}</a>
-              } else {
-                return <a>{artist.name}</a>
-              }
-            }
-            )}
-            artistlink={album.artists.map(
-              (artist) => artist.external_urls.spotify
-            )}
-          />
-        );
-      })}
+    <div className="title-wrapper">
+      <h1>New albums & singles</h1>
+    
+    
+      <div className="albums-wrapper">
+      
+        {AlbumArray.map((album) => {
+
+          // const numberOfArtists = album.artists.length
+
+          return (
+            
+            <Album
+              key={album.id}
+              image={album.images[1].url}
+              title={album.name}
+              titleLink={album.external_urls.spotify}
+              artist={album.artists}
+
+              
+
+              // artist={album.artists = ((artist, index) => {
+                
+              //    const numberOfArtists = album.artists.length
+
+              //   if (numberOfArtists === index + 1 && numberOfArtists !== 1) {
+              //     return <span> & {artist.name}</span>
+              //   } else if (numberOfArtists === index + 2 && index !== 0) {
+              //     return <span>, {artist.name}</span>
+              //   } else {
+              //     return <span>{artist.name}</span>
+              //   }
+                
+              // })}
+
+
+              // artist={album.artists.map((artist, index) => {
+                
+              //    const numberOfArtists = album.artists.length
+
+              //   if (numberOfArtists === index + 1 && numberOfArtists !== 1) {
+              //     return <span> & {artist.name}</span>
+              //   } else if (numberOfArtists === index + 2 && index !== 0) {
+              //     return <span>, {artist.name}</span>
+              //   } else {
+              //     return <span>{artist.name}</span>
+              //   }
+                
+              // })}
+              // artistLink={album.artists.map(
+              //   (artist) => artist.external_urls.spotify
+              // )}
+            />  
+          );
+          })
+        }
+      
+      </div>
     </div>
+    
   );
 };
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -3,13 +3,44 @@ import Artist from "./Artist";
 import CoverAlbum from "./CoverAlbum";
 import SongTitle from "./SongTitle";
 
-const Album = ({ image, title, titleLink, artist, artistlink }) => {
+const Album = ({ image, title, titleLink, artist }) => {
+  // console.log(artist.length)
+  const numberOfArtists = artist.length
+  console.log(numberOfArtists)
+  
+  const artistCommaAmpersand = (artist, index) => {
+    if (numberOfArtists === index + 1 && numberOfArtists !== 1) {
+      // console.log('Ampersand success!')
+        return <span> & {artist.name}</span>
+      } else if (numberOfArtists === index + 2 && index !== 0) {
+        // console.log('Comma success!')
+        return <span>, {artist.name}</span>
+      } else {
+        // console.log('Single artist success!')
+        return <span>{artist.name}</span>
+      }
+  }
+
   return (
     <div className="album-wrapper">
       <div className="album">
         <CoverAlbum image={image} />
         <SongTitle titleLink={titleLink} title={title} />
-        <Artist artistLink={artistlink} artist={artist} />
+        {/* <Artist artistLink={artistLink} artist={artist} /> */}
+
+        {artist.map((artist, index) => {
+            return <Artist 
+            artistLink={artist.external_urls.spotify}
+            artist={artistCommaAmpersand(artist, index)} />
+            
+          })}
+
+          {/* {artist.map((ar, index) => {
+            return <Artist 
+            artistLink={ar.external_urls.spotify} artist={ar.name} />
+            
+          })} */}
+        
       </div>
     </div>
   );
