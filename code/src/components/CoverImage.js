53bcf7d6739fb5cab140172a715a68cd71b83ca5
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-10-13 18:09:50 +0200

    could not deploy site, added alt to img tags

--- code/src/components/CoverImage.js
+++ code/src/components/CoverImage.js
@@ -7,9 +7,24 @@ export const CoverImage = (props) => {
         <img src={props.image} alt='album cover' className='cover-image' />;
         <div className='overlay'>
           <div className='icon-container'>
-            <img src='./icons/heart.svg' width='20' height='20' />
-            <img src='./icons/play.svg' width='50' height='50' />
-            <img src='./icons/dots.svg' width='20' height='20' />
+            <img
+              src='./icons/heart.svg'
+              width='20'
+              height='20'
+              alt='heart icon'
+            />
+            <img
+              src='./icons/play.svg'
+              width='50'
+              height='50'
+              alt='play icon'
+            />
+            <img
+              src='./icons/dots.svg'
+              width='20'
+              height='20'
+              alt='ellipsis icon'
+            />
           </div>
         </div>
       </div>
