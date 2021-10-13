a1beae7e9dd4dcfdf00cfb60492c1c09f8ed3981
Jakob Lindström <Snake@Jakobs-MacBook-Pro-2.local>
2021-10-13 11:12:40 +0200

    Added artist component

--- code/src/components/Vinyl.js
+++ code/src/components/Vinyl.js
@@ -9,7 +9,9 @@ const Vinyl = (props) => {
         <img src="/icons/play.svg" alt="play" className="icons play" />
         <img src="/icons/dots.svg" alt="dots" className="icons" />
       </div>
-      <img className="album-cover" src={props.cover} alt="album artwork" />
+      <div className="overlay">
+        <img className="album-cover" src={props.cover} alt="album artwork" />
+      </div>
       <h2 className="song-title">{props.song}</h2>
       <h2 className="artist">{props.artist}</h2>
     </article>
