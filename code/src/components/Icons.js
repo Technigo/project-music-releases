639a5895885725e83b509bf31a49d00bc0dd2e71
Gustav Frid <gustav.frid@gmail.com>
2021-10-13 15:43:57 +0200

    change icon styles

--- code/src/components/Icons.js
+++ code/src/components/Icons.js
@@ -4,19 +4,9 @@ import 'components/Icons.css'
 export const Icons = props => {
   return (
     <div className='icons-container'>
-      <img
-        className='icon heart'
-        src='/icons/heart.svg'
-        alt='heart icon'
-        style={{ width: '25px', zIndex: '1' }}
-      />
-      <img className='icon play' src='/icons/play.svg' alt='play icon' style={{ zIndex: '1' }} />
-      <img
-        className='icon dots'
-        src='/icons/dots.svg'
-        alt='dot icon'
-        style={{ width: '20px', zIndex: '1' }}
-      />
+      <img className='icon heart' src='/icons/heart.svg' alt='heart icon' />
+      <img className='icon play' src='/icons/play.svg' alt='play icon' />
+      <img className='icon dots' src='/icons/dots.svg' alt='dot icon' />
     </div>
   )
 }
