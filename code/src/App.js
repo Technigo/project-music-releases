7c99712f623d937b0f65c51d94c046c350168f2f
Gustav Frid <gustav.frid@gmail.com>
2021-10-13 21:30:00 +0200

    added sidebar

--- code/src/App.js
+++ code/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react'
 
 import { AlbumWrapper } from 'components/AlbumWrapper'
+import { Sidebar } from 'components/Sidebar'
 
 export const App = () => {
   return (
-    <div className='container'>
-      <h1 className='title'>New albums</h1>
-      <div className='album-grid'>
-        <AlbumWrapper type={'album'} />
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <div className='container'>
+        <h1 className='title'>New albums</h1>
+        <div className='album-grid'>
+          <AlbumWrapper type={'album'} />
+        </div>
+        <h1 className='title'>New singles</h1>
+        <div className='album-grid'>
+          <AlbumWrapper type={'single'} />
+        </div>
       </div>
-      <h1 className='title'>New singles</h1>
-      <div className='album-grid'>
-        <AlbumWrapper type={'single'} />
-      </div>
+      <aside className='sidebar'>
+        <h1 className='title'>Playlists</h1>
+        <Sidebar />
+      </aside>
     </div>
   )
 }
---
+++ code/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import data from '../stretch-goal.json'
+
+console.log(data)
+
+export const Sidebar = () => {
+  return data.playlists.items.map(item => (
+    <p className='playlist'>
+      <a href={item.external_urls.spotify} target='_blank' rel='noopener noreferrer'>
+        {item.name}
+      </a>
+    </p>
+  ))
+}
