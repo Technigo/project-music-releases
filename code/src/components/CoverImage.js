dc3d4e502f7410dacfa7785edb84bc037745931c
katiewu1 <83988618+katiewu1@users.noreply.github.com>
2021-10-13 23:55:39 +0200

    remove semicolon

--- code/src/components/CoverImage.js
+++ code/src/components/CoverImage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const CoverImage = (props) => {
   return (
     <div className='image-container'>
-      <img src={props.image} alt='album cover' className='cover-image' />;
+      <img src={props.image} alt='album cover' className='cover-image' />
       <div className='overlay'>
         <div className='icon-container'>
           <img
