301fb5b2d3ed2ebf5ae003b7d559be754edab26d
Darya <darya.lapata@gmail.com>
2021-10-14 11:27:47 +0200

    added icons component

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
+import { Icons } from '../components/Icons'
+
 export const Album  = (props) => {
     console.log('PROPS', props)
- 
     return (
     <div className="card">
+        <div className="img-container"> 
+            <a href={props.href}  target="_blank" rel="noopener noreferrer">
+    <img src={props.image} alt="" className="album-img" />
+                <Icons />
+            </a>
+        </div>
+
+        {/* album title*/}
         <a href={props.href}  target="_blank" rel="noopener noreferrer">
-            <img src={props.image} alt="" className="album-img" />
             <p className="albumTitle">{props.title}</p>
-            {/* <div className="icons-container">
-                <img className="icon" src="./icons/heart.svg" alt="heart-icon"/>
-                <img className="icon" src="./icons/play.svg" alt="play-icon" />
-                <img className="icon" src="./icons/dots.svg" alt="dots-icon" />
-            </div> */}
         </a>
 
         {/* ARTIST - we could create a component here, but this solution works as well  */}
--- code/src/components/Header.js
+++ code/src/components/Header.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
 export const Header  = () => {
-    return (
-       <h1>New Albums & singles</h1>
-    );
+    return <h1>New Albums & Singles</h1>
 };
