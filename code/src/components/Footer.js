4fad175e8f0565061636d41a2c1f8ec86297d8ef
MarySnopok <marysnopok8@gmail.com>
2021-10-14 13:04:34 +0200

    typo fix

--- code/src/components/Footer.js
+++ code/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <div className="signature-container">
-      <h5>Design and Developed by</h5>
+      <h5>Designed and Developed by</h5>
       <h5>Lovisa Carling, Mary Snopok, Haru Ahn</h5>
     </div>
   );
