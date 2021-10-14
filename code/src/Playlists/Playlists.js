e8ae0f99e4b2e91a84ffa9dd7a568e7799bef295
Никитина Юля <nik.iuliiasw@gmail.com>
2021-10-14 23:26:19 +0200

    fixed styling for small icon

--- code/src/Playlists/Playlists.js
+++ code/src/Playlists/Playlists.js
@@ -10,9 +10,9 @@ const Playlists = () => {
             {playlistsArray.map((playlist) => {
                 return (
                     <div key={playlist.id}>
-                        <div className="playlist-card">
-                            <img className="image-playlists" src={playlist.images[0].url} alt="playlist-cover" />
-                            <a href={playlist.external_urls.spotify} className="playlist-link" target="_blank" rel="noreferrer noopener" title="Click to listen"><img className="play-2" src="./icons/play.svg" alt="play icon" /></a>
+                        <div className="playlist-image">
+                            <img className="image-playlists" src={playlist.images[0].url} alt="image-playlists" />
+                            <div className="icon"><a href={playlist.external_urls.spotify} className="playlist-link" target="_blank" rel="noreferrer noopener" title="Click to listen"><img className="play2" src="./icons/play.svg" alt="play icon" /></a></div>
                         </div>
                         <a href={playlist.external_urls.spotify} className="playlist-link" target="_blank" rel="noreferrer noopener">{playlist.name} </a>
                         <p className="track-amount">{playlist.tracks.total} tracks</p>
