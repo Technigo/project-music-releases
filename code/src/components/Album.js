597ca4fd8f0b2236c9df0ce4accc4d8deab9628f
Birgit Nehrwein <nehrwein@posteo.de>
2021-10-14 11:41:16 +0200

    added icons and hover effects

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -8,12 +8,23 @@ const Album = (props) => {
 
   return (
     <article className="album">
-      <img src={props.url} alt="Album cover" />
+      <div class="image-icons-container">
+        <div class="image-container">
+         <img src={props.url} alt="Album cover" />
+        </div>
+        <div class="icons-container">
+          <img class="icons" src="./icons/heart.svg" />
+          <img class="play-icon" src="./icons/play.svg" />
+          <img class="icons" src="./icons/dots.svg" />
+        </div>
+      </div> 
       <h2>{props.title}</h2>
       <div className="artist-name-container">
+        <p className="artist"> 
         {props.artist.map((artist) => (
           <Artist key={artist.name} artistName={artist.name} />
         ))}
+        </p>
       </div>
     </article>
   )
