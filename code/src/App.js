7dce224c62fff1736b7f3af7c730060d09ea9c46
Harry Bäcklin <83465217+Groovedharry@users.noreply.github.com>
2021-10-15 00:42:02 +0200

    CSS
    Har mediaqueries kvar

--- code/src/App.js
+++ code/src/App.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import Header from 'components/Header';
 
+
 import data from './data.json';
 
 import Cover from './components/Cover';
 import Artist from './components/Artist';
 import Title from './components/Title';
 
+
 console.log(data); // REMOVE
 
 export const App = () => {
   return (
     <div className="app-section">
-      <Header />
+      <div id="wrapper">
+        <div>
+        <Header />
+      </div>
+      <div className="grid-container">
       {data.albums.items.map((element) => {
         console.log(element); //REMOVE
         return (
-          <div key={element.id} className="app-container">
+          <div key={element.id} className="card-container">
             <Cover item={element} />
             <Title item={element} />
             <Artist item={element} />
           </div>
         );
-      })}
+      })} 
+        </div>
+      </div>
     </div>
   );
+  
 };
+
 
 export default App;
--- code/src/components/Cover.js
+++ code/src/components/Cover.js
@@ -1,19 +1,33 @@
 import React from 'react';
 
+// let elementToHover = document.querySelector(".cover")
+
+// function mouseOverFunction(e) {
+//     document.querySelector(".icons").style.display = "flex";
+// }
+
+// function mouseOutFunction() {
+//     document.querySelector(".icons").style.display = "none";
+// }
+
+
+// elementToHover.addEventListener("mouseover", mouseOverFunction);
+// elementToHover.addEventListener("mouseout", mouseOutFunction);
+
 const Cover = (props) => {
   return (
     <div className="cover">
       <img
         className="cover-img"
-        src={props.item.images[0].url}
+        src={props.item.images[1].url}
         alt="Album cover"
       />
 
       <div className="icons">
-        <img className="icons-heart" src="./icons/heart.svg" alt="like icon" />
-        <img className="icons-play" src="./icons/play.svg" alt="play icon" />
+        <img className="icons-heart icons-small icon" src="./icons/heart.svg" alt="like icon" />
+        <img className="icons-play icon" src="./icons/play.svg" alt="play icon" />
         <img
-          className="icons-dots"
+          className="icons-dots icons-small icon"
           src="./icons/dots.svg"
           alt="read more icon"
         />
