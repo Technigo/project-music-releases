02a9aafd33927778dbe26611751ea84aaff03288
Rephili <72921631+Rephili@users.noreply.github.com>
2021-10-15 12:47:08 +0200

    cleaned up some code

--- code/src/App.js
+++ code/src/App.js
@@ -4,8 +4,6 @@ import data from "./data.json";
 import Album from "./components/Album";
 import Header from "./components/Header";
 
-// const album = data.albums.items[0];
-
 export const App = () => {
   return (
     <div>
