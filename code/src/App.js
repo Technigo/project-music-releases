77798fd7e172cce03acce182ea119d60888e7787
Gustav Frid <gustav.frid@gmail.com>
2021-10-15 10:13:45 +0200

    adde onclick

--- code/src/App.js
+++ code/src/App.js
@@ -1,38 +1,36 @@
-import React from "react";
+import React from 'react'
 
-import { AlbumWrapper } from "components/AlbumWrapper";
-import { Sidebar } from "components/Sidebar";
+import { AlbumWrapper } from 'components/AlbumWrapper'
+import { Sidebar } from 'components/Sidebar'
 
 export const App = () => {
   return (
-    <div className="album-wrapper">
+    <div className='album-wrapper'>
       <img
-        className="menu"
-        src="/icons/dots.svg"
-        alt="dot icon"
-        onKeyPress={(e) =>
-          (e.target.className = e.target.className.includes("open")
-            ? "menu"
-            : "menu open")
+        className='menu'
+        src='/icons/dots.svg'
+        alt='dot icon'
+        onClick={e =>
+          (e.target.className = e.target.className.includes('open') ? 'menu' : 'menu open')
         }
       />
-      <div className="container">
-        <h1 className="title">New albums</h1>
-        <div className="album-grid">
-          <AlbumWrapper type={"album"} />
+      <div className='container'>
+        <h1 className='title'>New albums</h1>
+        <div className='album-grid'>
+          <AlbumWrapper type={'album'} />
         </div>
-        <h1 className="title">New singles</h1>
-        <div className="album-grid">
-          <AlbumWrapper type={"single"} />
+        <h1 className='title'>New singles</h1>
+        <div className='album-grid'>
+          <AlbumWrapper type={'single'} />
         </div>
       </div>
 
-      <aside className="sidebar">
-        <h1 className="title">Playlists</h1>
+      <aside className='sidebar'>
+        <h1 className='title'>Playlists</h1>
         <ul>
           <Sidebar />
         </ul>
       </aside>
     </div>
-  );
-};
+  )
+}
