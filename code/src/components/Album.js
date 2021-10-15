6cd67a320acb55bc1ddcd01fd470ce67007ebcbd
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-10-15 10:29:34 +0200

    link target fixed and alt text added

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -11,13 +11,17 @@ export const Album = (props) => {
           alt="album cover"
         />
         <div className="icons">
-          <img className="heart-icon" src="/icons/heart.svg" />
-          <img className="play-icon" src="/icons/play.svg" />
-          <img className="dots-icon" src="/icons/dots.svg" />
+          <img className="heart-icon" src="/icons/heart.svg" alt="heart icon" />
+          <img className="play-icon" src="/icons/play.svg" alt="play icon" />
+          <img className="dots-icon" src="/icons/dots.svg" alt="dots icon" />
         </div>
       </div>
       <h3 className="album-title">
-        <a href={props.item.external_urls.spotify} target="_blank">
+        <a
+          href={props.item.external_urls.spotify}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           {props.item.name}
         </a>
       </h3>
