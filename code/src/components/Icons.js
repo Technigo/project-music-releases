60ca8efd18c799c5f3592bbaa2c6a036165b1e2d
jakobxlindstrom <jake@lindstroem.se>
2021-10-15 23:38:53 +0200

    .

--- code/src/components/Icons.js
+++ code/src/components/Icons.js
@@ -1,17 +1,12 @@
 import React from 'react'
 
-const Icons = (props) => {
+const Icons = () => {
   return (
-    <>
-      <div className="icon-container">
-        <img src="/icons/heart.svg" alt="heart" className="heart icons" />
-        {/* {i cant seem to get the link to work} */}
-        <a href={props.titleUrl} target="_blank" rel="noopener noreferrer">
-          <img src="/icons/play.svg" alt="play" className="play icons" />
-        </a>
-        <img src="/icons/dots.svg" alt="dots" className="dots icons" />
-      </div>
-    </>
+    <div className="icon-container">
+      <img src="/icons/heart.svg" alt="heart" className="heart icons" />
+      <img src="/icons/play.svg" alt="play" className="play icons" />
+      <img src="/icons/dots.svg" alt="dots" className="dots icons" />
+    </div>
   )
 }
 
