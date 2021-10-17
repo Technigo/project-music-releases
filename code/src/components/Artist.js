e808ebc92ec03f9515c29026f540ec39b8ae2236
AschwinSiaila <aschwin76@hotmail.com>
2021-10-17 19:55:15 +0200

    merging branches

--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import "./artist.css";
 
-    //Here's the component to extract the artist information
+//Here's the component to extract the artist information
 export const Artist = (props) => {
-    return (
-        <a className="artist-title" href={props.item.external_urls.spotify}>
-            {props.item.name}
-        </a>
-    )
-}
+  console.log(props);
+  return (
+    <a className="artist-title" href={props.item.external_urls.spotify}>
+      {props.item.name}
+    </a>
+  );
+};
