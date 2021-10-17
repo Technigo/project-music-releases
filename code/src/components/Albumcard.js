829aeb7459d011bf2890d8eaaa863e31e17d4cbf
MarySnopok <marysnopok8@gmail.com>
2021-10-17 19:21:02 +0200

    sligt change in links

--- code/src/components/Albumcard.js
+++ code/src/components/Albumcard.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import { Overlay } from 'components/Overlay'
+import React from "react";
+import { Overlay } from "components/Overlay";
 
 export const Albumcard = (props) => {
   return (
     <section>
-      <article className='album-container'>
-        <div className='cover-container'>
-          <div className='overlay-container'>
+      <article className="album-container">
+        <div className="cover-container">
+          <div className="overlay-container">
             <Overlay />
           </div>
-          <div className='image-container'>
-            <img alt={props.name} className='song-img' src={props.image} />
+          <div className="image-container">
+            <img alt={props.name} className="song-img" src={props.image} />
           </div>
         </div>
-        <h2 className='album-name'>
-          <a href={props.hrefAlbum} target='blank' rel='noopener noreferrer'>
+        <h2 className="album-name">
+          <a href={props.hrefAlbum} target="blank" rel="noopener noreferrer">
             {props.name}
           </a>
         </h2>
-        <h3 className='artists'>
-          <a href={props.hrefArtists} target='blank' rel='noopener noreferrer'>
-            {props.artists}
-          </a>
-        </h3>
+        <h3 className="artists">{props.artists}</h3>
       </article>
     </section>
-  )
-}
+  );
+};
