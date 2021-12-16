75283c1a7833ee31983103178a070ef715bae518
Jsfrulle <Jessicamhermansson@gmail.com>
2021-12-16 14:16:35 +0100

    fix mapping

--- code/src/App.js
+++ code/src/App.js
@@ -3,38 +3,25 @@ import data from "./data.json";
 import Album from "components/Album";
 import Header from "components/Header";
 
-const albumArray = data.albums.items
+const albumArray = data.albums.items;
 
 export const App = () => {
-	return (
+  return (
     <div className="container">
-      <Header/>
-    <div className="album-container">
-
-      {albumArray.map((album) => {
-        return (
-          <Album
-            key={album.name}
-            title={album.name}
-            cover={album.images[0].url}
-            albumurl={album.external_urls.spotify}
-            artist={album.artists.map((item) =>
-              <div key={item.id}>
-                <div>
-                  <a className="link" target="_blank" rel="noopener noreferrer" href={album.external_urls.spotify}>
-                  <h2 className="album-title">{album.name}</h2></a>
-                </div>
-                <div>
-                  <a className="link" target="_blank" rel="noopener noreferrer" href={item.external_urls.spotify}>
-                  <h3 className="artist-name">{item.name}</h3></a>
-                </div>
-              </div>
-            )
-            }
-          />
-        )
-      })}
+      <Header />
+      <div className="album-container">
+        {albumArray.map((album) => {
+          return (
+            <Album
+              key={album.artists.id}
+              artists={album.artists}
+              name={album.name}
+              cover={album.images[0].url}
+              albumurl={album.external_urls.spotify}
+            />
+          );
+        })}
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,27 +1,57 @@
 import React from "react";
-import { ReactComponent as LogoOne } from 'icons/heart.svg'
-import { ReactComponent as LogoTwo } from 'icons/play.svg'
-import { ReactComponent as LogoThree } from 'icons/dots.svg'
+import { ReactComponent as LogoOne } from "icons/heart.svg";
+import { ReactComponent as LogoTwo } from "icons/play.svg";
+import { ReactComponent as LogoThree } from "icons/dots.svg";
 
 const Album = (album) => {
-	return (
-		<div className="albums">
+  return (
+    <div className="albums">
+      <div className="img-container">
+        <div className="overlay">
+          <div className="logo-heart">
+            {" "}
+            <LogoOne />
+          </div>
+          <div className="logo-play">
+            {" "}
+            <LogoTwo />
+          </div>
+          <div className="logo-dot">
+            {" "}
+            <LogoThree />
+          </div>
+        </div>
+        <img className="image" src={album.cover} alt="album cover" />
+      </div>
 
-		<div className="img-container">
-			<div className="overlay">
-				<div className="logo-heart"> <LogoOne /></div>
-				<div className="logo-play"> <LogoTwo /></div>
-				<div className="logo-dot"> <LogoThree /></div>	
-			</div>     
-			<img className="image" src={album.cover} alt="album cover"/>
-		</div>
+      <div className="album-textcontainer">
+        <a
+          className="album-title"
+          href={album.albumurl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="album-title"> {album.name} </h2>
+        </a>
+        <div className="artist-text-container">
+          {album.artists.map((artist) => {
+            return (
+              <div key={artist.id} className="inner-artist-container">
+                <a
+                  className="artist-name"
+                  href={artist.external_urls.spotify}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {artist.name}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-		<div className="album-textcontainer">
-			<h2 className="album-title"> {album.name} </h2>
-			<h3 className="artist-name">{album.artist}</h3>		
-		</div>	
-		</div>
-	)
-}
-
-export default Album
+export default Album;
