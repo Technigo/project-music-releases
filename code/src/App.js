016447dd233e30bf82ab004a34a3bfba369cf69a
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-03-07 20:00:16 +0100

    Co-authored-by: Sofie Pellegrini <SofiePellegrini@users.noreply.github.com>

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import data from './data.json'
 
+import { Album } from './components/Album'
+import { Header} from './components/Header'
+
 console.log(data)
+
+const albumsArray = data.albums.items
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <>
+    <header>
+      <Header />
+    </header>
+    <main className="albums-container">
+    
+
+      {albumsArray.map((album) => {
+        return (
+        <>
+        <Album 
+        key={album.name}
+        image={album.images[0].url}
+        title={album.name}
+        href={album.external_urls.spotify}
+        album={album.artists}
+        />
+        </>
+      )
+   })}
+
+   </main>
+   </>
+   )
+  }
---
+++ code/src/components/Album.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { Icons } from '../components/Icons'
+
+export const Album = (props) => {
+    console.log('PROPS'. props)
+    return (
+        <div className="card">
+            <div className="img-container">
+                <a href={props.href} target="_blank" rel="noopener noreferrer">
+                    <img src={props.image} alt="Album Cover" className="album-img"/>
+                    <Icons />
+                </a>
+            </div>
+        <a href={props.href} target="_blank" rel="noopener noreferrer">
+            <p className="albumTitle">{props.title}</p>
+        </a>
+        <div className="artistName">
+            {props.album.map(artist => {
+             return (
+             <a className="artistLink" key={artist.id} 
+                href={artist.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                {artist.name}
+            </a>)   
+            })}
+        </div>     
+        </div>
+    );
+};
+
