e266408106ac1c3dead72372a9295722a07d3fe3
Marianne Ardin <55622908+MarianneArdin@users.noreply.github.com>
2022-03-07 14:29:03 +0100

    test

--- code/src/App.js
+++ code/src/App.js
@@ -2,10 +2,24 @@ import React from 'react'
 import data from './data.json'
 
 console.log(data)
+const MovieData= () => {
+  const AlbumName= data.albums.items.map(album => {
+    return (<div className="display">
+      <img src={album.images[0].url}></img>
+      <p> {
+   album.name
+   } </p>
+      <p> {album.href} </p>
+      <p> {album.album_type} </p>
+      </div> )
+  })
+  return AlbumName
+}
 
 export const App = () => {
   return (
-    <div>
+    <div className='displayAlbum'>
+    <MovieData/>
       Find me in src/app.js!
     </div>
   )
