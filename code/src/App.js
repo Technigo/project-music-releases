e0b473c47416a3b95a4373c7cafb973c02adb71e
Louisa <lundislon@gmail.com>
2022-03-07 11:58:53 +0100

    monday live session

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
-import data from './data.json'
 
-console.log(data)
+import { Article } from 'components/Article'
+// import data from './data.json'
+
+// console.log(data)
+// divs down
+
 
 export const App = () => {
-  return (
-    <div>
-      Find me in src/app.js!
-    </div>
+  return (<section>
+    <Article 
+    title="Hello Technigo!"
+    description="some text"
+    />
+    <Article 
+    title="second!"
+    description="some other"
+    />
+  </section>
+    // <article className="article"> 
+    //   <h2>Hello Technigo!</h2>
+    //   <p>some text</p>
+    //   <p>date </p>
+    //   <img src="https://www.placecage.com/200/300" alt="nicolas"/>
+    // <div className="tag-container">
+    //   <span className="tag">programming</span>
+    //   <span className="tag">react</span>
+    //   <span className="tag">learning</span>
+    // </div>
+    // </article>
   )
 }
