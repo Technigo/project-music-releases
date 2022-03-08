6ddc2ce62001796b3848be230530ee7987edeeb5
Isabell Wästfelt <isabell@iMac-som-tillhor-Isabell.local>
2022-03-08 15:53:24 +0100

    added components and did some import/export

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import data from './data.json'
+import React from "react";
+import data from "./data.json";
 
-console.log(data)
+import { Album } from "./components/Album";
+// import { Artist } from "./components/Artist";
+
+console.log(data);
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <section>
+      <Album />
+    </section>
+  );
+};
---
+++ code/src/components/Album.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Album = (props) => {
+  return (
+    <div className="album">
+      <img
+        src="https://i.scdn.co/image/6d61103bd4043701d6cd230a02fa43d916c4f144"
+        alt="blabla"
+      />
+      <p>Don't Start Now</p>
+      <p>Dua Lipa</p>
+    </div>
+  );
+};
