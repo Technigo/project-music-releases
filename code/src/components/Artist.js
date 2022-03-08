46142f0fad6923066c611129ab6d983aa3b586cc
Louisa <lundislon@gmail.com>
2022-03-08 12:48:39 +0100

    created artistList for links to artists Spotify page

--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -3,11 +3,33 @@ import React from 'react';
 export const Artist = (props) => {
     const artist = props.artist
     // console.log(props.artist[0].name)
+
     const artistList = artist.map((artist) =>
-    artist.name)
-    console.log(artistList)
+        <div>
+            <a href={artist.external_urls.spotify}>{artist.name}</a>
+        </div>
+    )
+
+    console.log(artistList.length)
+    const artistLink = artist.map((artist) => 
+    artist.external_urls.spotify)
+    // console.log(artistLink)
+    // <p><a href='link'> artist <a>, <a href= link> artist <a></p>
+    // artistList = artistList.join(', ')
+    // console.log(arti)
+    let newList = []
+    if (artistList.length > 1) {
+        // artistList = artistList.join(', ')
+        artistList.forEach(item =>
+            newList.push(item)   
+        )
+    }
+
+    console.log(newList)
+    // console.log(artistList.join(', '))
     return (
         // props.artist[0].name
-        artistList.join(", ")
+        artistList
+        
         )
 }
