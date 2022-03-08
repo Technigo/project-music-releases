adf732c63ca2bda2d35712450963f1407cbc086c
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-03-08 20:17:55 +0100

    Co-authored-by: Sofie Pellegrini <SofiePellegrini@users.noreply.github.com>

--- code/src/components/Icons.js
+++ code/src/components/Icons.js
@@ -5,7 +5,7 @@ export const Icons = () => {
         <div className="iconsContainer">
             <img className="heart-icon icons" src="../icons/heart.svg" alt="heart-icon" />
             <img className="play-icon icons" src="../icons/play.svg" alt="play-icon" />
-            <img className="dots-icon icons" src="../icons/dot.svg" alt="dots-icon" />
+            <img className="dots-icon icons" src="../icons/dots.svg" alt="dots-icon" />
         </div>
     );
 };
