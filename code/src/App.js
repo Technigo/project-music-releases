66e7601bbe4c0de999c12d6ddccc4eb5f27cf39f
Ida Halling <idahalling@Idas-MacBook-Pro.local>
2022-03-09 15:25:22 +0100

    added album info to album in components, importet in app.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import data from './data.json'
+import Album from './components/Album'
 
-console.log(data)
+console.log(data.albums.items[3].name)
+
+
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-    <div>Testing testing</div>
+		// <section> 
+		// 	{data.album.items.map((Album)) => {
+		// 	return <Album key=[items.id] articleDetails={singleArticle}/>
+		// })}
+		// </section>
+
+    <div>hej</div>
   )
 }
+
---
+++ code/src/components/Album.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Album = () => {
+    return (
+        <article className="album-card">
+            <div className="image-container">
+                <img src={albums.item.images[0].url} alt="album cover"/>
+            </div>
+            <div className="text-wrapper">
+                <p className="album-title">Album Title X</p>
+                <p className="artist-name">Artist Name X</p>
+            </div>
+        </article>
+    )
+}
+
+export default Album 
