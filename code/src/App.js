93a9a21f19b516f45bdc84efc3b6a5da7ce57faa
Jacob <jacob@engd.se>
2022-03-09 23:44:52 +0100

    added Title and worked on hover effects

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import MusicInfo from 'components/MusicInfo'
 import data from 'data.json'
+import Header from 'components/Header'
+import Hover from 'components/Hover'
 
 //console.log(data)
 
 export const App = () => {
 	return (
-    <section className="page-wrapper">
+    <div className="page-wrapper">
+      <Header />
       <MusicInfo musicList={data} />
-    </section>
+    </div>
 	)}
---
+++ code/src/components/Hover.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hover = (props) => {
+    return (
+      <div className="hover">
+
+        <div className="icons">
+          <img
+            className="icons-heart "
+            src="./icons/heart.svg"
+            alt="like icon"
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export default Hover;
