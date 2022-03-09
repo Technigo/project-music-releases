d5c8bad57215e061b9fe24135f376f4a43b1e516
Dorothea-B <barre.dorothea@gmail.com>
2022-03-09 08:25:23 +0100

    added album icons and hover styling

--- code/src/components/Article.js
+++ code/src/components/Article.js
@@ -3,7 +3,19 @@ import React from "react";
 export const Article = (props) => {
     return (
     <article className="album">
-      <img src="https://www.placecage.com/328/328" alt="album cover"/>
+
+      <div className="album-cover">
+                    <img className="album-image" src="https://www.placecage.com/328/328" alt="album cover" />
+
+                    <div className="icons">
+                      <img className="icon" src="/icons/heart.svg" alt="heart icon" />
+                      <img className="play" src="/icons/play.svg" alt="play icon" />
+                      <img className="icon" src="/icons/dots.svg" alt="dots icon" />
+                    </div>
+      </div>
+
+
+
       <p className="title">{props.type}</p>
       <p className="artist">{props.artist}</p>
     </article>
