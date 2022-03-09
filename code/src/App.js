35f10f1271ff0812c028c596a707eab86e89da1b
Emma Högberg <emmahogberg88@gmail.com>
2022-03-09 23:05:21 +0100

    add styling

--- code/src/App.js
+++ code/src/App.js
@@ -5,29 +5,25 @@ import { Header } from './components/Header'
 import { Album } from './components/Album'
 // import { Artist } from './components/Artist' 
 
-console.log('data:', data)
-console.log('album-cover', data.albums.items[0].images[1].url)
-console.log('album-name', data.albums.items[0].name)
-console.log('album-link', data.albums.items[0].external_urls.spotify)
-console.log('artist-name', data.albums.items[0].artists[0].name)
-console.log('artist-items', data.albums.items)
-
-
-
 export const App = () => {
+
+
    return (
     <section>
-      <Header />
-
+      <div className='header'>
+        <Header />
+      </div>
+      <div className='album-container'>
       {data.albums.items.map((albumData) => {
         return (
-          <Album 
-            key={albumData.id}
+          <Album
+          key={albumData.id}
             data={albumData}
-          />
-        )
-      })
-    }
+            />
+            )
+          })
+        }
+      </div>
     </section>
     )
   }
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -4,22 +4,23 @@ import React from 'react'
  
 export const Album = (props) => {
   const albums = props.data
-  console.log(albums)
 
-  const artists = albums.artists.map(artist => {
+  const artists = albums.artists.map((artist) => {
     return (
         <h3>{artist.name}</h3>
         )
       })
 
   return (
-    <article>
-      <img src={albums.images[1].url} alt='album cover'/>
-      <a href={albums.external_urls.spotify}>
-      <h2>{albums.name}</h2>
-      </a>
-      {artists}
-    </article>
+    <div className='album-container'>
+      <article>
+        <img src={albums.images[1].url} alt='album cover'/>
+        <a className='nav-link' href={albums.external_urls.spotify}>
+        <h2>{albums.name}</h2>
+        </a>
+        {artists}
+      </article>
+    </div>
        
        )
       }
