f18d64d56b030aa25a8eaef02df3cd6a2649eb80
Dorothea-B <barre.dorothea@gmail.com>
2022-03-10 13:23:28 +0100

    sorted albums from singles and added playlists-section

--- code/src/App.js
+++ code/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import data from './data.json'
+import playlistdata from './stretch-goal.json'
 
-import { Article } from 'components/Article'
+
+// import { Article } from 'components/Article'
+import { Albums } from 'components/Albums'
+import { Singles } from 'components/Singles'
+import { Playlists } from 'components/Playlists'
 import { Header } from 'components/Header'
 
 console.log('data', data)
+console.log('PLAYLISTS', playlistdata)
+
 
 export const App = () => {
 
@@ -12,9 +19,30 @@ export const App = () => {
     
   <div>
       <Header />
+
+      <h2>New Albums</h2>
+
       <section>
-      {data.albums.items.map((singleArticle) => {
-        return <Article key={singleArticle.id} articleDetails={singleArticle}/>
+      {data.albums.items.map((singleAlbum) => {
+        if (singleAlbum.album_type === "album")
+        return <Albums key={singleAlbum.id} articleDetails={singleAlbum}/>
+      })}
+      </section>
+
+      <h2>New Singles</h2>
+
+      <section>
+      {data.albums.items.map((singleSingle) => {
+        if (singleSingle.album_type === "single")
+        return <Singles key={singleSingle.id} articleDetails={singleSingle}/>
+      })}
+     </section>
+
+     <h2>Playlists</h2>
+
+      <section>
+      {playlistdata.playlists.items.map((singleList) => {
+        return <Playlists key={singleList.id} articleDetails={singleList}/>
       })}
      </section>
   </div>
---
+++ code/src/components/Albums.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const Albums = (props) => {
+  
+    return (
+    <article className="album">
+      {/* ===== ALBUM COVER ===== */}
+      <div className="album-cover">
+        <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+          <img className="album-image" src={props.articleDetails.images[1].url} alt="album cover" />
+        </a>         
+        <div className="icons">
+          <img className="icon" src="/icons/heart.svg" alt="heart icon" />
+          <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+            <img className="play" src="/icons/play.svg" alt="play icon" />
+          </a> 
+          <img className="icon" src="/icons/dots.svg" alt="dots icon" />
+        </div>
+      </div>
+      {/* ===== ALBUM TITLE ===== */}
+      <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="title">{props.articleDetails.name}</a>
+
+      {/* ===== ARTIST ===== */}
+      <div className="artist-container">
+       {props.articleDetails.artists.map((eachartist) => {
+       return <a href={eachartist.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="artist" key={eachartist.id} >{eachartist.name} </a>
+     })}</div>
+    </article>
+    )
+}
+      
+
+
+       
+   
