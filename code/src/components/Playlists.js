8201f2bcaad4267498cb496e07b59f62c8aa7a20
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-03-10 22:41:28 +0200

    small changes to styling

--- code/src/components/Playlists.js
+++ code/src/components/Playlists.js
@@ -7,7 +7,7 @@ export const Playlists = (props) => {
       {/* ===== ALBUM COVER ===== */}
       <div className="album-cover">
 
-      <div >
+      
         <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer">
           <img className="album-image" src={props.articleDetails.images[0].url} alt="album cover" />
         </a>
@@ -20,10 +20,11 @@ export const Playlists = (props) => {
         </div>
         {/* ===== PLAYLIST TITLE =====*/}
       </div >
-      <div className="listname-container">
+      
+      
         <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="title">{props.articleDetails.name}</a>
-        </div>
-      </div>
+      
+      
       
     </article>
     )
