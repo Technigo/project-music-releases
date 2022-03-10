873b0bbbf4e5a4546808a973bde3b53b41dbce20
Neaa99 <linnea@kringelkroken.se>
2022-03-10 11:00:05 +0100

    a

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -24,7 +24,7 @@ const Album = (props) => {
 
         <section className="song-info">
           <div className="song-name">
-            <a href="#">{props.albumName}</a>
+            <a href="google.se">{props.albumName}</a>
           </div>
           <div className="song-artist">
             {props.artist.map((artist) => (
