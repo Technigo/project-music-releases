dd399e82fc217ba0fa289826e559806fc74ab325
motherofbeasts <91845855+motherofbeasts@users.noreply.github.com>
2022-03-10 22:27:25 +0100

    added icons

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Icons} from '../components/AllIcons'
 
 export const Album = (props) => {
     // console.log(props)
@@ -6,13 +7,14 @@ export const Album = (props) => {
     return (
             <article className="all-albums">
                 <a href={props.albumDetails.external_urls.spotify} target="_blank">
-                    <img src={props.albumDetails.images.[1].url} alt="Album Cover" />
+                    <img src={props.albumDetails.images[1].url} alt="Album Cover" />
                 </a>
                 <a href={props.albumDetails.external_urls.spotify} target="_blank">
                     <p className="album-name">{props.albumDetails.name}</p>
                 </a>
-                <a href={props.albumDetails.artists.[0].external_urls.spotify} target="_blank">
-                    <p className="artist-name">{props.albumDetails.artists.[0].name}</p>
+                <a href={props.albumDetails.artists[0].external_urls.spotify} target="_blank">
+                    <p className="artist-name">{props.albumDetails.artists[0].name}</p>
+                    <AllIcons />
                 </a>
             </article>
     );
---
+++ code/src/components/IconFunctions/AllIcons.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const AllIcons = () => {
+    return (
+        <div className="IconContainer">
+            <img className="iconHeart icons" src="../Icons/heart.png" alt="A heart icon that works as a button"/>
+            <img className="iconPlay icons" src="../Icons/play.png" alt="The icon of a play button"/>
+            <img className="iconEllipsis icons" src="..//Icons/ellipsis.png" alt="An elipsis icon that works as a button"/>
+            </div>
+    );
+};
