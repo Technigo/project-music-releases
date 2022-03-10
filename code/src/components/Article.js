0388bdd2109f4fccc07f608ea8b67be0380112fb
Kristiina Kolu <kristiinakolu@Maxs-MacBook-Air.local>
2022-03-10 11:20:52 +0200

    added links to albums and artists

--- code/src/components/Article.js
+++ code/src/components/Article.js
@@ -1,26 +1,29 @@
 import React from "react";
 
 export const Article = (props) => {
-  console.log('props:', props)
+  
     return (
     <article className="album">
 
       <div className="album-cover">
-                    <img className="album-image" src={props.articleDetails.images[1].url} alt="album cover" />
-
-                    <div className="icons">
-                      <img className="icon" src="/icons/heart.svg" alt="heart icon" />
-                      <img className="play" src="/icons/play.svg" alt="play icon" />
-                      <img className="icon" src="/icons/dots.svg" alt="dots icon" />
-                    </div>
+        <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+          <img className="album-image" src={props.articleDetails.images[1].url} alt="album cover" />
+        </a>         
+        <div className="icons">
+          <img className="icon" src="/icons/heart.svg" alt="heart icon" />
+          <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+            <img className="play" src="/icons/play.svg" alt="play icon" />
+          </a> 
+          <img className="icon" src="/icons/dots.svg" alt="dots icon" />
+        </div>
       </div>
       {/* ===== ALBUM TITLE ===== */}
-      <p className="title">{props.articleDetails.name}</p>
+      <a href={props.articleDetails.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="title">{props.articleDetails.name}</a>
 
       {/* ===== ARTIST ===== */}
       <div className="artist-container">
        {props.articleDetails.artists.map((eachartist) => {
-       return <p className="artist" key={eachartist.id} >{eachartist.name} </p>
+       return <a href={eachartist.external_urls.spotify} target="_blank" rel="noopener noreferrer" className="artist" key={eachartist.id} >{eachartist.name} </a>
      })}</div>
     </article>
     )
