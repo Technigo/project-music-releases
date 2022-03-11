a5068ea1502161b398e31dee44de86206c2e3971
Joanna Lodell <joannalodell@c80-216-119-65.bredband.tele2.se>
2022-03-11 11:55:23 +0100

    [JL] linked album to spotify

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -6,14 +6,16 @@ const Album = (props) => {
 
     return (
         <article className="album-card">
-            <div className="image-container">
-                <Icon />
-                <img src={props.item.images[1].url} className="album-image" alt="album cover"/>
-            </div>
+            <a className="linked-album" href={props.item.external_urls.spotify} alt="link to album">      
+                <div className="image-container">
+                    <Icon />
+                    <img src={props.item.images[1].url} className="album-image" alt="album cover"/>
+                </div>
+            </a>
             <div className="text-wrapper">
                 <h3 className="album-title">{props.item.name}</h3>
                 <h4 className="artist-name"> <Artist artistArray={props.item.artists} /></h4>
-                <a className="linked-artist" href={props.artistLink} alt="link to artist">Test</a>
+                <a className="linked-artist" href={props.artistLink} alt="link to artist"> </a>
             </div>
          </article>
     )
