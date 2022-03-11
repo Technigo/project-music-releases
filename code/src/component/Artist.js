e22f940db8eb467ff6328f54d77fcf3d01758ca8
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-03-11 16:33:17 +0100

    final version

--- code/src/component/Artist.js
+++ code/src/component/Artist.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
 const Artist = (props) => {
-    console.log('artister', props)
+  
     return (
         <div>
-        <h3 className="artist-name">        
-          {props.artistArray.map((artist) =>
-          <a 
-          href={artist.external_urls.spotify}
-          target="_blank"
-          rel="noopener noreferrer"
-          >
-          <span>{artist.name}</span></a>)}
-        </h3>
-      </div>
+            <h3 className="artist-name">        
+                {props.artistArray.map((artist) =>
+                <a key={artist.id}
+                href={artist.external_urls.spotify}
+                target="_blank"
+                rel="noopener noreferrer">
+                <span>{artist.name}</span></a>)}
+            </h3>
+        </div>
     )
 }
 
@@ -24,26 +23,4 @@ export default Artist
 
 
 
-// import React from 'react'
-
-// const Artist = (props) => {
-
-//     console.log(props, 'hejhej')
-//     return (
-//       <span>{props.item.name} </span>
-//     )
-//   }
-
-//   export default Artist
-
-// import React from "react";
-
-
-// export const Artist = (props) => {
-//   return (
-//     <p className="artist">
-//       <a href={props.url}>{props.name}</a>
-//     </p>
-//   );
-// };
 
---
+++ code/src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+      <div className="header-container">
+        <h1>New albums &amp; singles</h1>
+      </div>
+    )
+}
+
+export default Header
+  
