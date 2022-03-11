291b4ccc96b657c8608ef94c7ed19c59c705eb0b
sofiaannajonsson <84452023+sofiaannajonsson@users.noreply.github.com>
2022-03-11 11:25:03 +0100

    fixed responsiveness bug

--- code/src/components/Albums.js
+++ code/src/components/Albums.js
@@ -4,20 +4,20 @@ import Hover from 'components/Hover'
 export const Albums = (props) => {
   return (
     <div className='artistCard'>
-          <div class="image-wrapper">
-      <a href={props.albumURL}>
-        <Hover />
-      <div className='album-cover'>
-        <img className='album-image' src={props.image} alt='Album' />
+      <div class="image-wrapper">
+        <a href={props.albumURL}>
+          <Hover />
+        <div className='album-cover'>
+          <img className='album-image' src={props.image} alt='Album' />
+        </div>
+        </a>
       </div>
-      </a>
-          </div>
-          <div className='album-text'>
-          <div className='album-name'>
-            <a href={props.albumURL} target="_blank">{props.albumTitle}</a>
+      <div className='album-text'>
+        <div className='album-name'>
+          <a href={props.albumURL} target="_blank">{props.albumTitle}</a>
           <div className='artist-name'>
-          <a href={props.artistURL} target="_blank">{props.artists}</a>
-          </div>
+           <a href={props.artistURL} target="_blank">{props.artists}</a>
+         </div>
         </div >
       </div >
     </div >
