83cd3e934abc663a2af0046583b64fe8f4df61a4
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-03-13 18:13:50 +0100

    Changed logic for commas

--- code/src/App.js
+++ code/src/App.js
@@ -6,65 +6,38 @@ import Albuminfo from './components/Albuminfo.js'
 import Artistinfo from './components/Artistinfo.js'
 import Header from './components/Header.js'
 
-console.log(data)
-
 export const App = () => {
   const itemArray = data.albums.items
+
+  const getArtistsInfo = artists => {
+    let newArray = artists.map((artist, index) => {
+      const lastIndex = artists.length - 1;
+      let artistName = '';
+
+      if(index === lastIndex) {
+        artistName = artist.name;
+      } else {
+        artistName = artist.name + ", ";
+      }
+      
+      return ( 
+        <Artistinfo 
+          key={artist.id}            
+          artist={artistName}            
+          artistUrl={artist.external_urls.spotify}
+        />
+      )
+      })
+      console.log(newArray)
+      return newArray;
+  }
   
   return (
     <section>
       <Header />
       <article className="grid-container">
       {itemArray.map(item => {
-        //console.log(item.artists.length)
         let artistNames = item.artists;
-        console.log(artistNames);
-        // artistNames.map((artist) => {
-        //   console.log(artist)
-        //   return (
-        //     <Albuminfo 
-        //     key={item.artists[0].id}
-        //     album={item.name}
-        //     artist={artist.name}
-        //     url={item.external_urls.spotify}
-        //     artistUrl={artist.external_urls.spotify}
-        //   />
-        //   )
-        // })
-
-        // if (item.artists.length >= 2) {
-          
-        //   let artistName = '';
-        //   let artistLink = '';
-        //   for (let i = 0; i < item.artists.length; i++) {
-        //     artistLink = item.artists[i].external_urls.spotify;
-        //     console.log(artistLink)
-        //     if (i === item.artists.length - 1) {
-        //       artistName += item.artists[i].name;                        
-        //     } else if (i === item.artists.length - 2 && i !== 0) {
-        //       artistName += item.artists[i].name + ' & ';              
-        //     } else {
-        //       artistName += item.artists[i].name + ', ';                   
-        //     }            
-        //   }
-        //   return (
-        //     <div className="grid-container-div">
-        //       <Albumcover 
-        //         key={item.id}
-        //         src={item.images[1].url} 
-        //         url={item.external_urls.spotify}
-        //       />
-        //       <Albuminfo 
-        //         key={item.artists[0].id}
-        //         album={item.name}
-        //         artist={artistName}
-        //         url={item.external_urls.spotify}
-        //         artistUrl={artistLink}
-        //       />
-        //     </div>            
-        //   )
-        // }  
-      
         return (
           <div className="grid-container-div">
             <Albumcover 
@@ -72,52 +45,15 @@ export const App = () => {
               src={item.images[1].url} 
               url={item.external_urls.spotify}
             />
-            <Albuminfo
+            <Albuminfo    
+            key={item.id + '1'}        
             album={item.name}
             url={item.external_urls.spotify}
             />
-
-          {artistNames.map((artist) => {
-            //console.log(artist)
-            console.log(artistNames.length)
-            if (artistNames.length === 1) {
-            return (
-              <Artistinfo 
-                key={item.artists[0].id}            
-                artist={artist.name + ' '}            
-                artistUrl={artist.external_urls.spotify}
-              />
-            )
-            //make it else if currentIndex = artistNames[artistNames.length-1]
-            //probably that if dont work in map, it will return what it returns, no matter condition?
-            } else if (artistNames.length >= 2) {      
-                if(artistNames[artistNames.length-1]) {
-                  console.log(artistNames[artistNames.length-1])
-                  return (
-                    <Artistinfo 
-                      key={item.artists[0].id}            
-                      artist={artist.name + ', '}            
-                      artistUrl={artist.external_urls.spotify}
-                    />  
-                  )
-                }
-              //  else {
-              //     return (
-              //       <Artistinfo 
-              //         key={item.artists[0].id}            
-              //         artist={artist.name + ', '}            
-              //         artistUrl={artist.external_urls.spotify}
-              //       />
-              //     )
-              //   }
-              
-              }
-            
-            })}
+          {getArtistsInfo(artistNames)}
           </div>
         )
       })}
-
       </article>
   </section>
   )
