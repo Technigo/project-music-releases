04f2566a3a38213fd66176983711f3f0535004bd
idanaslund <idanaslund@users.noreply.github.com>
2022-03-14 11:31:33 +0100

    removing code

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Icons } from './Icons'
 
 import Artist from './Artist'
-import propTypes from 'eslint-plugin-react/lib/rules/prop-types'
+
 
 // Named export
 const Album = (props) => {
