ff2f737715b5215c59b7d2e2ece8c4e8e591f160
Lisa Bergström <lisa.bergstrom@live.se>
2022-03-21 19:56:59 +0100

    changed small details in css

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -4,13 +4,13 @@ import { Icons } from "./Icons";
 
 export const Album = (props) => {
   return (
-    <div>
-      <div class="image-container">
-        <a href={props.url}>
-          <Icons />
-          <img src={props.img} alt="Album cover" className="album-cover" />
-        </a>
-      </div>
+    <div class="image-container">
+      <a href={props.url}>
+        <Icons />
+        <img src={props.img} alt="Album cover" className="album-cover" />
+      </a>
+
+      {/* </div> */}
       <a href={props.url}>
         <p className="albums"> {props.name}</p>
       </a>
