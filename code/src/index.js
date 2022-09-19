cb5ebebb8a446b796b1796f676fe772bf0416bd6
Scarylie <saralie.bognandi@gmail.com>
2022-09-19 16:13:04 +0200

    Optimized code for ReactDOM

--- code/src/index.js
+++ code/src/index.js
@@ -1,8 +1,8 @@
-import { createRoot } from 'react-dom';
-import './index.css';
-import { App } from './App';
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+//const container = document.getElementById('root');
+//const root = createRoot(container);
 
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
