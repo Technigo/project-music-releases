a51382dd40129c40d8a9dc0a87595823c5eb789b
Kneedler <sarahklindelow@gmail.com>
2022-09-20 13:54:52 +0200

    added file

--- code/src/index.js
+++ code/src/index.js
@@ -8,14 +8,14 @@ const root = createRoot(container);
 root.render(<App />);
 
 //från daniel:
-// import React from 'react';
-//import ReactDOM from 'react-dom/client';
-//import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import default exported App component
 // import App  from './App';
 
 // import named exported App component
-//import {App}  from './App';
+import { App } from "./App";
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
