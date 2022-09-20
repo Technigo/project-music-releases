e984dc54f0ec519867398eaea925d580f6b3899a
Linus Eriksson <erilinus@gmail.com>
2022-09-20 17:01:21 +0200

    resized icons, added image div for image and icons

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -8,8 +8,10 @@ const Album = (props) => {
       <>
         {albumsArray.map((item) => (
           <div className="album-card" key={item.id}>
-            <img src={item.images[1].url} className='album-image' alt='Cover'/>
-            <Icons />
+            <div className="image-container">   
+              <img src={item.images[1].url} className='album-image' alt='Cover'/>
+              <Icons />
+            </div>
             <a href={item.external_urls.spotify} className='album-name'>{item.name}</a>
             <div className="artists">{item.artists.map((artist) => (
               <a className='artist-name' href={artist.external_urls.spotify} key={artist.id}>{artist.name}</a>
