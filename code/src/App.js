c952eabb85bf8bef50549ea3316e3e0a0f92881f
fannycardell <fannycardell@gmail.com>
2022-09-21 11:10:08 +0200

    added first component header

--- code/src/App.js
+++ code/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import data from './data.json';
+import Header from './Components/Header';
 
 console.log(data);
 
 export const App = () => {
   return (
+    <>
+    <Header/>
     <div>
       Find me in src/app.js!
     </div>
+    </> 
   );
 }
---
+++ code/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <h2>
+      Test Header here we come
+    </h2>
+  );
+}
+
+export default Header;
