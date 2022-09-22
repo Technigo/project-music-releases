ab59a9f86653c79c4af1d0520c2eaad3f8edd83e
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-09-22 23:05:19 +0200

    starta om

--- code/src/App.js
+++ code/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import data from './data.json';
+<<<<<<< HEAD
 import './index.css';
 
 import { Header } from './Header';
+=======
+>>>>>>> parent of 2e5a36f (start)
 
 console.log(data);
 
 export const App = () => {
+<<<<<<< HEAD
   return <Header />;
 };
+=======
+  return (
+    <div>
+      Find me in src/app.js!
+    </div>
+  );
+}
+>>>>>>> parent of 2e5a36f (start)
