0c1b4555c6028c79926d8035d2c9854f31466145
a-well <amandaemkwellington@gmail.com>
2022-09-22 11:14:54 +0200

    Remove test text, clean up comments

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -5,7 +5,7 @@ const Album = (props) => {
   return (
     <div className="Album">
       <div className="Cover"><img src={props.img} alt="Album cover" /></div>
-      <p className="AlbumName">Album name: {props.name}</p>
+      <p className="AlbumName">{props.name}</p>
 
       {props.artists.map((artist) => (
         <Artist
