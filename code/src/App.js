f45e116c3bb554e9915c232e2c26496c1a0430c3
a-well <amandaemkwellington@gmail.com>
2022-09-22 16:29:45 +0200

    Add Header
    Co-authored-by: Annika Gadman <annika.gadman@gmail.com>

--- code/src/App.js
+++ code/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import data from './data.json';
 import AlbumCard from './components/AlbumCard';
+import Header from './components/Header';
 
 console.log(data);
 
 export const App = () => {
   return (
-    <div className="App">
-      {data.albums.items.map((album) => (
-
-        <AlbumCard
-          key={album.id}
-          name={album.name}
-          img={album.images[1].url}
-          artists={album.artists} />
-
-      ))};
-    </div>
+    <>
+      <header><Header /></header>
+      <div className="App">
+        {data.albums.items.map((album) => (
+          <AlbumCard
+            key={album.id}
+            name={album.name}
+            img={album.images[1].url}
+            artists={album.artists} />
+        ))};
+      </div>
+    </>
   );
 };
 
