9b1ab558c3dcfb3582dd22fd4146dfff79962c6e
Josefine Engdahl <josefineengdahl@MacBook-Air-som-tillhor-Josefine.local>
2022-09-22 10:16:53 +0200

    continued working..

--- code/src/App.js
+++ code/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import data from './data.json';
 import Card from './components/Card'
 import Header from './components/Header'
+// import Artist from './components/Artist'
 
 console.log(data);
 // named export
@@ -16,6 +17,10 @@ export const App = () => {
             <Card 
               key={item.id} 
               data = {item} 
+              // AlbumImage = {props.data.images[1].url} //sending the image link to the article component
+              // AlbumName = {props.data.name} //sending the album name to the article component
+              // AlbumLink = {props.data.external_urls.spotify} //sending the album url to the article component
+              // bandName = {props.data.artists[0].name}
             />
           </div>
         )
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -4,21 +4,51 @@ import React from 'react';
 const Artist = (props) => {
  
 return (
+    
+/* <h3 className="artist-name">
+{' '}
+<a href ={props.ArtistLink} target="_blank">
+{' '}
+{props.ArtistName}
+</a>
+ </h3>  */
 
-        <h3>
-            {props.ArtistName}
-            {props.ArtistLink}
-        </h3> 
+<a
+className="artistUrl"
+href={props.ArtistLink}
+target="_blank"
+rel="noopener noreferrer"
+>
+{' '}
+<h3>{props.ArtistName}</h3>
+</a>
 
-    /* return (
-        {props.data.artists.map((artist, i) => {
-            let divider = " ";
-            if (i < props.data.artists.length - 2) {
-            divider = ", ";
-            } else if (i === props.data.artists.length - 2) {
-            divider = " & ";
-            }})
-            }) */
+
+
+
+// span className
+// <a href={props.ArtistLink} >
+// {props.ArtistName}
+// </a>
+
+
+/* <span className="artistNameLink" key={item.id}>
+<a href={props.ArtistLink}>{props.ArtistName}</a>
+</span> */
+
+
+    // /* return (
+    //   
+    //     {props.data.artists.map((artist, i) => {
+    //         let divider = " ";
+    //         if (i < props.data.artists.length - 2) {
+    //         divider = ", ";
+    //         } else if (i === props.data.artists.length - 2) {
+    //         divider = " & ";
+    //         }})
+    //         }) */
+          
 )}
+
 
 export default Artist;
