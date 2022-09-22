ac27c385a5a8108ab283f7a2e0b1b18c3f60cd10
Scarylie <saralie.bognandi@gmail.com>
2022-09-22 09:57:57 +0200

    Icons added and style.css

--- code/src/components/AlbumCard.js
+++ code/src/components/AlbumCard.js
@@ -1,19 +1,29 @@
 /* eslint-disable no-undef */
-import React from 'react'
-import ArtistName from './ArtistName'
-import AlbumName from './AlbumName'
+import React from "react";
+import ArtistName from "./ArtistName";
+import AlbumName from "./AlbumName";
+import Icons from "./Icons";
 
 const AlbumCard = (props) => {
   return (
-    <section>
-      <img
-        src={props.album.images[1].url}
-        className="cover-img"
-        alt={props.album.name} />
+    <section className="album-card">
+      <div className="album-container">
+        <img
+          src={props.album.images[1].url}
+          className="cover-img"
+          alt={props.album.name}
+        />
+        <div className="hidden-icons">
+          <h2>
+            <Icons />
+          </h2>
+        </div>
+      </div>
+
       <AlbumName />
       <ArtistName artists={props.album.artists} />
     </section>
-  )
-}
+  );
+};
 
-export default AlbumCard
+export default AlbumCard;
