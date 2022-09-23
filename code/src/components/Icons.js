c34ef799a8d293c5110a4fe5e9c1435800d7bb5f
a-well <amandaemkwellington@gmail.com>
2022-09-23 12:40:58 +0200

    Fix icon class name

--- code/src/components/Icons.js
+++ code/src/components/Icons.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Icons = () => {
   return (
-    <div className="iconsContainer">
+    <div className="icons-container">
       <img className="heart-icon icons" src="../icons/heart.svg" alt="heart-icon" />
       <img className="play-icon icons" src="../icons/play.svg" alt="play-icon" />
       <img className="dots-icon icons" src="../icons/dots.svg" alt="dots-icon" />
