7839372bcff74064968ce6dfbb37a22b73e032fc
Kneedler <sarahklindelow@gmail.com>
2022-09-23 10:02:02 +0200

    trying out comma

--- code/src/components/ArtistName.js
+++ code/src/components/ArtistName.js
@@ -1,13 +1,22 @@
 // import React from "react";
 
+const separator = (index, length) => {
+  if (index === length - 2) {
+    return ' & ';
+  } else if (index < length - 2) {
+    return ', ';
+  }
+  return;
+};
+
 const ArtistName = (props) => {
   // const artistArray = props; //.data.artists;
   console.log(props);
   // return <p> {props.artists[0].name}</p>;
   if (props.artists.length > 1) {
-    const allArtists = props.artists.map((artist) => {
+    const allArtists = props.artists.map((artist, index) => {
       console.log(`artistname:${artist.name}`);
-      return `${artist.name} `;
+      return `${artist.name} ${separator(index, allArtists.length)}`;
     });
     return allArtists;
   } else {
