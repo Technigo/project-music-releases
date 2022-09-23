e3ea2f5280a530ce2f0bc5e24cbd58d3d5745230
KajaWilbik <wilbik.kaja@gmail.com>
2022-09-23 21:45:49 +0200

    fixed link onClick play

--- code/src/App.js
+++ code/src/App.js
@@ -3,32 +3,34 @@ import data from './data.json';
 import { ArtistName } from './components/ArtistName';
 import { SongTitle } from './components/SongTitle';  //showing in html
 import { AlbumCover } from './components/AlbumCover';
-//import { TheAlbum } from './components/TheAlbum'
+
+
 console.log(data);
 
-/*<AlbumCover AlbumCover= {album.images[0].url}/>*/
-/* <div className='innit'> 
-                      {data && data.albums.map(item  =>{
-                        return <AlbumCover AlbumCover = {props.AlbumCover}
-                        name = {props.name}/>
-                      })}
-                    </div>*/ 
 export const App = () => {
   return(
-    <div className='Outline'> 
-        <div className='Album-Wrapper'>
-          <h1>New albums & singles</h1>
-              <section className="The-Album">     
-                  <div className='Album-Picture'>
+      
+        <div className='Outline'> 
+          <div className='Album-Wrapper'>
+            <h1>New albums & singles</h1>
+                <section className="The-Album">     
+                    <div className='Album-Picture'>
                     {data.albums.items.map(item => {
                       return <ArtistName key = {data.id} data = {item} text = "Test"/> //declaring a property called data ,item is the value
                       })}
-                    <AlbumCover key = {data.id} AlbumCover = {data.AlbumCover}/> 
+                    <AlbumCover key = {data.id} AlbumCover = {data.AlbumCover}/>
+                    {data.albums.items.map(item => {
+                      console.log('image url', item.images[2])
+                      return 
+                    <>
+                        <ArtistName key={data.id} data={item} /> //declaring a property called data ,item is the valu
+                        <AlbumCover key={data.id} AlbumCover={item.images[2].url} /></> 
+                    })
+                  } 
 
                 </div>
             </section>
         </div>
     </div>
-    )
-
-}
+   )
+  }
--- code/src/components/ArtistName.js
+++ code/src/components/ArtistName.js
@@ -3,22 +3,30 @@ import { SongTitle } from "./SongTitle";
 import { AlbumCover } from "./AlbumCover";
 
 export const ArtistName = (props) => {      // this is the song name
-    console.log(props.data)                 //checking the extraction in the console
+    console.log(props.data)   
+    console.log('webside', props.data.external_urls.spotify);              //checking the extraction in the console
     return (
 
         <>
     <div className="Box">
-        <h2 className="Song">Song: {props.data.name}</h2>          
-        <SongTitle Artist = {props.data.artists[0].name}/> 
-        
-        
+        <div className="title">
+            <h2 className="Song">Song: {props.data.name}</h2>          
+            <SongTitle Artist = {props.data.artists[0].name}/> 
+            
+        </div>
         <div className="icon-container"> 
-            <a><img className="Heart-Icon" src="/icons/heart.svg" alt="Heart-Icon"/></a>
-            <img className="Play-Icon" src="/icons/play.svg" alt="Play-Icon" /> 
+            <img className="Heart-Icon" src="/icons/heart.svg" alt="Heart-Icon"/>
+            <a href={props.data.external_urls.spotify}
+                target="_blank">
+                <img className="Play-Icon" src="/icons/play.svg" alt="Play-Icon" /> 
+            </a>
             <img className="Dot-Icon" src="/icons/dots.svg" alt="Dot-Icon" /> 
         </div> 
         
     </div>
         </>   
-    )
+       
+    ) 
+    
+   
 }
