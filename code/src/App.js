9130e79b3f2d9633d7095b40a2768477f85b0d3f
David Ballester <dballesterfont@gmail.com>
2022-09-23 14:58:28 +0200

    Deploying test 5

--- code/src/App.js
+++ code/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import data from './data.json';
 
-import { MusicCard } from './components/MusicCard'
+import { MusicCard } from '../../MusicCard'
 
 console.log(data);
 export const App = () => {
