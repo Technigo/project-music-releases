0cee54723eff3e7c29a77f46ac93031cb3afbca9
Annika Gadman <annika.gadman@gmail.com>
2022-09-22 22:34:28 -0500

    added icons and hover

--- code/src/components/AlbumCard.js
+++ code/src/components/AlbumCard.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Icons } from './Icons';
 
 const AlbumCard = (props) => {
   return (
     <div>
       <div className="Cover">
+        <Icons />
         <a href={props.url}>
           <img src={props.img} alt="Album cover" className="album-cover" />
         </a>
