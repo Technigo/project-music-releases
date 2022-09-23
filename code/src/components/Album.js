c38f775e4eea8116f4b76cf0d6017aafbdac5c3c
Kristin <kristin@iMac.local>
2022-09-23 15:49:16 +0200

    Finalised CSS

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -6,35 +6,47 @@ const Album = ({ coverImage, albumUrl, albumName, albumArtists}) => {
   // console.log("albumArtists", albumArtists);
 
   return (
-    <div className="albumsWrapper">
-      {/* albumwrapper from APP.JS */}
+    <div className="albumWrapper">
       <div className="imageWrapper">
-        <div className="icon-image-wrapper">
-        <img className="albumImage" src={coverImage} alt="album cover" />
-          <div className="icon-wrapper">
-            <img className="icon" src="./icons/heart.svg" />
-              <div className="playWrapper">
+      <img className="albumImage" src={coverImage} alt="album cover" />
+      <div className="icon-wrapper">
+      <img className="icon" src="./icons/heart.svg" />
+      <div className="playWrapper">
                 <a href={albumUrl}>
                 <img className="playIcon" src="./icons/play.svg" />
                 </a>
               </div>
-            <img className="icon" src="./icons/dots.svg" />
-          </div>
-          </div>
-          
-        <h2 className="albumName">
+      <img className="icon" src="./icons/dots.svg" />
+      </div>
+   </div>
+      <h2 className="albumName">
           <a href={albumUrl}>{albumName}</a>
-        </h2>
-
+      </h2>
       <div className="artistWrapper">
         <Artists 
         artistList={albumArtists}
         />
       </div>
-    </div>
-    </div>
+  </div>
       
   );
 };
 
 export default Album; // defalut export viktigt
+
+
+/*
+          
+            
+              
+            
+          </div>
+        
+          
+       
+
+      
+
+
+
+*/
