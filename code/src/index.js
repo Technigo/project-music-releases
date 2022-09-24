48b98ff6485cd4c2fe75c343abb703a163f0704e
a-well <amandaemkwellington@gmail.com>
2022-09-24 22:58:26 +0200

    Clean up code

--- code/src/index.js
+++ code/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import default exported App component
-// import App  from './App';
-
-// import named exported App component
 import { App } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
