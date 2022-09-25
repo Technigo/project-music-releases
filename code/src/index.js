1de64816d6e80f941fc82b936df45b08e200b342
David Ballester <dballesterfont@gmail.com>
2022-09-25 16:32:55 +0200

    [db]Trying to get rid off Deceptive site ahead error

--- code/src/index.js
+++ code/src/index.js
@@ -1,8 +1,27 @@
-import * as ReactDOMClient from 'react-dom/client';
+import { createRoot } from 'react-dom';
+import './index.css';
+import { App } from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
+
+
+/* import * as ReactDOMClient from 'react-dom/client';
 import { App } from './App';
 
 import './index.css';
 
 const root = ReactDOMClient.createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(<App />); */
+
+
+/* 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { App } from './App'
+
+ReactDOM.render(<App />, document.getElementById('root')) */
