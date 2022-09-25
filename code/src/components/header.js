a3461f022926605a118ab4c791e26058e19b3ea6
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-09-25 20:11:39 +0200

    Header

--- code/src/components/header.js
+++ code/src/components/header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export const Header = () => {
+const Header = () => {
   return (
     <header>
       <h1>New albums & singles</h1>
     </header>
   );
 };
+
+export default Header;
