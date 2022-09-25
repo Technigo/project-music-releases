ced1ba33c63f97f22d27299034ddbf9422ce981a
SofieRydmark <you@example.com>
2022-09-25 12:11:38 +0200

    responsive playlist sidebar

--- code/src/components/Playlist.js
+++ code/src/components/Playlist.js
@@ -2,20 +2,12 @@ import React from 'react'
 
 const Playlist = ( {playlistName, hrefPlaylist} ) => {
     return (   
-    <div className="sidePlaylist">
-        <div className="navheader">
-            <h4>Popular playlists</h4>
-            <a className="closebtn" onclick="closeNav()">&times;</a>
-        </div>
-            <div className="navlinks">
-                    <a href={hrefPlaylist} aria-label="Go to playlist" target="_blank">{playlistName}</a>
-                    <a href={hrefPlaylist} aria-label="Go to playlist" target="_blank">{playlistName}</a>
-                    <a href={hrefPlaylist} aria-label="Go to playlist" target="_blank">{playlistName}</a>
-                    <a href={hrefPlaylist} aria-label="Go to playlist" target="_blank">{playlistName}</a>
-                </div>
-            </div>
+            <li className="navlinks">
+                <a href={hrefPlaylist} aria-label="Go to playlist" target="_blank">{playlistName}</a>
+            </li>
+            
         )
-
+        
 }
 export default Playlist
 
