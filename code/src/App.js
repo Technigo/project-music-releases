2e6ff72de0760404c42bb85a5bcc33ee9dbe5e0c
Maria <so.mariagloria@gmail.com>
2022-09-25 14:35:57 +0300

    Last changes

--- code/src/App.js
+++ code/src/App.js
@@ -1,33 +1,23 @@
 import React from 'react';
-import data from './data.json'
-import { ArtistName } from './components/ArtistName';
-import { SongTitle } from './components/SongTitle';  //showing in html
-import { AlbumCover } from './components/AlbumCover';
-//import { TheAlbum } from './components/TheAlbum'
+import data from './data.json';
+import { AlbumDetails } from './components/AlbumDetails';
 
 console.log(data);
 
-/*<AlbumCover AlbumCover= {album.images[0].url}/>*/
-/* <div className='innit'> 
-                      {data && data.albums.map(item  =>{
-                        return <AlbumCover AlbumCover = {props.AlbumCover}
-                        name = {props.name}/>
-                      })}
-                    </div>*/ 
 export const App = () => {
   return(
+    <>
     <div className='Outline'> 
         <div className='Album-Wrapper'>
-            <section className="The-Album">         
-                <div className='Album-Picture'>
-                  {data.albums.items.map(item => {
-                    return <ArtistName key = {data.id} data = {item} coverpic = {data.albums.items[0].url}/> //declaring a property called data ,item is the value
-                    })}
-                   
-                </div>
+          <h1>New albums & singles</h1>
+              <section className="The-Album">   
+                    {data.albums.items.map(item => {
+                      return <AlbumDetails key = {data.id} data = {item}/>
+                      })}
             </section>
         </div>
     </div>
+    </>
     )
 
 }
