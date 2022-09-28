79c7d1f269c09db08acd265decd135c947e4b807
a-well <amandaemkwellington@gmail.com>
2022-09-28 13:12:20 +0200

    Remove unneccessary console.log

--- code/src/App.js
+++ code/src/App.js
@@ -3,8 +3,6 @@ import data from './data.json';
 import Header from './components/Header';
 import AlbumCard from './components/AlbumCard';
 
-console.log(data);
-
 export const App = () => {
   return (
     <>
