11a0de308e11d6af16832f01c2d14f701b49587f
Nora Terbocs <nora.terbocs@gmail.com>
2023-03-07 20:54:07 +0100

    Added key to artist

--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -5,7 +5,7 @@ const Artist = (props) => {
     <div className="artist">
       {props.artistName.map((artist, index) => {
         return (
-          <a className="artist-name" target="_blank" href={artist.external_urls.spotify} alt="Artistname" rel="noreferrer">{artist.name}    {props.artistName.length - 1 > index ? ', ' : ''}</a>
+          <a key={artist.id} className="artist-name" target="_blank" href={artist.external_urls.spotify} alt="Artistname" rel="noreferrer">{artist.name}    {props.artistName.length - 1 > index ? ', ' : ''}</a>
 
         )
       })}
