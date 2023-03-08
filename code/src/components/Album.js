22f36875c9f23e341d051a209442c39eb3cea576
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-03-08 18:05:47 +0100

    Album names component complete

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { AlbumArtists } from './AlbumArtists';
 import { AlbumImages } from './AlbumImages';
+import { AlbumName } from './AlbumName';
 
 export const Album = (props) => {
   console.log(props);
   return (
     <div>
       <AlbumImages imagesInput={props.albumInput.images} />
+      <AlbumName albumNamesInput={props.albumInput.name} />
       <AlbumArtists artistsInput={props.albumInput.artists} />
     </div>
   );
