98c9a2aa57beaabbd8e659b883eab1a53897689d
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-03-08 12:50:54 +0100

    added links

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -3,12 +3,17 @@ import React from 'react';
 const Album = (props) => {
   return (
     <article className="card">
-      <img className="albumArt" src={props.img} alt={props.albumName} />
-      <h2 className="albumName">{props.albumName}</h2>
-
+      <div className="linked-album">
+        <a href={props.albumLink} target="_blank" rel="noopener noreferrer">
+          <img className="albumArt" src={props.img} alt={props.albumName} />
+          <h2 className="albumName">{props.albumName}</h2>
+        </a>
+      </div>
       {props.artists.map((artist) => {
         return (
-          <h2 className="artistName">{artist.name}</h2>
+          <a href={artist.external_urls.spotify} target="_blank" rel="noopener noreferrer">           
+            <h2 className="artistName">{artist.name}</h2>
+          </a>
         )
       })}
     </article>
