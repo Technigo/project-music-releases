f78273071e10ec32c592c0aa465fdac9db6a44bc
theresBL <tbrannberglendt@gmail.com>
2023-03-08 21:18:51 +0100

    got all the info to show

--- code/src/components/Albums.js
+++ code/src/components/Albums.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import data from "../data.json"
-import { Card } from "./Card"
+import data from '../data.json'
+import { Card } from './Card'
 
 export const Albums = () => {
-    return (
-        <div className="album-names">
-            {data.albums.items.map((singleAlbumName) => (<Card props={singleAlbumName} />))}
-        </div>
-    )
+  return (
+    <div className="album-names">
+      {data.albums.items.map((singleAlbumName) => (<Card props={singleAlbumName} />))}
+    </div>
+  )
 }
 
--- code/src/components/Card.js
+++ code/src/components/Card.js
@@ -1,9 +1,24 @@
-import React from "react"
+import React from 'react'
+import '../index.css'
 
 export const Card = ({ props }) => {
-    return (
-        <div className="album-card">
-            <h1>{props.name}</h1>
-        </div>
-    )
+  return (
+    <div className="album-card">
+      <img src={props.images[1].url} alt={props.name} />
+      <h1>{props.name}</h1>
+      <h2>
+        {props.artists.map((artist) => (
+          <a
+            key={artist.id}
+            href={artist.external_urls.spotify}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="artist-container">
+            <span>{artist.name}</span>
+          </a>
+        ))}
+      </h2>
+    </div>
+  )
 }
+
