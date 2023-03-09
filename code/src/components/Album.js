131996b899dc1728dd8021b578d8830ef4fcd9b8
Irupé Pozo <irupe.pozo.graviz@gmail.com>
2023-03-09 10:35:51 +0100

    icons responding now

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -8,13 +8,15 @@ const Album = (props) => {
   return (
     <div className="album-container">
       <a className="albumcover" href={props.albums.external_urls.spotify} target="_blank" rel="noreferrer noopener">
+        <div className="icons">
+          <img src="./icons/heart.svg" alt="heart-icon" className="heart" />
+          <img src="./icons/play.svg" alt="play-icon" className="play" />
+          <img className="dots" src="./icons/dots.svg" alt="three-dots-icon" />
+        </div>
         <div className="album-img">
+
           <img src={props.albums.images[0].url} alt="album-cover" />
-          <div className="icons">
-            <img src="./icons/heart.svg" alt="heart-icon" className="heart" />
-            <img src="./icons/play.svg" alt="play-icon" className="play" />
-            <img className="dots" src="./icons/dots.svg" alt="three-dots-icon" />
-          </div>
+
         </div>
       </a>
       <a href={props.albums.external_urls.spotify} target="_blank" rel="noreferrer noopener">
