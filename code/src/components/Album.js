0a3a8f75cc4377534d59e77b849afe25510ef98b
Frid Matilda <matilda.x.frid@scania.com>
2023-03-09 14:40:05 +0100

    merge fixes

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import CoverImage from "./components/CoverImage";
-import ArtistName from "./ArtistName/ArtistName";
-import AlbumName from "./AlbumName/AlbumName";
-
-
+import CoverImage from './components/CoverImage/CoverImage';
+import ArtistName from './components/ArtistName/ArtistName';
+import AlbumName from './components/AlbumName/AlbumName';
 
 const Album = (props) => {
-    console.log(`logging props:`, props);
-    return (
-      <section>
-        {props.mapdata.albums.items.map((item) => {
-          return (
-            <div key={item.id}>
-              <CoverImage />
-              <ArtistName />
-              <AlbumName />
-            </div>
-          );
-        })}
-      </section>
-    );
-  };
+  console.log('logging props:', props);
+  return (
+    <section>
+      {props.mapdata.albums.items.map((item) => {
+        return (
+          <div key={item.id}>
+            <CoverImage />
+            <ArtistName />
+            <AlbumName />
+          </div>
+        );
+      })}
+    </section>
+  );
+};
 
-  export default Album;
+export default Album;
