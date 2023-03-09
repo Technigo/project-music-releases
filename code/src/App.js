ad9283f077f232e2a2f5b47e89e42f625ecc5dbb
Hannah Ek <ekhannahek@gmail.com>
2023-03-09 07:20:06 +0100

    updated

--- code/src/App.js
+++ code/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
 
 import data from './data.json';
-import Album from './components/Album';
-import { Header } from './components/Header';
+import Album from './components/album';
 
 console.log(data);
 
 export const App = () => {
   return (
-    <>
-      <Header />
-      <div>
-        <main className="albums">
-          {data.albums.items.map((item) => {
-            return <Album
-              key={item.id}
-              name={item.name} />
-          })}
-        </main>
-      </div>
-    </>
+    <div>
+      <main className="albums">
+        {data.albums.items.map((item) => {
+          return <Album
+            key={item.id}
+            name={item.name} />
+        })}
+      </main>
+    </div>
   );
 }
