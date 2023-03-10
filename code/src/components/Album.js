2cbcea10557c7de2bc32262043f73583d8670a3f
Johanna Leonsson <johanna@leonsson.se>
2023-03-10 12:13:49 +0100

    styled hover functions for cver image

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -5,7 +5,15 @@ import { ArtistName } from './ArtistName';
 export const Album = ({ album }) => {
   return (
     <div className="album-container" key={album.id}>
-      <img className="album-cover" src={album.images[1].url} alt={`${album.name} album cover`} />
+      <div className="img-overlay">
+        <img className="album-cover" src={album.images[1].url} alt={`${album.name} album cover`} />
+
+        <div className="img-icons">
+          <img className="heart-icon" src="./icons/heart.svg" alt="heart this song" />
+          <img className="play-icon" src="./icons/play.svg" alt="play this song" />
+          <img className="dots-icon" src="./icons/dots.svg" alt="options" />
+        </div>
+      </div>
       <a
         href={album.external_urls.spotify}
         className="song-title"
