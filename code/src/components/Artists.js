5061d25465cca94daf902da90cfe2fa27b6fa352
NinaW <120198299+NinaWald@users.noreply.github.com>
2023-03-10 17:33:41 +0100

    first in

---
+++ code/src/components/Artists.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const generateDelimiter = (index, length) => {
+  if (index === length - 2) {
+    return ' & ';
+  } else if (index < length - 2) {
+    return ', ';
+  }
+};
+
+export const Artist = (props) => {
+  const { artist } = props;
+  return (
+    <span>
+      {artist.map((items, index) => {
+        return (
+          <>
+            <a className="artist-link" href={items.external_urls.spotify} target="_blank" rel="noopener noreferrer">{items.name}</a>
+            {generateDelimiter(index, artist.lenght)}
+          </>
+        )
+      })}
+    </span>
+  )
+};
