d12db6232c97011d337756b15d0a87908e7945aa
camcron <camillacronqvist@hotmail.com>
2023-03-10 18:52:42 +0100

    ArtisName + links for srtist working

--- code/src/components/ArtistName/ArtistName.js
+++ code/src/components/ArtistName/ArtistName.js
@@ -1,14 +1,25 @@
-// Add external Spotify URL:s on artist & album
 import React from "react";
 import "./ArtistName.css";
 
 const ArtistName = (props) => {
-    return (
-        <div>
-            <h3 className="ArtistName"></h3>
-        </div>
-    );
+  return (
+    <div>
+      {props.singleArtistItem.artists.map((artist) => {
+        console.log("log artist:", artist);
+        return (
+          <div>
+            <a
+              href={artist.external_urls.spotify}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <h3 className="ArtistName">{artist.name}</h3>
+            </a>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
-
 
 export default ArtistName;
