2819249e8a10a306a1f707d2ae9bd4b95e70879b
Sofia Gerdmar <sofia.gerdmar@gmail.com>
2023-03-10 14:50:46 +0100

    fixed artist links in one line

--- code/src/components/ArtistsName.js
+++ code/src/components/ArtistsName.js
@@ -3,16 +3,12 @@ import React from 'react';
 export const ArtistsName = (props) => {
   console.log('artist', props);
   return (
-    <div className="artistContainer">
+    <span className="artistContainer">
       <a href={props.artistUrl} target="_blank" rel="noreferrer" className="artistLink">
         <span className="artistName">{props.name}</span>
       </a>
-      {props.isLast ? '' : ','}
-    </div>
+      {props.isLast ? '' : ', '}
+    </span>
   );
 };
 
-// eslint-disable-next-line max-len
-// {props.link} Lyfte ut denna så länge från href, tror det ska vara något annat där men osäker på vad.
-// eslint-disable-next-line max-len
-// Vi behöver även fixa så att det blir en rad när det är flera artistnamn och att det är samma färg på kommatecknet och texten <span></span>?
