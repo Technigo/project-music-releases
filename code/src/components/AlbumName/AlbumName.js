f146ba1ad6b1c5a4791e9b381613198a35b5934f
camcron <camillacronqvist@hotmail.com>
2023-03-10 17:52:30 +0100

    AlbumName

--- code/src/components/AlbumName/AlbumName.js
+++ code/src/components/AlbumName/AlbumName.js
@@ -1,13 +1,15 @@
-// Add external Spotify URL:s on artist & album
-import React from "react";
-import "./AlbumName.css";
+
+import React from 'react';
+import './AlbumName.css';
 
 const AlbumName = (props) => {
-    return (
-        <div>
-            <h2 className="AlbumName"></h2>
-        </div>
-    );
+  const { name } = props.singleAlbumItem;
+
+  return (
+    <div className="AlbumName">
+      <h3>{name}</h3>
+    </div>
+  );
 };
 
 export default AlbumName;
