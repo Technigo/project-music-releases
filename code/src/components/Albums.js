f2672c29213dc9b048681382d51cb073af90c0ac
Sandra Olsson <saol3165@win.su.se>
2023-03-10 19:13:40 +0100

    added comments

--- code/src/components/Albums.js
+++ code/src/components/Albums.js
@@ -3,6 +3,14 @@ import Artist from './Artist';
 import Buttons from './Buttons';
 import '../album.css';
 
+// const Albums function = Defining the functional component Albums that takes in a props object.
+// <img>-content= Creating an img element with the album cover image as the source,
+// and a class name of 'albumCover'.
+// <a>-content = Creating an anchor element with the album name as the text content,
+// and a class name of 'albumName'.
+// <Artist js-mount = Creating an instance of the Artist component and passing in the
+// artists array as a prop.
+
 const Albums = (props) => {
   return (
     <div className="album">
--- code/src/components/Artist.js
+++ code/src/components/Artist.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import '../artist.css'
 
+// const Artist = Defines a functional component called "Artist" that takes in a "props"
+// object as a parameter.
+
+// {index < props.artists.length - 1 && ...} is a conditional statement that checks whether
+// the current artist being mapped is not the last artist in the "artists" array. If it's not
+// the last artist, then a comma (',') is displayed after the artist's name. If it is
+// the second to last artist, ('&') is displayed instead of the comma. The ...
+// syntax is used to include a <span> element that contains either the comma or the ampersand,
+// depending on the current artist's index in the array.
+
 const Artist = (props) => {
   return (
     <div>
