f30ed62299a8cca65abf8a19fd9eb04252e1eea4
Malin <malin@MacBook-Pro-2.local>
2023-03-10 17:52:43 +0100

    blue level done Co-authored-by: Josefin Robertsson <JosefinRobertsson@users.noreply.github.com>

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -5,23 +5,36 @@ export const Album = (props) => {
   return (
     <div className="album-container">{props.spotifyData.albums.items.map((albumTitle) => {
       return (
-        <div>
-          <img src={albumTitle.images[1].url} alt="pic" />
+        <div className="single-album-container">
+          <div className="album-cover">
+            <a
+              href={albumTitle.external_urls.spotify}
+              target="_blank"
+              rel="noreferrer">
+              <div className="icon-container">
+                <img src="./icons/heart.svg" alt="heart" />
+                <img src="./icons/play.svg" alt="play-button" />
+                <img src="./icons/dots.svg" alt="dots" />
+              </div>
+            </a>
+            <img src={albumTitle.images[1].url} className="cover" alt="pic" />
+          </div>
           <a
             href={albumTitle.external_urls.spotify}
             target="_blank"
             rel="noreferrer">
             <h2>{albumTitle.name}</h2>
           </a>
-          {albumTitle.artists.map((singleArtist) => {
+          <div className="artist-container">{albumTitle.artists.map((singleArtist) => {
             return (
-              <a href={singleArtist.external_urls.spotify} target="_blank" rel="noreferrer">
-                <h3><span>{singleArtist.name}, </span></h3>
+              <a href={singleArtist.external_urls.spotify} className="artist-link" target="_blank" rel="noreferrer">
+                <h3><span>{singleArtist.name}</span></h3>
               </a>)
           })}
-        </div>)
+          </div>
+        </div>
+      )
     })}
-
     </div>
   );
 }
