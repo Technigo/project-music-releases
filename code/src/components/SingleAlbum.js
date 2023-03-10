cf98d01d88e9219b77e641bdc48ac74e05e6cdf7
Nora Terbocs <nora.terbocs@gmail.com>
2023-03-10 19:37:40 +0100

    Changed width of playlist section to 100%

--- code/src/components/SingleAlbum.js
+++ code/src/components/SingleAlbum.js
@@ -4,28 +4,33 @@ import Artist from './Artist';
 
 const SingleAlbum = (props) => {
   return (
-    <section className="albums albums-type-singles">
-      {props.data.albums.items.map((singleItem) => {
-        if (singleItem.album_type === 'single') {
-          return (
-            <div className="album-info" key={singleItem.id}>
-              <div className="img-btn-cover">
-                <div className="album-img-container">
-                  <img className="album-img" src={singleItem.images[0].url} alt={singleItem.name} />
+    <section>
+      <h2 className="sub-title">New Singles</h2>
+      <div className="albums albums-type-singles">
+
+        {props.data.albums.items.map((singleItem) => {
+          if (singleItem.album_type === 'single') {
+            return (
+              <div className="album-info" key={singleItem.id}>
+                <div className="img-btn-cover">
+                  <div className="album-img-container">
+                    <img className="album-img" src={singleItem.images[0].url} alt={singleItem.name} />
+                  </div>
+                  <Buttons />
                 </div>
-                <Buttons />
+                <a className="album-name" target="_blank" href={singleItem.external_urls.spotify} alt={singleItem.name} rel="noreferrer">{singleItem.name}</a>
+                <Artist artistName={singleItem.artists} />
               </div>
-              <a className="album-name" target="_blank" href={singleItem.external_urls.spotify} alt={singleItem.name} rel="noreferrer">{singleItem.name}</a>
-              <Artist artistName={singleItem.artists} />
-            </div>
-          )
-        } else {
-          return (
-            console.log('Not a single')
-          )
-        }
-      })}
+            )
+          } else {
+            return (
+              console.log('Not a single')
+            )
+          }
+        })}
+      </div>
     </section>
+
   );
 };
 
