5c29f19216d76d33243a5233e91af7beaf061eb7
Frid Matilda <matilda.x.frid@scania.com>
2023-03-11 15:29:33 +0100

    a lot of fixes

--- code/src/App.js
+++ code/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import data from './data.json';
-import { Header } from './components/Header/Header';
-import { Album } from './components/Album';
+import { Header } from './components/Header/Header.js';
+import { Album } from './components/Album.js'
+
+import './index.css';
 
 console.log(data);
 
 export const App = () => {
   return (
     <div>
+      <h1>Tjoho</h1>
       <Header />
       <Album mapdata={data} />
     </div>
--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import CoverImage from './components/CoverImage';
-import { ArtistName } from './ArtistName/ArtistName';
-import { AlbumName } from './AlbumName/AlbumName';
+import { CoverImage } from './CoverImage/CoverImage.js';
+import { ArtistName } from './ArtistName/ArtistName.js';
+import { AlbumName } from './AlbumName/AlbumName.js';
 
 export const Album = (props) => {
   console.log('logging props:', props);
   return (
-    <section>
+    <section className="album-wrapper">
       {props.mapdata.albums.items.map((singleItem) => {
+        console.log('log singleItem:', singleItem);
         return (
-          <div className="album-wrapper" key={singleItem.id}>
-            <CoverImage itemCoverImage={singleItem} />
+          <div className="album" key={singleItem.id}>
+            <CoverImage singleCoverItem={singleItem} />
             <AlbumName singleAlbumItem={singleItem} />
             <ArtistName singleArtistItem={singleItem} />
           </div>
