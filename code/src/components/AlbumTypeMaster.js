285132e8851a9116fbfe15941b3d50caf9dbf96e
Vera Sjunnesson <Vera@admins-MacBook-Pro.local>
2023-03-12 10:40:52 +0100

    minor formatting fix

--- code/src/components/AlbumTypeMaster.js
+++ code/src/components/AlbumTypeMaster.js
@@ -5,12 +5,10 @@ import { Album } from './Album';
 export const AlbumTypeMaster = (props) => {
   return (
     <div className="albums">
-      {
-        data.albums.items.filter((item) => item.album_type === props.albumTypeCategory)
-          .map((item) => {
-            return <Album albumCategory={item} key={item.id} />
-          })
-      }
+      {data.albums.items.filter((item) => item.album_type === props.albumTypeCategory)
+        .map((item) => {
+          return <Album albumCategory={item} key={item.id} />
+        })}
     </div>
   );
 };
