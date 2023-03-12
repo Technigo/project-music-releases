b19460100285a116ace1c50107cdcb41b998fcd9
AjmalZ <ajmal.zahir@hotmail.com>
2023-03-12 21:52:07 +0100

    clone

---
+++ code/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>Playlist Curated and Coded</p>
+      <p>by Ajmal and Peki</p>
+      <p>2023</p>
+    </div>
+  );
+}
+export default Footer;
---
+++ code/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1 className="header-title">New Music</h1>
+      <div className="header-line" />
+    </div>
+  );
+}
+// export default
+export default Header;
